b396127af367650c395c4189d6595d18321162aa
arendst <mhtarends@gmail.com>
2016-07-22 14:26:44 +0200

    Updated with hostname information

--- api/upload-arduino.php
+++ api/upload-arduino.php
@@ -5,10 +5,11 @@
 
 $image = basename($_FILES["file"]["name"]);
 $target_file = "arduino/".$image;
+$hostname = $_SERVER['SERVER_NAME'];
 
 if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
-  echo "The file $image has been uploaded to the OTA server. \n";
+  echo "The file $image has been uploaded to OTA server $hostname. \n";
 } else {
-  echo "Sorry, there was an error uploading your file. \n";
+  echo "Sorry, there was an error uploading your file $image to OTA server $hostname. \n";
 }
 ?>
