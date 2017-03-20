e19bd1be100a6d0750d9567e36a71b7ba06837ad
Ion <ionciubotaru@users.noreply.github.com>
2017-03-20 08:24:16 +0200

    Add dedicated space for user's functions

---
+++ sonoff/user.ino
@@ -0,0 +1,12 @@
+void user_before_setup() {
+// code to run before Tasmota's setup
+}
+
+void user_after_setup() {
+// code to run after Tasmota's setup
+}
+
+void user_loop() {
+// code to run inside loop
+}
+
