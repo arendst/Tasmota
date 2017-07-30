6ca7240c9da85657cf6130b504ed87555c03d673
Sebastian Muszynski <s.muszynski@constructiva.de>
2017-07-30 09:55:43 +0200

    Discovery of the used i2c address doesn't work. The sensor returns always true on tsl.begin().

--- sonoff/xsns_tsl2561.ino
+++ sonoff/xsns_tsl2561.ino
@@ -29,18 +29,7 @@ uint8_t tsl2561addr;
 uint8_t tsl2561type = 0;
 char tsl2561stype[8];
 
-uint8_t tsl2561addresses[] = {
-  TSL2561_ADDR_FLOAT,
-  TSL2561_ADDR_LOW,
-  TSL2561_ADDR_HIGH
-};
-
-TSL2561 tsl(tsl2561addresses[0]);
-
-uint16_t tsl2561_readLux(void)
-{
-  return tsl.getLuminosity(TSL2561_VISIBLE);
-}
+TSL2561 tsl(TSL2561_ADDR_FLOAT);
 
 boolean tsl2561_detect()
 {
@@ -49,30 +38,23 @@ boolean tsl2561_detect()
   }
 
   char log[LOGSZ];
-  boolean success = false;
 
-  for (byte i = 0; i < 4; i++) {
-    tsl2561addr = tsl2561addresses[i];
-    TSL2561 tsl(tsl2561addr);
+  snprintf_P(log, sizeof(log), PSTR("I2C: Probing addr 0x%x for TSL2561"), tsl2561addr);
+  addLog(LOG_LEVEL_DEBUG, log);
 
-    snprintf_P(log, sizeof(log), PSTR("I2C: Probing addr 0x%x for TSL2561."), tsl2561addr);
+  if(tsl.begin()) {
+    tsl.setGain(TSL2561_GAIN_16X);
+    tsl.setTiming(TSL2561_INTEGRATIONTIME_101MS);
+
+    tsl2561type = 1;
+    strcpy(tsl2561stype, "TSL2561");
+    snprintf_P(log, sizeof(log), PSTR("I2C: %s found at address 0x%x"), tsl2561stype, tsl2561addr);
     addLog(LOG_LEVEL_DEBUG, log);
 
-    if(tsl.begin()) {
-      tsl.setGain(TSL2561_GAIN_16X);
-      tsl.setTiming(TSL2561_INTEGRATIONTIME_101MS);
-
-      success = true;
-      tsl2561type = 1;
-      strcpy(tsl2561stype, "TSL2561");
-
-      snprintf_P(log, sizeof(log), PSTR("I2C: %s found at address 0x%x"), tsl2561stype, tsl2561addr);
-      addLog(LOG_LEVEL_DEBUG, log);
-      break;
-    }
+    return true;
   }
 
-  return success;
+  return false;
 }
 
 /*********************************************************************************************\
@@ -85,7 +67,7 @@ void tsl2561_mqttPresent(char* svalue, uint16_t ssvalue, uint8_t* djson)
     return;
   }
 
-  uint16_t l = tsl2561_readLux();
+  uint16_t l = tsl.getLuminosity(TSL2561_VISIBLE);
   snprintf_P(svalue, ssvalue, PSTR("%s, \"%s\":{\"Illuminance\":%d}"), svalue, tsl2561stype, l);
   *djson = 1;
 #ifdef USE_DOMOTICZ
@@ -102,7 +84,7 @@ String tsl2561_webPresent()
   String page = "";
   if (tsl2561type) {
     char sensor[80];
-    snprintf_P(sensor, sizeof(sensor), HTTP_SNS_TSL2561, tsl2561_readLux());
+    snprintf_P(sensor, sizeof(sensor), HTTP_SNS_TSL2561, tsl.getLuminosity(TSL2561_VISIBLE));
     page += sensor;
   }
   return page;
