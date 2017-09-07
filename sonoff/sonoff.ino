92ff7f0adc3f54f07e1f95bd4b37c38cb1143744
arendst <mhtarends@gmail.com>
2017-09-07 18:53:59 +0200

    v5.7.0
    5.7.0 20170907
    * Shrink module configuration webpage
    * Fix settings order during startup to allow for displaying debug
    messages
    * Fix some string length issues
    * Add more string length tests by using strncpy
    * Add Ai-Thinker RGBW led (AiLight)
    * Add Power check and add PulseTime to power check at startup (#526)
    * Add Supla Espablo support (#755)
    * Add more precision to Sonoff Pow period and power results using
    command WattRes 0|1 (#759)
    * Add basic internationalization and localization (#763)
    * Add more Sonoff Pow range checking (#772)
    * Fix invalid JSON (#786, #822)
    * Add duplicate check to received RF signal within 2 seconds for Sonoff
    Bridge (#810)

--- sonoff/sonoff.ino
+++ sonoff/sonoff.ino
@@ -25,7 +25,7 @@
     - Select IDE Tools - Flash Size: "1M (no SPIFFS)"
   ====================================================*/
 
-#define VERSION                0x0506010C  // 5.6.1l
+#define VERSION                0x05070000  // 5.7.0
 
 enum log_t   {LOG_LEVEL_NONE, LOG_LEVEL_ERROR, LOG_LEVEL_INFO, LOG_LEVEL_DEBUG, LOG_LEVEL_DEBUG_MORE, LOG_LEVEL_ALL};
 enum week_t  {Last, First, Second, Third, Fourth};
