ab5b53703cfbf093f32a4c11c98bf6beab1992f3
arendst <mhtarends@gmail.com>
2017-09-30 15:49:31 +0200

    Revert "Add optional pio OTA upload"
    This reverts commit 615a398b17c98aa5965f61e0c43e351bbd86084f.

--- pio/sftp-uploader.py
+++
@@ -1,6 +0,0 @@
-Import("env")
-from base64 import b64decode
-
-env.Replace(UPLOADER="scp")
-env.Replace(UPLOADERFLAGS="")
-env.Replace(UPLOADCMD="$UPLOADER $SOURCES " + b64decode(ARGUMENTS.get("UPLOAD_PORT")) + "/" + b64decode(ARGUMENTS.get("PIOENV")) + ".bin")
