8d27323fe68bb2915eea83621a1ea11adaa7e416
reloxx13 <reloxx@interia.pl>
2017-12-29 03:11:08 +0100

    new version

--- releases/releases.bat
+++ releases/releases.bat
@@ -1,4 +1,4 @@
-set version=5.10.0c-mod-1.2.2
+set version=5.10.0e-mod-1.2.3
 
 mkdir %version%
 
@@ -6,5 +6,6 @@ copy ..\.pioenvs\sonoff\firmware.bin %version%\EN-firmware-%version%.bin
 copy ..\.pioenvs\sonoff-DE\firmware.bin %version%\DE-firmware-%version%.bin
 copy ..\.pioenvs\sonoff-ds18x20\firmware.bin %version%\DS18X20-firmware-%version%.bin
 copy ..\.pioenvs\sonoff-minimal\firmware.bin %version%\MINIMAL-firmware-%version%.bin
+copy ..\.pioenvs\sonoff-IT\firmware.bin %version%\IT-firmware-%version%.bin
 copy ..\.pioenvs\sonoff-NL\firmware.bin %version%\NL-firmware-%version%.bin
 copy ..\.pioenvs\sonoff-PL\firmware.bin %version%\PL-firmware-%version%.bin
