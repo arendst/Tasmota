028967cc6fc0998fe7af52f1b2f1efd7d3858826
John Gouk <johngouk@googlemail.com>
2018-03-08 18:07:26 +0000

    Support multiple SHT3X/SHTC3 sensors on I2C

--- sonoff/xsns_14_sht3x.ino
+++ sonoff/xsns_14_sht3x.ino
@@ -20,7 +20,7 @@
 #ifdef USE_I2C
 #ifdef USE_SHT3X
 /*********************************************************************************************\
- * SHT3X and SHTC3 - Temperature and Humidy
+ * SHT3X and SHTC3 - Temperature and Humidity
  *
  * I2C Address: 0x44, 0x45 or 0x70 (SHTC3)
 \*********************************************************************************************/
@@ -29,14 +29,18 @@
 #define SHT3X_ADDR_VDD      0x45       // address pin high (VDD)
 #define SHTC3_ADDR          0x70       // address for shtc3 sensor
 
+#define MAX_SHT_SENSORS     3
+
 const char kShtTypes[] PROGMEM = "SHT3X|SHT3X|SHTC3";
 
-uint8_t sht3x_type = 0;
-uint8_t sht3x_address;
-uint8_t sht3x_addresses[] = { SHT3X_ADDR_GND, SHT3X_ADDR_VDD, SHTC3_ADDR };
-char sht3x_types[6];
+uint8_t sht3x_count = 0;
+uint8_t sht3x_addresses[MAX_SHT_SENSORS] = { SHT3X_ADDR_GND, SHT3X_ADDR_VDD, SHTC3_ADDR };
+struct SENSORINFO {
+  uint8_t address;    // I2C buss address
+  char type[11];      // Sensor type name and address - "SHT3X-0xXX"
+} sensors [MAX_SHT_SENSORS];
 
-bool Sht3xRead(float &t, float &h)
+bool Sht3xRead(float &t, float &h, uint8_t sht3x_address) // Parameterised for device address
 {
   unsigned int data[6];
 
@@ -49,7 +53,7 @@ bool Sht3xRead(float &t, float &h)
     Wire.write(0x17);                  // sleep
     Wire.endTransmission();
     Wire.beginTransmission(sht3x_address);
-    Wire.write(0x78);                  // Dissable clock stretching ( I don't think that wire library support clock stretching )
+    Wire.write(0x78);                  // Disable clock stretching ( I don't think that wire library support clock stretching )
     Wire.write(0x66);                  // High resolution
   } else {
     Wire.write(0x2C);                  // Enable clock stretching
@@ -72,49 +76,56 @@ bool Sht3xRead(float &t, float &h)
 
 void Sht3xDetect()
 {
-  if (sht3x_type) {
+  if (sht3x_count) {
     return;
   }
 
   float t;
   float h;
-  sht3x_type = 1;
-  for (byte i = 0; i < sizeof(sht3x_addresses); i++) {
-    sht3x_address = sht3x_addresses[i];
-    if (Sht3xRead(t, h)) {
-      GetTextIndexed(sht3x_types, sizeof(sht3x_types), i, kShtTypes);
-      snprintf_P(log_data, sizeof(log_data), S_LOG_I2C_FOUND_AT, sht3x_types, htu_address);
+  for (byte i = 0; i < MAX_SHT_SENSORS; i++) {
+    sensors[sht3x_count].address = sht3x_addresses[i];
+    if (Sht3xRead(t, h, sensors[sht3x_count].address)) {
+      sensors[sht3x_count].address = sht3x_addresses[i];
+      GetTextIndexed(sensors[sht3x_count].type, sizeof(sensors[sht3x_count].type), i, kShtTypes); // Put main Sensor type name 
+      snprintf_P(log_data, sizeof(log_data), S_LOG_I2C_FOUND_AT, sensors[sht3x_count].type, sensors[sht3x_count].address);  // Log type/address
       AddLog(LOG_LEVEL_DEBUG);
-      return;
+      // Rewrite Type name to include address for JSON/Web output
+      snprintf_P(sensors[sht3x_count].type, sizeof(sensors[sht3x_count].type), PSTR("%s_0x%02x"), sensors[sht3x_count].type, sensors[sht3x_count].address);
+      sht3x_count++;
     }
   }
-  sht3x_type = 0;
 }
 
 void Sht3xShow(boolean json)
 {
-  if (sht3x_type) {
+  if (sht3x_count) {
     float t;
     float h;
-    if (Sht3xRead(t, h)) {
-      char temperature[10];
-      char humidity[10];
-      dtostrfd(t, Settings.flag2.temperature_resolution, temperature);
-      dtostrfd(h, Settings.flag2.humidity_resolution, humidity);
+    char temperature[10];
+    char humidity[10];
+    bool Domoticz_notified = false; // Currently Domoticz only supports one T/H sensor
+    for (byte i = 0; i<sht3x_count; i++) {
+      if (Sht3xRead(t, h, sensors[i].address)) {
+        dtostrfd(t, Settings.flag2.temperature_resolution, temperature);
+        dtostrfd(h, Settings.flag2.humidity_resolution, humidity);
 
-      if (json) {
-        snprintf_P(mqtt_data, sizeof(mqtt_data), JSON_SNS_TEMPHUM, mqtt_data, sht3x_types, temperature, humidity);
+        if (json) {
+          snprintf_P(mqtt_data, sizeof(mqtt_data), JSON_SNS_TEMPHUM, mqtt_data, sensors[i].type, temperature, humidity);
 #ifdef USE_DOMOTICZ
-        DomoticzTempHumSensor(temperature, humidity);
+          if (!Domoticz_notified) {
+            DomoticzTempHumSensor(temperature, humidity);
+            Domoticz_notified = true;
+          }
 #endif  // USE_DOMOTICZ
 #ifdef USE_WEBSERVER
-      } else {
-        snprintf_P(mqtt_data, sizeof(mqtt_data), HTTP_SNS_TEMP, mqtt_data, sht3x_types, temperature, TempUnit());
-        snprintf_P(mqtt_data, sizeof(mqtt_data), HTTP_SNS_HUM, mqtt_data, sht3x_types, humidity);
+        } else {
+          snprintf_P(mqtt_data, sizeof(mqtt_data), HTTP_SNS_TEMP, mqtt_data, sensors[i].type, temperature, TempUnit());
+          snprintf_P(mqtt_data, sizeof(mqtt_data), HTTP_SNS_HUM, mqtt_data, sensors[i].type, humidity);
 #endif  // USE_WEBSERVER
-      }
-    }
-  }
+        } // IF Json or Webserver
+      }   // If valid read on sensor
+    }     // For all detected sensors
+  }       // Do we have any sensors?
 }
 
 /*********************************************************************************************\
