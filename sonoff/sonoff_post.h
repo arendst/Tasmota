3758541a51c8d727ec828a6609d5e84694d591d3
gemu2015 <gmutz2010@googlemail.com>
2018-03-31 10:22:02 +0200

    Sensirion SGP30 Support

--- sonoff/sonoff_post.h
+++ sonoff/sonoff_post.h
@@ -50,6 +50,7 @@ void WifiWpsStatusCallback(wps_cb_status status);
 #define USE_TSL2561                           // Add I2C code for TSL2561 sensor using library Adafruit TSL2561 Arduino (+1k2 code)
 #define USE_ADS1115                           // Add I2C code for ADS1115 16 bit A/D converter based on Adafruit ADS1x15 library (no library needed) (+0k7 code)
 //#define USE_ADS1115_I2CDEV                    // Add I2C code for ADS1115 16 bit A/D converter using library i2cdevlib-Core and i2cdevlib-ADS1115 (+2k code)
+#define USE_SGP30                              // Add I2C code for SGP30 sensor (+4k code)
 #define USE_INA219                            // Add I2C code for INA219 Low voltage and current sensor (+1k code)
 #define USE_MGS                               // Add I2C code for Xadow and Grove Mutichannel Gas sensor using library Multichannel_Gas_Sensor (+10k code)
 #define USE_MHZ19                             // Add support for MH-Z19 CO2 sensor (+2k code)
