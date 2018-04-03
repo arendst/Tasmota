4f593eafd95481eb362e06432e5398034c9e793f
reloxx13 <reloxx@interia.pl>
2018-04-03 21:21:14 +0200

    add new languages

--- releases/releases.bat
+++ releases/releases.bat
@@ -4,6 +4,7 @@ mkdir %version%
 
 copy ..\.pioenvs\sonoff\firmware.bin %version%\EN-firmware-%version%.bin
 copy ..\.pioenvs\sonoff-CN\firmware.bin %version%\CN-firmware-%version%.bin
+copy ..\.pioenvs\sonoff-CZ\firmware.bin %version%\CZ-firmware-%version%.bin
 copy ..\.pioenvs\sonoff-DE\firmware.bin %version%\DE-firmware-%version%.bin
 copy ..\.pioenvs\sonoff-ds18x20\firmware.bin %version%\DS18X20-firmware-%version%.bin
 copy ..\.pioenvs\sonoff-ES\firmware.bin %version%\ES-firmware-%version%.bin
@@ -15,5 +16,6 @@ copy ..\.pioenvs\sonoff-NL\firmware.bin %version%\NL-firmware-%version%.bin
 copy ..\.pioenvs\sonoff-PL\firmware.bin %version%\PL-firmware-%version%.bin
 copy ..\.pioenvs\sonoff-PT\firmware.bin %version%\PT-firmware-%version%.bin
 copy ..\.pioenvs\sonoff-RU\firmware.bin %version%\RU-firmware-%version%.bin
+copy ..\.pioenvs\sonoff-TW\firmware.bin %version%\TW-firmware-%version%.bin
 
 copy ..\.pioenvs\sonoff-DE-4MB\firmware.bin %version%\4MB-DE-firmware-%version%.bin
