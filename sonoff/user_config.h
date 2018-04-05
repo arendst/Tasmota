88046b37305405fcce4e804df1224b94297b4952
Theo Arends <mhtarends@gmail.com>
2018-04-05 16:13:17 +0200

    Align user_config_override_sample

--- sonoff/user_config.h
+++ sonoff/user_config.h
@@ -33,13 +33,14 @@
  *   Changes in this section do NOT need a change of define CFG_HOLDER.
  *
  * ATTENTION:
- * Users are advised to use the user_config_override.h file for any local changes.
+ * Users are advised to use the user_config_override.h file for most changes.
 \*********************************************************************************************/
 
 //#define USE_CONFIG_OVERRIDE                      // Uncomment to use user_config_override.h file. See README.md
 
 /*********************************************************************************************\
  * SECTION 1
+ * - After initial load any change here only take effect if CFG_HOLDER is changed too
 \*********************************************************************************************/
 
 // -- Master parameter control --------------------
