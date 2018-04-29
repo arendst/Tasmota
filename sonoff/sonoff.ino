3331585102a461b6b0f32bbe0390b7e95cc12d89
IceStuff <info@icestuff.com>
2018-04-29 19:44:05 -0300

    Disable Output If Temperature Sensor Fails

--- sonoff/sonoff.ino
+++ sonoff/sonoff.ino
@@ -25,7 +25,7 @@
     - Select IDE Tools - Flash Size: "1M (no SPIFFS)"
   ====================================================*/
 
-#define VERSION                0x050C000C   // 5.12.0l
+#define VERSION                0x050C0200   // 5.12.2a
 
 // Location specific includes
 #include <core_version.h>                   // Arduino_Esp8266 version information (ARDUINO_ESP8266_RELEASE and ARDUINO_ESP8266_RELEASE_2_3_0)
