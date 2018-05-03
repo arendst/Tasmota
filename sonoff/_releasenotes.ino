e6e126391dfd91d0d6cdfb8a8261e505ac052377
Theo Arends <mhtarends@gmail.com>
2018-05-03 15:21:45 +0200

    Fix compile error when ADC enabled and No rules
    5.13.1a
     * Fix compile error when ADC is enabled and Rules are disabled
    (#2608)

--- sonoff/_releasenotes.ino
+++ sonoff/_releasenotes.ino
@@ -1,5 +1,6 @@
 /* 5.13.1a
  * Change user_config.h otaurl to http://sonoff.maddox.co.uk/tasmota/sonoff.bin (#2588, #2602)
+ * Fix compile error when ADC is enabled and Rules are disabled (#2608)
  *
  * 5.13.1 20180501
  * Fix JSON buffers size too small for execution in some situations (#2580)
