d18707f9777bf7ca35c7c43da6942620e029a270
Theo Arends <mhtarends@gmail.com>
2018-05-11 15:00:41 +0200

    Fix KNX reconnection issue
    5.13.1a
    * Fix KNX reconnection issue (#2679)

--- sonoff/_releasenotes.ino
+++ sonoff/_releasenotes.ino
@@ -6,6 +6,7 @@
  * Fix several timer data input and output errors (#2597, #2620)
  * Fix KNX config error (#2628)
  * Fix sensor MHZ-19 vanishing data over time (#2659)
+ * Fix KNX reconnection issue (#2679)
  * Add Portuguese in Brazil language file
  * Add rule state test for On/Off in addition to 0/1 (#2613)
  * Add hardware serial option to MHZ-19 sensor (#2659)
