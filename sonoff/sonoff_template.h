89a4120082aa5f1cca5985b8cc50f74fa3c9b1fc
ken <ken@pavy>
2018-05-12 00:04:50 +0100

    added rcswitch functionality

--- sonoff/sonoff_template.h
+++ sonoff/sonoff_template.h
@@ -95,6 +95,7 @@ enum UserSelectablePins {
   GPIO_SR04_ECHO,      // SR04 Echo pin
   GPIO_SDM120_TX,      // SDM120 Serial interface
   GPIO_SDM120_RX,      // SDM120 Serial interface
+  GPIO_RCCWITCH,
   GPIO_SENSOR_END };
 
 // Programmer selectable GPIO functionality offset by user selectable GPIOs
@@ -139,7 +140,7 @@ const char kSensorNames[] PROGMEM =
   D_SENSOR_PMS5003 "|" D_SENSOR_SDS0X1 "|"
   D_SENSOR_SBR_TX "|" D_SENSOR_SBR_RX "|"
   D_SENSOR_SR04_TRIG "|" D_SENSOR_SR04_ECHO "|"
-  D_SENSOR_SDM120_TX "|" D_SENSOR_SDM120_RX;
+  D_SENSOR_SDM120_TX "|" D_SENSOR_SDM120_RX "|" D_SENSOR_RCSWITCH;
 
 /********************************************************************************************/
 
