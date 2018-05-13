cae2ed369add880bfd0eaa6e1f5742a4bb11025c
reloxx13 <reloxx@interia.pl>
2018-05-13 21:17:04 +0200

    new version

--- releases/releases.bat
+++ releases/releases.bat
@@ -1,4 +1,4 @@
-set version=5.13.1a-mod-1.21.1
+set version=5.13.1a-mod-1.21.2
 
 mkdir %version%
 
@@ -12,11 +12,16 @@ copy ..\.pioenvs\sonoff-FR\firmware.bin %version%\FR-firmware-%version%.bin
 copy ..\.pioenvs\sonoff-GR\firmware.bin %version%\GR-firmware-%version%.bin
 copy ..\.pioenvs\sonoff-HU\firmware.bin %version%\HU-firmware-%version%.bin
 copy ..\.pioenvs\sonoff-IT\firmware.bin %version%\IT-firmware-%version%.bin
-copy ..\.pioenvs\sonoff-minimal\firmware.bin %version%\MINIMAL-firmware-%version%.bin
 copy ..\.pioenvs\sonoff-NL\firmware.bin %version%\NL-firmware-%version%.bin
 copy ..\.pioenvs\sonoff-PL\firmware.bin %version%\PL-firmware-%version%.bin
 copy ..\.pioenvs\sonoff-PT\firmware.bin %version%\PT-firmware-%version%.bin
 copy ..\.pioenvs\sonoff-RU\firmware.bin %version%\RU-firmware-%version%.bin
 copy ..\.pioenvs\sonoff-TW\firmware.bin %version%\TW-firmware-%version%.bin
+
+copy ..\.pioenvs\sonoff-minimal\firmware.bin %version%\MINIMAL-firmware-%version%.bin
+copy ..\.pioenvs\sonoff-classic\firmware.bin %version%\CLASSIC-firmware-%version%.bin
+copy ..\.pioenvs\sonoff-knx\firmware.bin %version%\KNX-firmware-%version%.bin
+copy ..\.pioenvs\sonoff-allsensors\firmware.bin %version%\ALL_SENSORS-firmware-%version%.bin
+
 
 copy ..\.pioenvs\sonoff-DE-4MB\firmware.bin %version%\4MB-DE-firmware-%version%.bin
