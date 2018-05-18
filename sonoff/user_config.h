66720754817652221d8753aff5434e585466a3e8
ascillato <ascillato@gmail.com>
2018-05-18 15:00:37 -0300

    Added Optional Files
    Added Optional Files

--- sonoff/user_config.h
+++ sonoff/user_config.h
@@ -309,6 +309,10 @@
 
 //#define USE_SR04                                 // Add support for HC-SR04 ultrasonic devices (+1k code)
 
+/*********************************************************************************************\
+ * DISPLAY
+\*********************************************************************************************/
+
 //#define USE_DISPLAY                            // Add I2C Display Support for LCD, Oled and up to eigth Matrices (+19k code)
 
 //#define DISPLAY_CONFIG                         // Display Support for LCD, Oled
@@ -335,6 +339,167 @@
 #endif  // DISPLAY_CONFIG
 
 /*********************************************************************************************\
+ * TIME PROPORTIONAL CONTROLLER
+\*********************************************************************************************/
+
+/**
+ * Code to drive one or more relays in a time proportioned manner give a
+ * required power value.
+ *
+ * Given required power values in the range 0.0 to 1.0 the relays will be
+ * driven on/off in such that the average power suppled will represent
+ * the required power.
+ * The cycle time is configurable.  If, for example, the
+ * period is set to 10 minutes and the power input is 0.2 then the output will
+ * be on for two minutes in every ten minutes.
+ *
+ * A value for actuator dead time may be provided. If you have a device that
+ * takes a significant time to open/close then set this to the average of the
+ * open and close times.  The algorithim will then adjust the output timing
+ * accordingly to ensure that the output is not switched more rapidly than
+ * the actuator can cope with.
+ *
+ * A facility to invert the output is provided which can be useful when used in
+ * refrigeration processes and similar.
+ *
+ * In the case where only one relay is being driven the power value is set by
+ * writing the value to the mqtt topic cmnd/timeprop_setpower_0.  If more than
+ * one relay is being driven (as might be the case for a heat/cool application
+ * where one relay drives the heater and the other the cooler) then the power
+ * for the second relay is written to topic cmnd/timeprop_setpower_1 and so on.
+ *
+ * To cope with the problem of temporary wifi failure etc a
+ * TIMEPROP_MAX_UPDATE_INTERVALS value is available. This can be set to the max
+ * expected time between power updates and if this time is exceeded then the
+ * power will fallback to a given safe value until a new value is provided. Set
+ * the interval to 0 to disable this feature.
+ *
+ **/
+
+//#define USE_TIMEPROP    //  include the timeprop feature (+1.2k)
+
+// Configuration for single output
+/*
+ #define TIMEPROP_NUM_OUTPUTS          1       // how many outputs to control (with separate alogorithm for each)
+ #define TIMEPROP_CYCLETIMES           60      // cycle time seconds
+ #define TIMEPROP_DEADTIMES            0       // actuator action time seconds
+ #define TIMEPROP_OPINVERTS            false   // whether to invert the output
+ #define TIMEPROP_FALLBACK_POWERS      0       // falls back to this if too long betwen power updates
+ #define TIMEPROP_MAX_UPDATE_INTERVALS 120     // max no secs that are allowed between power updates (0 to disable)
+ #define TIMEPROP_RELAYS               1       // which relay to control 1:8
+/*
+
+// Configuration for two outputs:
+/*
+ #define TIMEPROP_NUM_OUTPUTS          2               // how many outputs to control (with separate alogorithm for each)
+ #define TIMEPROP_CYCLETIMES           60,     10      // cycle time seconds
+ #define TIMEPROP_DEADTIMES            0,      0       // actuator action time seconds
+ #define TIMEPROP_OPINVERTS            false,  false   // whether to invert the output
+ #define TIMEPROP_FALLBACK_POWERS      0,      0       // falls back to this if too long betwen power updates
+ #define TIMEPROP_MAX_UPDATE_INTERVALS 120,    120     // max no secs that are allowed between power updates (0 to disable)
+ #define TIMEPROP_RELAYS               1,      2       // which relay to control 1:8
+*/
+
+/*********************************************************************************************\
+ * PID CONTROLLER
+\*********************************************************************************************/
+
+// Help with using the PID algorithm and with loop tuning can be found at
+// http://blog.clanlaw.org.uk/2018/01/09/PID-tuning-with-node-red-contrib-pid.html
+// This is directed towards using the algorithm in the node-red node node-red-contrib-pid but the algorithm here is based on
+// the code there and the tuning techique described there should work just the same.
+
+//#define USE_PID         // include the pid feature (+4.3k)
+  #define PID_SETPOINT                  19.5    // Setpoint value. This is the process value that the process is
+                                                // aiming for.
+                                                // May be adjusted via MQTT using cmnd pid_sp
+
+  #define PID_PROPBAND                  5       // Proportional band in process units (eg degrees). This controls
+                                                // the gain of the loop and is the range of process value over which
+                                                // the power output will go from 0 to full power. The units are that
+                                                // of the process and setpoint, so for example in a heating
+                                                // application it might be set to 1.5 degrees.
+                                                // May be adjusted via MQTT using cmnd pid_pb
+
+  #define PID_INTEGRAL_TIME             1800    // Integral time seconds. This is a setting for the integral time,
+                                                // in seconds. It represents the time constant of the integration
+                                                // effect. The larger the value the slower the integral effect will be.
+                                                // Obviously the slower the process is the larger this should be. For
+                                                // example for a domestic room heated by convection radiators a setting
+                                                // of one hour might be appropriate (in seconds). To disable the
+                                                // integral effect set this to a large number.
+                                                // May be adjusted via MQTT using cmnd pid_ti
+
+  #define PID_DERIVATIVE_TIME           15      // Derivative time seconds. This is a setting for the derivative time,
+                                                // in seconds. It represents the time constant of the derivative effect.
+                                                // The larger the value the greater will be the derivative effect.
+                                                // Typically this will be set to somewhat less than 25% of the integral
+                                                // setting, once the integral has been adjusted to the optimum value. To
+                                                // disable the derivative effect set this to 0. When initially tuning a
+                                                // loop it is often sensible to start with derivative zero and wind it in
+                                                // once other parameters have been setup.
+                                                // May be adjusted via MQTT using cmnd pid_td
+
+  #define PID_INITIAL_INT               0.5     // Initial integral value (0:1). This is an initial value which is used
+                                                // to preset the integrated error value when the flow is deployed in
+                                                // order to assist in homing in on the setpoint the first time. It should
+                                                // be set to an estimate of what the power requirement might be in order
+                                                // to maintain the process at the setpoint. For example for a domestic
+                                                // room heating application it might be set to 0.2 indicating that 20% of
+                                                // the available power might be required to maintain the setpoint. The
+                                                // value is of no consequence apart from device restart.
+
+  #define PID_MAX_INTERVAL              300     // This is the maximum time in seconds that is expected between samples.
+                                                // It is provided to cope with unusual situations such as a faulty sensor
+                                                // that might prevent the node from being supplied with a process value.
+                                                // If no new process value is received for this time then the power is set
+                                                // to the value defined for PID_MANUAL_POWER.
+                                                // May be adjusted via MQTT using cmnd pid_max_interval
+
+  #define PID_DERIV_SMOOTH_FACTOR       3       // In situations where the process sensor has limited resolution (such as
+                                                // the DS18B20), the use of deriviative can be problematic as when the
+                                                // process is changing only slowly the steps in the value cause spikes in
+                                                // the derivative. To reduce the effect of these this parameter can be
+                                                // set to apply a filter to the derivative term. I have found that with
+                                                // the DS18B20 that a value of 3 here can be beneficial, providing
+                                                // effectively a low pass filter on the derivative at 1/3 of the derivative
+                                                // time. This feature may also be useful if the process value is particularly
+                                                // noisy. The smaller the value the greater the filtering effect but the
+                                                // more it will reduce the effectiveness of the derivative. A value of zero
+                                                // disables this feature.
+                                                // May be adjusted via MQTT using cmnd pid_d_smooth
+
+  #define PID_AUTO                      1       // Auto mode 1 or 0 (for manual). This can be used to enable or disable
+                                                // the control (1=enable, auto mode, 0=disabled, manual mode). When in
+                                                // manual mode the output is set the value definded for PID_MANUAL_POWER
+                                                // May be adjusted via MQTT using cmnd pid_auto
+
+  #define PID_MANUAL_POWER              0       // Power output when in manual mode or fallback mode if too long elapses
+                                                // between process values
+                                                // May be adjusted via MQTT using cmnd pid_manual_power
+
+  #define PID_UPDATE_SECS               0       // How often to run the pid algorithm (integer secs) or 0 to run the algorithm
+                                                // each time a new pv value is received, for most applictions specify 0.
+                                                // Otherwise set this to a time
+                                                // that is short compared to the response of the process.  For example,
+                                                // something like 15 seconds may well be appropriate for a domestic room
+                                                // heating application.
+                                                // May be adjusted via MQTT using cmnd pid_update_secs
+
+  #define PID_USE_TIMPROP               1       // To use an internal relay for a time proportioned output to drive the
+                                                // process, set this to indicate which timeprop output to use. For a device
+                                                // with just one relay then this will be 1.
+                                                // It is then also necessary to define USE_TIMEPROP and set the output up as
+                                                // explained in xdrv_91_timeprop.ino
+                                                // To disable this feature leave this undefined (undefined, not defined to nothing).
+
+  #define PID_USE_LOCAL_SENSOR                  // if defined then the local sensor will be used for pv. Leave undefined if
+                                                // this is not required.  The rate that the sensor is read is defined by TELE_PERIOD
+                                                // If not using the sensor then you can supply process values via MQTT using
+                                                // cmnd pid_pv
+
+
+/*********************************************************************************************\
  * Select features and sensors enabled in previous version saving space
 \*********************************************************************************************/
 
@@ -350,7 +515,7 @@
  * Select KNX without Emulation to save space
 \*********************************************************************************************/
 
-#define USE_KNX_NO_EMULATION                     // Create sonoff-knx with KNX but without Emulation (See sonoff_post.h)
+//#define USE_KNX_NO_EMULATION                     // Create sonoff-knx with KNX but without Emulation (See sonoff_post.h)
 
 /*********************************************************************************************\
  * Compile a minimal version if upgrade memory gets tight ONLY TO BE USED FOR UPGRADE STEP 1!
