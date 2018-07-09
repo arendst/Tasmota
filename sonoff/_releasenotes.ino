d875b0b1adc18cea65dad2368bb89159a058947b
Theo Arends <mhtarends@gmail.com>
2018-07-09 15:50:52 +0200

    Change DS18x20 driver
    Change DS18x20 driver to provide better instant results (#3169)

--- sonoff/_releasenotes.ino
+++ sonoff/_releasenotes.ino
@@ -1,5 +1,6 @@
 /* 6.1.0a
- * Change DS18B20 driver to provide instant results
+ * Change DS18x20 driver to provide better instant results (#3169)
+ * Change DS18B20 driver to provide better instant results
  * Remove TSL2561 debug message and update library (#2415)
  * Change SHT1x sensor initialization from pre-teleperiod to once during restart to fix I2C interference
  * Add wifi and mqtt status led blinkyblinky to be disabled by SetOption31 1. Does not work when LedPower is On (deliberate) (#871, #2230, #3114, #3155)
