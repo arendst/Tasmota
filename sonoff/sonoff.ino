5198ef9e74742e67625a3629327e6d37bd9537f7
reloxx13 <reloxx@interia.pl>
2018-07-12 19:23:44 +0200

    new version

--- sonoff/sonoff.ino
+++ sonoff/sonoff.ino
@@ -27,7 +27,7 @@
 
 #define VERSION                0x06010001   // 6.1.0a
 
-#define MOD_VERSION_STRING  "mod-1.34.1"    // Would be great to have a macro that fills this from VERSION ...
+#define MOD_VERSION_STRING  "mod-1.35.1"    // Would be great to have a macro that fills this from VERSION ...
 // Location specific includes
 #include <core_version.h>                   // Arduino_Esp8266 version information (ARDUINO_ESP8266_RELEASE and ARDUINO_ESP8266_RELEASE_2_3_0)
 #include "sonoff.h"                         // Enumeration used in user_config.h
