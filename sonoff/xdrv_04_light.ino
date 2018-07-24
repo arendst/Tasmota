d0fb7946bc183812013eac9c842414ee41648609
nhorvath <nhorvath@gmail.com>
2018-07-24 14:46:57 -0400

    merge changes to Use digitalWrite for PWM values over the threshold

--- sonoff/xdrv_04_light.ino
+++ sonoff/xdrv_04_light.ino
@@ -783,12 +783,16 @@ void LightAnimate()
         if (light_type < LT_PWM6) {
           if (pin[GPIO_PWM1 +i] < 99) {
             if (cur_col[i] > 0xFC) {
-              cur_col[i] = 0xFC;   // Fix unwanted blinking and PWM watchdog errors for values close to pwm_range (H801, Arilux and BN-SZ01)
+              // Fix unwanted blinking and PWM watchdog errors for values close to pwm_range (H801, Arilux and BN-SZ01)
+//              snprintf_P(log_data, sizeof(log_data), PSTR(D_LOG_APPLICATION "Cur_Col%d %d > 252 using digitalWrite instead of PWM"), i, cur_col[i]);
+//              AddLog(LOG_LEVEL_DEBUG);
+              digitalWrite(pin[GPIO_PWM1 +i], bitRead(pwm_inverted, i) ? LOW : HIGH);
+            } else {
+              uint16_t curcol = cur_col[i] * (Settings.pwm_range / 255);
+//              snprintf_P(log_data, sizeof(log_data), PSTR(D_LOG_APPLICATION "Cur_Col%d %d, CurCol %d"), i, cur_col[i], curcol);
+//              AddLog(LOG_LEVEL_DEBUG);
+              analogWrite(pin[GPIO_PWM1 +i], bitRead(pwm_inverted, i) ? Settings.pwm_range - curcol : curcol);
             }
-            uint16_t curcol = cur_col[i] * (Settings.pwm_range / 255);
-//            snprintf_P(log_data, sizeof(log_data), PSTR(D_LOG_APPLICATION "Cur_Col%d %d, CurCol %d"), i, cur_col[i], curcol);
-//            AddLog(LOG_LEVEL_DEBUG);
-            analogWrite(pin[GPIO_PWM1 +i], bitRead(pwm_inverted, i) ? Settings.pwm_range - curcol : curcol);
           }
         }
       }
