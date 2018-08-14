be82c830637fd3de3eaa77930a82935ca8cbf35c
gemu2015 <gmutz2010@googlemail.com>
2018-08-14 09:48:17 +0200

    redundancy removed

--- sonoff/xdrv_98_display.ino
+++ sonoff/xdrv_98_display.ino
@@ -41,6 +41,204 @@ const char S_JSON_DISPLAY_COMMAND_VALUE[] PROGMEM =        "{\"" D_CMND_DISPLAY 
 int16_t xpos,ypos;
 
 
+boolean DisplayCommand() {
+  char command [CMDSZ];
+  boolean serviced = true;
+  uint8_t disp_len = strlen(D_CMND_DISPLAY);  // Prep for string length change
+
+  if (!strncasecmp_P(XdrvMailbox.topic, PSTR(D_CMND_DISPLAY), disp_len)) {  // Prefix
+    int command_code = GetCommandCode(command, sizeof(command), XdrvMailbox.topic +disp_len, kDisplayCommands);
+    if (-1 == command_code) {
+      serviced = false;  // Unknown command
+    }
+    else if (CMND_DISP_TEXT == command_code) {
+      if (XdrvMailbox.data_len > 0) {
+        char *cp=XdrvMailbox.data;
+        uint8_t lpos,escape=0,var;
+        int16_t lin=0,col=0,fill=0,temp,temp1;
+        uint8_t font_x=6,font_y=8,fontnumber=1;
+        char linebuf[80],*dp=linebuf;
+        memset(linebuf,' ',sizeof(linebuf));
+        linebuf[sizeof(linebuf)-1]=0;
+        *dp=0;
+
+        while (*cp) {
+            if (!escape) {
+              // check for escape
+              if (*cp=='[') {
+                escape=1;
+                cp++;
+                // if string in buffer print it
+                if ((uint32_t)dp-(uint32_t)linebuf) {
+                    if (!fill) *dp=0;
+                    if (col>0 && lin>0) {
+                      // use col and lin
+                      DrawStringAt(col,lin,linebuf,1);
+                      //paint.DrawStringAt((col-1)*selected_font->Width,(lin-1)*selected_font->Height,linebuf,selected_font, COLORED);
+                    } else {
+                      // use xpos, ypos
+                      DrawStringAt(xpos,ypos,linebuf,0);
+                      //paint.DrawStringAt(xpos,ypos,linebuf,selected_font, COLORED);
+                    }
+                    memset(linebuf,' ',sizeof(linebuf));
+                    linebuf[sizeof(linebuf)-1]=0;
+                    dp=linebuf;
+                }
+              } else {
+                // copy chars
+                *dp++=*cp++;
+              }
+            } else {
+              // check escapes
+              if (*cp==']') {
+                escape=0;
+                cp++;
+              } else {
+                // analyze escapes
+                switch (*cp++) {
+                  case 'z':
+                    // clear display
+                    Clr_screen();
+                    break;
+                  case 'i':
+                    // init display with partial update
+                    Init_Partial();
+                    break;
+                  case 'I':
+                    // init display with full refresh
+                    Init_Full();
+                    break;
+                  case 'x':
+                    // set xpos
+                    var=atoiv(cp,&xpos);
+                    cp+=var;
+                    break;
+                  case 'y':
+                    // set ypos
+                    var=atoiv(cp,&ypos);
+                    cp+=var;
+                    break;
+                  case 'l':
+                    // text line lxx
+                    var=atoiv(cp,&lin);
+                    cp+=var;
+                    //display.setCursor(display.getCursorX(),(lin-1)*font_y*txtsize);
+                    break;
+                  case 'c':
+                    // text column cxx
+                    var=atoiv(cp,&col);
+                    cp+=var;
+                    //display.setCursor((col-1)*font_x*txtsize,display.getCursorY());
+                    break;
+                  case 'p':
+                    // pad field with spaces fxx
+                    var=atoiv(cp,&fill);
+                    cp+=var;
+                    linebuf[fill]=0;
+                    break;
+                  case 'h':
+                      // hor line to
+                    var=atoiv(cp,&temp);
+                    cp+=var;
+                    if (temp<0) {
+                      Draw_HLine(xpos+temp,ypos,-temp);
+                    } else {
+                      Draw_HLine(xpos,ypos,temp);
+                    }
+                    xpos+=temp;
+                    break;
+                  case 'v':
+                    // vert line to
+                    var=atoiv(cp,&temp);
+                    cp+=var;
+                    if (temp<0) {
+                      Draw_VLine(xpos,ypos+temp,-temp);
+                    } else {
+                      Draw_VLine(xpos,ypos,temp);
+                    }
+                    ypos+=temp;
+                    break;
+                  case 'k':
+                    // circle
+                    var=atoiv(cp,&temp);
+                    cp+=var;
+                    Draw_Circle(xpos,ypos,temp);
+                    break;
+                  case 'K':
+                    // filled circle
+                    var=atoiv(cp,&temp);
+                    cp+=var;
+                    Draw_FilledCircle(xpos,ypos,temp);
+                    break;
+                  case 'r':
+                    // rectangle
+                    var=atoiv(cp,&temp);
+                    cp+=var;
+                    cp++;
+                    var=atoiv(cp,&temp1);
+                    cp+=var;
+                    Draw_Rectangle(xpos,ypos,temp,temp1);
+                    break;
+                  case 'R':
+                    // filled rectangle
+                    var=atoiv(cp,&temp);
+                    cp+=var;
+                    cp++;
+                    var=atoiv(cp,&temp1);
+                    cp+=var;
+                    Draw_FilledRectangle(xpos,ypos,temp,temp1);
+                    break;
+                  case 't':
+                    sprintf(dp,"%02d:%02d",RtcTime.hour,RtcTime.minute);
+                    dp+=5;
+                    break;
+                  case 'd':
+                    // force draw grafics buffer
+                    Draw_Frame();
+                    break;
+                  case 's':
+                  case 'f':
+                    // size or font sx
+                    SetFontorSize(*cp&3);
+                    cp+=1;
+                    break;
+                  default:
+                    // unknown escape
+                    snprintf_P(XdrvMailbox.data, XdrvMailbox.data_len, PSTR("unknown escape"));
+                    goto exit;
+                    break;
+                }
+              }
+            }
+        }
+        exit:
+        // now draw buffer
+        if ((uint32_t)dp-(uint32_t)linebuf) {
+            if (!fill) *dp=0;
+            if (col>0 && lin>0) {
+              // use col and lin
+              DrawStringAt(col,lin,linebuf,1);
+              //paint.DrawStringAt((col-1)*selected_font->Width,(lin-1)*selected_font->Height,linebuf,selected_font, COLORED);
+            } else {
+              // use xpos, ypos
+              DrawStringAt(xpos,ypos,linebuf,0);
+              //paint.DrawStringAt(xpos,ypos,linebuf,selected_font, COLORED);
+            }
+        }
+        // draw buffer
+        Draw_Frame();
+
+      } else {
+        snprintf_P(XdrvMailbox.data, XdrvMailbox.data_len, PSTR("no Text"));
+      }
+      snprintf_P(mqtt_data, sizeof(mqtt_data), S_JSON_DISPLAY_COMMAND_VALUE, command, XdrvMailbox.data);
+    }
+  }
+  else serviced = false;  // Unknown command
+
+  return serviced;
+}
+
 #define XDRV_98
 
 boolean Xdrv98(byte function)
--- sonoff/xdrv_98_ssd1306.ino
+++ sonoff/xdrv_98_ssd1306.ino
@@ -43,6 +43,7 @@ Adafruit_SSD1306 display(OLED_RESET);
 #error("Height incorrect, please fix Adafruit_SSD1306.h!");
 #endif
 
+uint8_t font_x=6,font_y=8,txtsize=1;
 
 void DisplayInit(void) {
   // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
@@ -59,183 +60,58 @@ void DisplayInit(void) {
 }
 
 
-boolean DisplayCommand() {
-  char command [CMDSZ];
-  boolean serviced = true;
-  uint8_t disp_len = strlen(D_CMND_DISPLAY);  // Prep for string length change
+void Clr_screen() {
+  display.clearDisplay();
+  display.display();
+  display.setCursor(0,0);
+}
 
-  if (!strncasecmp_P(XdrvMailbox.topic, PSTR(D_CMND_DISPLAY), disp_len)) {  // Prefix
-    int command_code = GetCommandCode(command, sizeof(command), XdrvMailbox.topic +disp_len, kDisplayCommands);
-    if (-1 == command_code) {
-      serviced = false;  // Unknown command
-    }
-    else if (CMND_DISP_TEXT == command_code) {
-      if (XdrvMailbox.data_len > 0) {
-        char *cp=XdrvMailbox.data;
-        uint8_t lpos,escape=0,var;
-        int16_t lin=0,col=0,fill=0,temp,temp1;
-        uint8_t font_x=6,font_y=8,txtsize=1;
-        char linebuf[80],*dp=linebuf;
-        memset(linebuf,' ',sizeof(linebuf));
-        linebuf[sizeof(linebuf)-1]=0;
+void Init_Partial() {
+}
 
-        while (*cp) {
-            if (!escape) {
-              // character copy
-              if (*cp=='[') {
-                escape=1;
-                cp++;
-                if ((uint32_t)dp-(uint32_t)linebuf) {
-                  if (!fill) *dp=0;
-                  if (col==0 && lin==0) {
-                    // use xpos,ypos
-                    display.setCursor(xpos,ypos);
-                  }
-                  display.println(linebuf);
-                  display.display();
-                  memset(linebuf,' ',sizeof(linebuf));
-                  linebuf[sizeof(linebuf)-1]=0;
-                  dp=linebuf;
-                }
-              } else {
-                // copy chars
-                *dp++=*cp++;
-              }
-            } else {
-              // check escapes
-              if (*cp==']') {
-                escape=0;
-                cp++;
-              } else {
-                // analyze escapes
-                switch (*cp++) {
-                  case 'z':
-                    // clear display
-                    display.clearDisplay();
-                    display.display();
-                    display.setCursor(0,0);
-                    break;
-                  case 'i':
-                  case 'I':
-                    break;
-                  case 'x':
-                    // set xpos
-                    var=atoiv(cp,&xpos);
-                    cp+=var;
-                    break;
-                  case 'y':
-                    // set ypos
-                    var=atoiv(cp,&ypos);
-                    cp+=var;
-                    break;
-                  case 'l':
-                    // text line lxx
-                    var=atoiv(cp,&lin);
-                    cp+=var;
-                    display.setCursor(display.getCursorX(),(lin-1)*font_y*txtsize);
-                    break;
-                  case 'c':
-                    // text column cxx
-                    var=atoiv(cp,&col);
-                    cp+=var;
-                    display.setCursor((col-1)*font_x*txtsize,display.getCursorY());
-                    break;
-                  case 'p':
-                    // pad field with spaces fxx
-                    var=atoiv(cp,&fill);
-                    cp+=var;
-                    linebuf[fill]=0;
-                    break;
-                  case 'h':
-                      // hor line to
-                    var=atoiv(cp,&temp);
-                    cp+=var;
-                    if (temp<0) {
-                      display.writeFastHLine(xpos+temp,ypos,-temp,WHITE);
-                    } else {
-                      display.writeFastHLine(xpos,ypos,temp,WHITE);
-                    }
-                    xpos+=temp;
-                    break;
-                  case 'v':
-                    // vert line to
-                    var=atoiv(cp,&temp);
-                    cp+=var;
-                    if (temp<0) {
-                      display.writeFastVLine(xpos,ypos+temp,-temp,WHITE);
-                    } else {
-                      display.writeFastVLine(xpos,ypos,temp,WHITE);
-                    }
-                    ypos+=temp;
-                    break;
-                  case 'k':
-                    // circle
-                    var=atoiv(cp,&temp);
-                    cp+=var;
-                    display.drawCircle(xpos,ypos,temp,WHITE);
-                    break;
-                  case 'K':
-                    // filled circle
-                    var=atoiv(cp,&temp);
-                    cp+=var;
-                    display.fillCircle(xpos,ypos,temp,WHITE);
-                    break;
-                  case 'r':
-                    // rectangle
-                    var=atoiv(cp,&temp);
-                    cp+=var;
-                    var=atoiv(cp,&temp1);
-                    cp+=var;
-                    display.drawRect(xpos,ypos,temp,temp1,WHITE);
-                    break;
-                  case 'R':
-                    // filled rectangle
-                    var=atoiv(cp,&temp);
-                    cp+=var;
-                    var=atoiv(cp,&temp1);
-                    cp+=var;
-                    display.fillRect(xpos,ypos,temp,temp1,WHITE);
-                    break;
-                  case 't':
-                    sprintf(dp,"%02d:%02d",RtcTime.hour,RtcTime.minute);
-                    dp+=5;
-                    break;
-                  case 's':
-                  case 'f':
-                    // size sx
-                    txtsize=*cp&7;
-                    cp+=1;
-                    display.setTextSize(txtsize);
-                    break;
-                  default:
-                    // unknown escape
-                    snprintf_P(XdrvMailbox.data, XdrvMailbox.data_len, PSTR("unknown escape"));
-                    goto exit;
-                    break;
-                }
-              }
-            }
-        }
-        exit:
-        // now draw buffer
-        if ((uint32_t)dp-(uint32_t)linebuf) {
-          if (!fill) *dp=0;
-          if (col==0 && lin==0) {
-            // use xpos,ypos
-            display.setCursor(xpos,ypos);
-          }
-          display.println(linebuf);
-          display.display();
-        }
-      } else {
-        snprintf_P(XdrvMailbox.data, XdrvMailbox.data_len, PSTR("no Text"));
-      }
-      snprintf_P(mqtt_data, sizeof(mqtt_data), S_JSON_DISPLAY_COMMAND_VALUE, command, XdrvMailbox.data);
-    }
+void Init_Full() {
+}
+
+void Draw_HLine(uint16_t x,uint16_t y,int16_t len) {
+  display.writeFastHLine(x,y,len,WHITE);
+}
+
+void Draw_VLine(uint16_t x,uint16_t y,int16_t len) {
+  display.writeFastVLine(x,y,len,WHITE);
+}
+
+void Draw_Circle(uint16_t x,uint16_t y,uint16_t rad) {
+  display.drawCircle(x,y,rad,WHITE);
+}
+
+void Draw_FilledCircle(uint16_t x,uint16_t y,uint16_t rad) {
+  display.fillCircle(x,y,rad,WHITE);
+}
+
+void Draw_Rectangle(uint16_t x,uint16_t y,uint16_t x2,uint16_t y2) {
+  display.drawRect(x,y,x2,y2,WHITE);
+}
+
+void Draw_FilledRectangle(uint16_t x,uint16_t y,uint16_t x2,uint16_t y2) {
+  display.fillRect(x,y,x2,y2,WHITE);
+}
+
+void Draw_Frame() {
+  display.display();
+}
+
+void SetFontorSize(uint8_t font) {
+  display.setTextSize(txtsize);
+}
+
+
+void DrawStringAt(uint16_t x,uint16_t y,char *str,uint8_t flag) {
+  if (!flag) {
+    display.setCursor(x,y);
+  } else {
+    display.setCursor((x-1)*font_x*txtsize,(y-1)*font_y*txtsize);
   }
-  else serviced = false;  // Unknown command
-
-  return serviced;
+  display.println(str);
 }
 
 
