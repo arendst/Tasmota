04c81e31d185343df7d6f379a3bc1290db9c66c3
Theo Arends <11044339+arendst@users.noreply.github.com>
2018-08-27 18:16:28 +0200

    Fix Sonoff Bridge data length
    Fix Sonoff Bridge data reception when using Portisch EFM8 firmware and in data buffer length (#3605)

--- sonoff/_changelog.ino
+++ sonoff/_changelog.ino
@@ -2,6 +2,7 @@
  * Add commands ButtonDebounce 40..1000 and SwitchDebounce 40..1000 to have user control over debounce timing. Default is 50mS (#3594)
  * Add rule variables %sunrise%, %sunset%, %uptime% and %time% (#3608)
  * Fix handling use of default names when using names starting with shortcut character (#3392, #3600)
+ * Fix Sonoff Bridge data reception when using Portisch EFM8 firmware and in data buffer length (#3605)
  *
  * 6.1.1.11 20180826
  * Change scheduler phase 1/3 - Fixed when sleep is enabled: Uptime, Delay, PulseTime and TelePeriod (#3581)
