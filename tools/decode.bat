e24507059be082c1c13633fe57c24dbf4867abbf
Adrian Scillato <35405447+ascillato@users.noreply.github.com>
2018-09-04 15:15:04 -0300

    Uploaded windows batch file for decode-status.py and status.json example

---
+++ tools/decode.bat
@@ -0,0 +1,9 @@
+@echo off
+cls
+echo -------------------- TASMOTA FEATURES DECODE -------------------- 
+echo.
+echo.
+C:\Users\Usuario\.platformio\python27\python decode-status.py -f status.json
+echo.
+echo.
+pause
