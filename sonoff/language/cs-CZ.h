174f2736dccbd5307c6d33bc46e8d73e24786399
synekvl <42292671+synekvl@users.noreply.github.com>
2018-10-06 18:46:29 +0200

    Update cs-CZ.h

--- sonoff/language/cs-CZ.h
+++ sonoff/language/cs-CZ.h
@@ -28,7 +28,7 @@
  * Use online command StateText to translate ON, OFF, HOLD and TOGGLE.
  * Use online command Prefix to translate cmnd, stat and tele.
  *
- * Updated until v6.1.1c
+ * Updated until v6.2.1.11
 \*********************************************************************/
 
 //#define LANGUAGE_MODULE_NAME         // Enable to display "Module Generic" (ie Spanish), Disable to display "Generic Module" (ie English)
