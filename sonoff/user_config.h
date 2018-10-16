2085f24675f440cc56bc5afbc0e16d679ffd1972
Theo Arends <11044339+arendst@users.noreply.github.com>
2018-10-16 14:33:31 +0200

    Update changelog
    Update PubSubClient Mqtt library to non-blocking EspEasy version

--- sonoff/user_config.h
+++ sonoff/user_config.h
@@ -219,7 +219,7 @@
 /*-------------------------------------------------------------------------------------------*\
  * Select ONE of possible MQTT library types below
 \*-------------------------------------------------------------------------------------------*/
-  // Default MQTT driver for both non-TLS and TLS connections. Blocks network if MQTT server is unavailable.
+  // Default MQTT driver for both non-TLS and TLS connections. Latest library version (20181016) does not block network if MQTT server is unavailable.
 //#define MQTT_LIBRARY_TYPE      MQTT_PUBSUBCLIENT   // Use PubSubClient library
   // Alternative MQTT driver does not block network when MQTT server is unavailable. No TLS support
 //#define MQTT_LIBRARY_TYPE      MQTT_TASMOTAMQTT    // Use TasmotaMqtt library (+4k4 (core 2.3.0), +14k4 (core 2.4.2 lwip2) code, +4k mem) - non-TLS only
