c1aabf85a40aec67d3ff98a9c2368ed8ecb34b89
Adrian Scillato <35405447+ascillato@users.noreply.github.com>
2018-10-24 18:03:27 -0300

    Reformat options for DS18x20
    #define W1_PARASITE_POWER is an option for #define USE_DS18x20
    
    Added space to show that.

--- sonoff/my_user_config.h
+++ sonoff/my_user_config.h
@@ -271,9 +271,9 @@
 
 // -- One wire sensors ----------------------------
                                                  // WARNING: Select none for default one DS18B20 sensor or enable one of the following two options for multiple sensors
-#define USE_DS18x20                              // Optional for more than one DS18x20 sensors with id sort, single scan and read retry (+1k3 code)
-//#define W1_PARASITE_POWER                        // If using USE_DS18x20 then optimize for parasite powered sensors
 //#define USE_DS18x20_LEGACY                       // Optional for more than one DS18x20 sensors with dynamic scan using library OneWire (+1k5 code)
+#define USE_DS18x20                              // Optional for more than one DS18x20 sensors with id sort, single scan and read retry (+1k3 code)
+//  #define W1_PARASITE_POWER                      // If using USE_DS18x20 then optimize for parasite powered sensors
 
 // -- I2C sensors ---------------------------------
 #define USE_I2C                                  // I2C using library wire (+10k code, 0k2 mem, 124 iram)
