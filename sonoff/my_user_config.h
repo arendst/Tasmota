7e576a58f93086397afb504e9fd3922ad8f0688e
evzone <evzone@gmail.com>
2018-11-18 11:05:50 +0200

    commented user override to pass travis ci test

--- sonoff/my_user_config.h
+++ sonoff/my_user_config.h
@@ -38,6 +38,7 @@
 
 //#define USE_CONFIG_OVERRIDE                      // Uncomment to use user_config_override.h file. See README.md
 
+
 /*********************************************************************************************\
  * SECTION 1
  * - After initial load any change here only take effect if CFG_HOLDER is changed too
@@ -366,7 +367,7 @@
 
 // -- Low level interface devices -----------------
 #define USE_IR_REMOTE                            // Send IR remote commands using library IRremoteESP8266 and ArduinoJson (+4k code, 0k3 mem, 48 iram)
-//  #define USE_IR_HVAC                            // Support for HVAC system using IR (+2k code)
+#define USE_IR_HVAC                              // Support for HVAC system using IR (+2k code)
   #define USE_IR_RECEIVE                         // Support for IR receiver (+5k5 code, 264 iram)
 
 #define USE_WS2812                               // WS2812 Led string using library NeoPixelBus (+5k code, +1k mem, 232 iram) - Disable by //
