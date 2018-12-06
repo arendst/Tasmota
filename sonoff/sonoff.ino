e64c8d61c92d9fcd3e3963948fad040f3b607681
Theo Arends <11044339+arendst@users.noreply.github.com>
2018-12-06 11:01:57 +0100

    Update sonoff.ino

--- sonoff/sonoff.ino
+++ sonoff/sonoff.ino
@@ -1311,7 +1311,7 @@ boolean SendKey(byte key, byte device, byte state)
   if (Settings.flag.mqtt_enabled && MqttIsConnected() && (strlen(key_topic) != 0) && strcmp(key_topic, "0")) {
     if (!key && (device > devices_present)) device = 1;                                             // Only allow number of buttons up to number of devices
     GetTopic_P(stopic, CMND, key_topic, GetPowerDevice(scommand, device, sizeof(scommand),
-               Settings.flag.device_index_enable));                                                 // cmnd/switchtopic/POWERx
+               (key + Settings.flag.device_index_enable)));                                                 // cmnd/switchtopic/POWERx
     if (9 == state) {
       mqtt_data[0] = '\0';
     } else {
