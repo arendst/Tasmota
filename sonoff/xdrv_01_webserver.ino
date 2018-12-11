95c0c849de84ffd6c454c06d6a89bab265e723cc
Julien Viard de Galbert <julien@vdg.name>
2018-12-12 00:34:50 +0100

    Read response on WebSend instead of just fulshing it
    This makes the communication between two tasmota devices reliable.
    
    Exemple to remote control a S26 from a T1:
    rule1 on Power2#State do WebSend [sonoff-2] Power %value% endon

--- sonoff/xdrv_01_webserver.ino
+++ sonoff/xdrv_01_webserver.ino
@@ -1981,7 +1981,16 @@ int WebSend(char *buffer)
 //AddLog(LOG_LEVEL_DEBUG);
 
         client.print(url.c_str());
-        client.flush();
+//        String rsp = "";
+        while (client.connected())
+        {
+          if (client.available()) {
+            char c = client.read();
+//            rsp += c;
+          }
+        }
+//        snprintf_P(log_data, sizeof(log_data), PSTR("DBG: Resp |%s|"), rsp.c_str());
+//        AddLog(LOG_LEVEL_DEBUG);
         client.stop();
         status = 0;                           // No error - Done
       } else {
