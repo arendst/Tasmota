f94c2245afefd63b646107063eaa43e532c0ca05
Xavier MULLER <33861984+localhost61@users.noreply.github.com>
2019-02-17 01:02:52 +0100

    Update fr-FR.h

--- sonoff/language/fr-FR.h
+++ sonoff/language/fr-FR.h
@@ -28,7 +28,7 @@
  * Use online command StateText to translate ON, OFF, HOLD and TOGGLE.
  * Use online command Prefix to translate cmnd, stat and tele.
  *
- * Updated until v6.3.0.17
+ * Updated until v6.4.1.17
 \*********************************************************************/
 
 #define LANGUAGE_MODULE_NAME         // Enable to display "Module Generic" (ie Spanish), Disable to display "Generic Module" (ie English)
