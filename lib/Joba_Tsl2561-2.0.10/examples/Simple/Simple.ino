b800db4bac460d95fabfd9073d2410f282960661
Theo Arends <11044339+arendst@users.noreply.github.com>
2019-04-18 17:28:56 +0200

    Update library Joba_Tsl2561 from 2.0.7 to 2.0.10
    Update library Joba_Tsl2561 from 2.0.7 to 2.0.10

--- lib/Joba_Tsl2561-2.0.10/examples/Simple/Simple.ino
+++ lib/Joba_Tsl2561-2.0.10/examples/Simple/Simple.ino
@@ -21,7 +21,7 @@ This file is part of the Joba_Tsl2561 Library.
 
 #include <Tsl2561.h>
 
-// to mimic Serial.printf() of esp8266 core for other platforms
+// to mimic Serial.printf() of esp cores for other platforms
 char *format( const char *fmt, ... ) {
   static char buf[128];
   va_list arg;
@@ -36,7 +36,7 @@ Tsl2561 Tsl(Wire);
 
 void setup() {
   Serial.begin(115200);
-  Wire.begin();
+  Wire.begin(TSL2561_SDA, TSL2561_SCL);
   Serial.println("\nStarting Tsl2561 simple loop");
 }
 
@@ -60,7 +60,7 @@ void loop() {
     Tsl.off();
   }
   else {
-    Serial.println("No Tsl2561 found. Check wiring.");
+    Serial.print(format("No Tsl2561 found. Check wiring: SCL=%u, SDA=%u\n", TSL2561_SCL, TSL2561_SDA));
   }
 
   delay(5000);
