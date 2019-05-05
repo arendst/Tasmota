b2f5ebaf643a66f9f2998c947e0ff9ad056a33a7
hans boot <hansboot020@yahoo.com>
2019-05-05 15:07:36 +0200

    oops, one forgotten. Add languages, prepare for final travis check

--- sonoff/language/ru-RU.h
+++ sonoff/language/ru-RU.h
@@ -294,7 +294,7 @@
 #define D_TEMPLATE "Template"
 #define D_ACTIVATE "Activate"
 #define D_WEB_ADMIN_PASSWORD "Пароль Web администратора"
-#define D_WEB_USER_PASSWORD "Пароль Web администратора"
+#define D_WEB_USER_PASSWORD "Пароль Web пользователя"
 #define D_MQTT_ENABLE "MQTT активен"
 #define D_FRIENDLY_NAME "Дружественное Имя"
 #define D_BELKIN_WEMO "Belkin WeMo"
