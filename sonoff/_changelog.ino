d0502c1af869100572f05d6e962d8969d6dbbe2f
Theo Arends <11044339+arendst@users.noreply.github.com>
2019-06-02 18:07:54 +0200

    Fix PZem startup issue
    Fix PZem startup issue (#5875)

--- sonoff/_changelog.ino
+++ sonoff/_changelog.ino
@@ -1,6 +1,7 @@
 /* 6.5.0.14 20190602
  * Change webserver HTML input, button, textarea, and select name based on id
  * Fix webserver multiple Javascript window.onload functionality
+ * Fix PZem startup issue (#5875)
  *
  * 6.5.0.13 20190527
  * Add command SetOption38 6..255 to set IRReceive protocol detection sensitivity mimizing UNKNOWN protocols (#5853)
