9aa05889b090165431cc9c0c86b5782c713b5316
Theo Arends <11044339+arendst@users.noreply.github.com>
2019-06-03 18:00:29 +0200

    Revert "Fix pio compile parameters"
    This reverts commit efb4d088234bc48342fcd445ad7f025a2900353f.

--- pio/http-uploader.py
+++ pio/http-uploader.py
@@ -4,11 +4,9 @@ from base64 import b64decode
 
 env.Replace(UPLOADER="pio\espupload.py")
 env.Replace(UPLOADERFLAGS="")
-env.Replace(UPLOADCMD="$UPLOADER -u $UPLOAD_PORT -f $SOURCES")
+env.Replace(UPLOADCMD="$UPLOADER -u " + b64decode(ARGUMENTS.get("UPLOAD_PORT")) + " -f $SOURCES")
 
 '''
-env.Replace(UPLOADCMD="$UPLOADER -u domus1:80/api/upload-arduino.php -f $SOURCES")
-env.Replace(UPLOADCMD="$UPLOADER -u " + b64decode(ARGUMENTS.get("UPLOAD_PORT")) + " -f $SOURCES")
 env.Replace(UPLOADCMD="pio\espupload.py -f $SOURCES") # Windows
 env.Replace(UPLOADCMD="pio/espupload.py -f $SOURCES") # Linux
 '''
