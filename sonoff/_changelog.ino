781cf6993359af9dde4f0afb03ab3ed27292017e
Theo Arends <11044339+arendst@users.noreply.github.com>
2019-07-31 16:59:12 +0200

    Add disable option to Width
    Add option 0 to Width1 (Marker), Width2 (Second), Width3 (Minute) and Width4 (Hour) disabling display (#6152)

--- sonoff/_changelog.ino
+++ sonoff/_changelog.ino
@@ -2,6 +2,8 @@
  * 6.6.0.3 20190725
  * Change filename of configuration backup from using FriendlyName1 to Hostname solving diacritic issues (#2422)
  * Upgrade library IRRemoteEsp8266 to 2.6.4, now using sendPioneer()
+ * Add support for MAX31865 Thermocouple sensor by Alberto Lopez Siemens
+ * Add option 0 to Width1 (Marker), Width2 (Second), Width3 (Minute) and Width4 (Hour) disabling display (#6152)
  *
  * 6.6.0.2 20190714
  * Change commands Var and Mem to show all parameters when no index is given (#6107)
