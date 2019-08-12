3e9ec0144332630e3c71044a4a9967c37cd91f65
PetrNovacek <39201428+r21m@users.noreply.github.com>
2019-08-12 20:26:42 +0200

    Add files via upload

--- sonoff/xsns_55_gdk.ino
+++ sonoff/xsns_55_gdk.ino
@@ -18,52 +18,47 @@
 #ifdef USE_GDK101
 
 #define XSNS_55 55
-#define I2_ADR_IRT 0x18
-
 //I2C register
 #define READ_FIRMWARE 0xB4
 #define RESET 0xA0
-
 #define READ_STATUS 0xB0
 #define READ_MEASURING_TIME 0xB1
-#define READ_10MIN_AVG 0xB2 //Read Measuring Value (10min avg / 1min update)
-#define READ_1MIN_AVG  0xB3 //Read Measuring Value (1min avg / 1min update)
-//gdk101 variables
-bool gdk_ready;
+#define READ_10MIN_AVG 0xB2 
+#define READ_1MIN_AVG  0xB3
 
-float fw_version;
-float mea_10min_avg;
-float mea_1min_avg;
+//gdk101 variables
+bool     gdk_ready;
+float    fw_version;
+float    mea_10min_avg;
+float    mea_1min_avg;
 uint8_t  gdk_status;
-bool vibration = false;
+bool     gdk_vibration = false;
 uint8_t  mea_time_min;
 uint8_t  mea_time_sec;
-uint8_t rw_buffer[2] = {0, 0};
-uint16_t _rw_buffer;
-
-uint8_t  gdk_resolution = 2;
+uint8_t  rw_buffer[2] = {0, 0};
+uint8_t  evr10 = 0;
+uint8_t  vibr = 0;
 
 #ifdef USE_WEBSERVER
 const char HTTP_GDK101_SNS[] PROGMEM = "%s"
-  "{s}" "GDK101 gamma sensor" "{m}"
-  "{s}" "average 10 min" "{m}%0.2f uSv/h{e}"
-  "{s}" "average 1 min " "{m}%0.2f uSv/h{e}"
-  "{s}" "Status"         "{m}%i{e}"
-  "{s}" "Vibration"      "{m}%i{e}";      // {s} = <tr><th>, {m} = </th><td>, {e} = </td></tr>
+                                       "{s}" "GDK101 Average 10 min" "{m}%0.2f uSv/h{e}"
+                                       "{s}" "GDK101 Average 1 min " "{m}%0.2f uSv/h{e}"
+                                       "{s}" "GDK101 Status"         "{m}%i{e}"
+                                       "{s}" "GDK101 VIBRATION"      "{m}%i{e}";
+                                     // {s} = <tr><th>, {m} = </th><td>, {e} = </td></tr>
 #endif  // USE_WEBSERVER
 
 
 void gdk101_Init() {
 
   if (gdk_ready) return;
-  
+
   bool ts;
   ts = gdk101_reset();
   if (ts) {
-    AddLog_P2(LOG_LEVEL_DEBUG, S_LOG_I2C_FOUND_AT, "GDK101", I2_ADR_IRT);
+    AddLog_P2(LOG_LEVEL_DEBUG, S_LOG_I2C_FOUND_AT, "GDK101", USE_GDK101_ADDR);
     gdk_ready = true;
   }
-
 
 }
 
@@ -71,7 +66,6 @@ bool gdk101_reset() {
   bool val;
   gamma_mod_read(RESET); //
   val = rw_buffer[0];
-  delay(10);
   return val;
 }
 
@@ -80,79 +74,55 @@ void gdk101_update_all() {
   gdk101_get_1min_avg();
   gdk101_get_vib();
   gdk101_get_status();
-  gdk101_get_measuring_time_min();
-  gdk101_get_measuring_time_sec();
+//  gdk101_get_measuring_time_min();
+//  gdk101_get_measuring_time_sec();
 }
 
 float gdk101_get_fw_version() {
   gamma_mod_read(READ_FIRMWARE);
   fw_version = rw_buffer[0] + (float)rw_buffer[1] / 10;
-  // return fw_version;
 }
 
 float gdk101_get_10min_avg() {
   gamma_mod_read(READ_10MIN_AVG);
   mea_10min_avg = rw_buffer[0] + (float)rw_buffer[1] / 100;
-  // return mea_10min_avg;
 }
 
 float gdk101_get_1min_avg() {
   gamma_mod_read(READ_1MIN_AVG);
   mea_1min_avg = rw_buffer[0] + (float)rw_buffer[1] / 100;
-  //  return mea_1min_avg;
 }
 
 bool gdk101_get_vib() {
   gamma_mod_read(READ_STATUS);
-  vibration = rw_buffer[1];
-  // return vib;
+  if(rw_buffer[1] == 1) gdk_vibration = true;
+  else gdk_vibration = false;
 }
 
 uint8_t gdk101_get_status() {
   //0 = Ready, 1 = 10min Waiting, 2 = Normal
   gamma_mod_read(READ_STATUS);
   gdk_status = rw_buffer[0];
-  //  return gdk_status;
 }
 
 uint8_t gdk101_get_measuring_time_min() {
   gamma_mod_read(READ_MEASURING_TIME);
   mea_time_min = rw_buffer[0];
-  //  return mea_time_min;
 }
 
 uint8_t gdk101_get_measuring_time_sec() {
   gamma_mod_read(READ_MEASURING_TIME);
   mea_time_sec = rw_buffer[1];
-  //  return mea_time_sec;
 }
-
-void gamma_mod_read_nw(uint8_t reg) {
-
-  uint8_t _adr = I2_ADR_IRT;
-  uint8_t data = 0;
-  
-  I2cWrite8(_adr, data, reg);
-  delay(5);
-  rw_buffer[0] =  I2cRead8(_adr, reg);
-  rw_buffer[1] =  I2cRead8(_adr, reg);
-
-  //Serial.print(">>> buffer ");
-  //Serial.print(rw_buffer[0]);
-  //Serial.println(rw_buffer[1]);
- 
-  delay(10);
-  
-};
 
 void gamma_mod_read(uint8_t reg) {
   Wire.begin();
   uint8_t lg = 2;
-  uint8_t _adr = I2_ADR_IRT;
+  uint8_t _adr = USE_GDK101_ADDR;
   Wire.beginTransmission(_adr);
   Wire.write(reg);
   Wire.endTransmission();
-  delay(10);
+  delay(5);
   Wire.requestFrom(_adr, lg);
   uint8_t i = 0;
   while (Wire.available())
@@ -167,43 +137,63 @@ void gamma_mod_read(uint8_t reg) {
    every sec show...
   \*********************************************************************************************/
 
-void gdk101_Every_Second(void) {
+void Every_Second(void) {
   if (!gdk_ready) return;
+  
+  gdk101_Every_Second();
+  
+  if (evr10 == 10) {
+    gdk101_Every_10_Second();
+    evr10 = 1;
+  }
+  else {
+    evr10++;
+  }
 
-      gdk101_update_all();
+}
+
+void gdk101_Every_Second(void) {
+  gdk101_get_vib();
+  gdk101_get_status();
+  if (gdk_vibration) vibr++;
+
+  if (vibr == 3){
+    gdk101_reset();
+    delay(10);
+    vibr = 0;
+  }
+  
+//  gdk101_get_measuring_time_sec();
+}
+
+void gdk101_Every_10_Second(void) {
+  gdk101_get_10min_avg();
+  gdk101_get_1min_avg();
+//  gdk101_get_measuring_time_min();
 }
 
 void gdk101_Show(uint8_t json) {
-  //dtostrfd(t, Settings.flag2.temperature_resolution, temperature);
-
   if (!gdk_ready) return;
-
-  char mea_10min_avg_tstr[16];
-  dtostrfd(mea_10min_avg, gdk_resolution, mea_10min_avg_tstr);
-
-  char mea_1min_avg_tstr[16];
-  dtostrfd(mea_1min_avg, gdk_resolution, mea_1min_avg_tstr);
-
+  
   if (json) {
-    snprintf_P(mqtt_data, sizeof(mqtt_data), PSTR(",\"GDK101\":{\"10MIN_AVG\":%.2f,\"1MIN_AVG\":%.2f,\"MEA_TIME_MIN\":%i,\"MEA_TIME_SEC\":%i,\"STATUS\":%i,\"VIBRATION\":%i}"),
-    mea_10min_avg, mea_1min_avg,mea_time_min,mea_time_sec,gdk_status,vibration);
+    //char mqtt_data[40];
+    snprintf_P(mqtt_data, sizeof(mqtt_data), 
+    PSTR("{\"GDK101\":{\"10MIN_AVG\":%.3f,\"1MIN_AVG\":%.3f,\"STATUS\":%i,\"VIBRATION\":%i}"),
+               mqtt_data, mea_10min_avg, mea_1min_avg, gdk_status, gdk_vibration);
+    //ResponseAppend_P(PSTR("{\"GDK101\":{\"10MIN_AVG\":%.3f,\"1MIN_AVG\":%.3f,\"STATUS\":%i,\"VIBRATION\":%i}"),
+    //           mqtt_data, mea_10min_avg, mea_1min_avg, gdk_status, gdk_vibration);
 
 #ifdef USE_WEBSERVER
   } else {
-    //snprintf_P(mqtt_data, sizeof(mqtt_data),"GDK101 gamma sensor");
-    //snprintf_P(mqtt_data, sizeof(mqtt_data),"10_min_avg:%0.2fuSv/h", mea_10min_avg);
-    //snprintf_P(mqtt_data, sizeof(mqtt_data)," 1 min avg:%0.2fuSv/h",mea_1min_avg); HTTP_MGC_3130_SNS, mqtt_data,
     snprintf_P(mqtt_data, sizeof(mqtt_data),
-    HTTP_GDK101_SNS, mqtt_data, mea_10min_avg, mea_1min_avg, gdk_status, vibration );
+               HTTP_GDK101_SNS, mqtt_data, mea_10min_avg, mea_1min_avg, gdk_status, gdk_vibration );
 
 #endif  // USE_WEBSERVER
   }
 }
-
-
 /*********************************************************************************************\
    Interface
-  \*********************************************************************************************/
+\*********************************************************************************************/
 
 bool Xsns55(byte function)
 {
@@ -215,7 +205,7 @@ bool Xsns55(byte function)
         gdk101_Init();
         break;
       case FUNC_EVERY_SECOND:
-        gdk101_Every_Second();
+        Every_Second();
         break;
       case FUNC_JSON_APPEND:
         gdk101_Show(1);
