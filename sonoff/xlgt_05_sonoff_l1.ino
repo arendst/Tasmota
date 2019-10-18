3bb58b3ea019e4561cdfa3f312a47410e08f607b
Theo Arends <11044339+arendst@users.noreply.github.com>
2019-10-18 12:53:35 +0200

    Disable some logging

--- sonoff/xlgt_05_sonoff_l1.ino
+++ sonoff/xlgt_05_sonoff_l1.ino
@@ -49,7 +49,7 @@ struct SNFL1 {
 
 void SnfL1Send(const char *buffer)
 {
-  AddLog_P2(LOG_LEVEL_DEBUG, PSTR("SL1: Send %s"), buffer);
+//  AddLog_P2(LOG_LEVEL_DEBUG, PSTR("SL1: Send %s"), buffer);
 
   Serial.print(buffer);
   Serial.write(0x1B);
@@ -79,7 +79,7 @@ bool SnfL1SerialInput(void)
     // AT+RESULT="sequence":"1554682835320"
     // AT+UPDATE="sequence":"34906","switch":"on","light_type":1,"colorR":0,"colorG":16,"colorB":0,"bright":6,"mode":1
     // AT+UPDATE="switch":"on","light_type":1,"colorR":255,"colorG":0,"colorB":0,"bright":6,"mode":1,"speed":100,"sensitive":10
-    AddLog_P2(LOG_LEVEL_DEBUG, PSTR("SL1: Rcvd %s"), serial_in_buffer);
+//    AddLog_P2(LOG_LEVEL_DEBUG, PSTR("SL1: Rcvd %s"), serial_in_buffer);
 
     if (!strncmp(serial_in_buffer +3, "RESULT", 6)) {
       Snfl1.receive_ready = true;
@@ -107,7 +107,7 @@ bool SnfL1SerialInput(void)
 
         if (!strncmp(token2, "\"sequence\"", 10)) {
 
-          AddLog_P2(LOG_LEVEL_DEBUG, PSTR("SL1: Rcvd sequence %s"), token3);
+//          AddLog_P2(LOG_LEVEL_DEBUG, PSTR("SL1: Rcvd sequence %s"), token3);
 
           token = nullptr;
         }
@@ -115,7 +115,7 @@ bool SnfL1SerialInput(void)
         else if (!strncmp(token2, "\"switch\"", 8)) {
           switch_state = !strncmp(token3, "\"on\"", 4) ? true : false;
 
-          AddLog_P2(LOG_LEVEL_DEBUG, PSTR("SL1: Rcvd switch %d (%d)"), switch_state, Light.power);
+//          AddLog_P2(LOG_LEVEL_DEBUG, PSTR("SL1: Rcvd switch %d (%d)"), switch_state, Light.power);
 
           is_power_change = (switch_state != Light.power);
         }
@@ -139,9 +139,9 @@ bool SnfL1SerialInput(void)
           bool all_color_channels_updated = color_updated[0] && color_updated[1] && color_updated[2];
           if (all_color_channels_updated) {
 
-            AddLog_P2(LOG_LEVEL_DEBUG, PSTR("SL1: Rcvd color R%d G%d B%d (R%d G%d B%d)"),
-              current_color[0], current_color[1], current_color[2],
-              Settings.light_color[0], Settings.light_color[1], Settings.light_color[2]);
+//            AddLog_P2(LOG_LEVEL_DEBUG, PSTR("SL1: Rcvd color R%d G%d B%d (R%d G%d B%d)"),
+//              current_color[0], current_color[1], current_color[2],
+//              Settings.light_color[0], Settings.light_color[1], Settings.light_color[2]);
 
             is_color_change = (Light.power && (memcmp(current_color, Settings.light_color, 3) != 0));
           }
@@ -151,7 +151,7 @@ bool SnfL1SerialInput(void)
         else if (!strncmp(token2, "\"bright\"", 8)) {
           uint8_t dimmer = atoi(token3);
 
-          AddLog_P2(LOG_LEVEL_DEBUG, PSTR("SL1: Rcvd dimmer %d (%d)"), dimmer, Settings.light_dimmer);
+//          AddLog_P2(LOG_LEVEL_DEBUG, PSTR("SL1: Rcvd dimmer %d (%d)"), dimmer, Settings.light_dimmer);
 
           is_brightness_change = (Light.power && (dimmer > 0) && (dimmer != Settings.light_dimmer));
           snprintf_P(cmnd_dimmer, sizeof(cmnd_dimmer), PSTR(D_CMND_DIMMER " %d"), dimmer);
