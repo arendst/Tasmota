ae216b99292e03b776655cb51b86b818ea4aee25
Adrian Scillato <35405447+ascillato@users.noreply.github.com>
2019-10-29 09:59:56 -0300

    PT-BR language update

--- tasmota/language/pt-BR.h
+++ tasmota/language/pt-BR.h
@@ -28,10 +28,10 @@
  * Use online command StateText to translate ON, OFF, HOLD and TOGGLE.
  * Use online command Prefix to translate cmnd, stat and tele.
  *
- * Updated until v5.14.0b
+ * Updated until v7.0.0.1
 \*********************************************************************/
 
-//#define LANGUAGE_MODULE_NAME         // Enable to display "Module Generic" (ie Spanish), Disable to display "Generic Module" (ie English)
+#define LANGUAGE_MODULE_NAME         // Enable to display "Module Generic" (ie Spanish), Disable to display "Generic Module" (ie English)
 
 #define LANGUAGE_LCID 1046
 // HTML (ISO 639-1) Language Code
