62c0c89cfb341bbe84d5300400124c2e3b33f9e7
Theo Arends <11044339+arendst@users.noreply.github.com>
2019-11-04 12:23:20 +0100

    Add user define MQTT_CLEAN_SESSION
    Add user define MQTT_CLEAN_SESSION (#6816)

--- tasmota/my_user_config.h
+++ tasmota/my_user_config.h
@@ -265,6 +265,7 @@
 
 // -- MQTT ----------------------------------------
 #define MQTT_TELE_RETAIN     0                   // Tele messages may send retain flag (0 = off, 1 = on)
+#define MQTT_CLEAN_SESSION   1                   // Mqtt clean session connection (0 = No clean session, 1 = Clean session (default))
 
 // -- MQTT - Domoticz -----------------------------
 #define USE_DOMOTICZ                             // Enable Domoticz (+6k code, +0.3k mem)
