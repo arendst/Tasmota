2eb00e519314439ed40d8e22d074eb7dc1be6749
Jason2866 <obermeier.johann@googlemail.com>
2019-11-16 12:55:38 +0100

    Change firmware.bin from rename to copy
    Both files are there so Platformio flashing does work

--- pio/rename-firmware.py
+++ pio/rename-firmware.py
@@ -1,5 +1,6 @@
 Import('env')
 import os
+import shutil
 
 def obj_ren_after_bin(source, target, env):
     #    print("Rename firmware.bin")
@@ -7,6 +8,7 @@ def obj_ren_after_bin(source, target, env):
     new_file = "{}{}{}.bin".format(base_dir, os.path.sep, str(target[0]).split(os.path.sep)[1])
     if os.path.isfile(new_file):
         os.remove(new_file)
-    os.rename(str(target[0]), new_file)
+    # os.rename(str(target[0]), new_file)
+    shutil.copyfile(str(target[0]), new_file)
 
-env.AddPostAction("$BUILD_DIR/${PROGNAME}.bin", [obj_ren_after_bin]) 
+env.AddPostAction("$BUILD_DIR/${PROGNAME}.bin", [obj_ren_after_bin])
