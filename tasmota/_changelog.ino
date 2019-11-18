cc6f2f7896ef517791edd354aa38ce4122c74f0a
Theo Arends <11044339+arendst@users.noreply.github.com>
2019-11-18 11:21:48 +0100

    Bump version 7.0.0.5

--- tasmota/_changelog.ino
+++ tasmota/_changelog.ino
@@ -1,4 +1,7 @@
 /*********************************************************************************************\
+ * 7.0.0.5 20191118
+ * Fix boot loop regression
+ *
  * 7.0.0.4 20191108
  * Add command WifiPower 0 .. 20.5 to set Wifi Output Power which will be default set to 17dBm
  * Change supported PCF8574 I2C address range to 0x20 - 0x26 allowing other I2C devices with address 0x27 to be used at the same time
