2190cf4e2c9a7313fc0e7978e2091ea4dec2ac97
Jason2866 <obermeier.johann@googlemail.com>
2019-12-03 11:05:44 +0100

    Update link in footer...
    to Tasmota.com.  Saves incredible 8 bytes :-)

--- tasmota/xdrv_01_webserver.ino
+++ tasmota/xdrv_01_webserver.ino
@@ -476,7 +476,7 @@ const char HTTP_COUNTER[] PROGMEM =
   "<br><div id='t' style='text-align:center;'></div>";
 
 const char HTTP_END[] PROGMEM =
-  "<div style='text-align:right;font-size:11px;'><hr/><a href='https://bit.ly/tasmota' target='_blank' style='color:#aaa;'>Tasmota %s " D_BY " Theo Arends</a></div>"
+  "<div style='text-align:right;font-size:11px;'><hr/><a href='//tasmota.com' target='_blank' style='color:#aaa;'>Tasmota %s " D_BY " Theo Arends</a></div>"
   "</div>"
   "</body>"
   "</html>";
