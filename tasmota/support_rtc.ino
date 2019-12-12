666cc86c9eed2dd827af9667b924c49aa1c03980
Adrian Scillato <35405447+ascillato@users.noreply.github.com>
2019-12-12 16:32:54 -0300

    Allow to disable SNTP when using TIME <epoch>

--- tasmota/support_rtc.ino
+++ tasmota/support_rtc.ino
@@ -460,7 +460,9 @@ void RtcSetTime(uint32_t epoch)
   if (epoch < START_VALID_TIME) {  // 2016-01-01
     Rtc.user_time_entry = false;
     ntp_force_sync = true;
+    sntp_init();
   } else {
+    sntp_stop();
     Rtc.user_time_entry = true;
     Rtc.utc_time = epoch -1;    // Will be corrected by RtcSecond
   }
