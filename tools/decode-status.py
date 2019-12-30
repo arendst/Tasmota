be29e71c96ce010706e508ff8590ef968bcff0ba
Theo Arends <11044339+arendst@users.noreply.github.com>
2019-12-30 14:24:00 +0100

    Update decode-status.py

--- tools/decode-status.py
+++ tools/decode-status.py
@@ -132,7 +132,7 @@ a_setoption = [[
     "Enable incrementing bootcount when deepsleep is enabled",
     "Do not power off if slider moved to far left",
     "Bypass Compatibility check",
-    "",
+    "Enable resetting of counters after telemetry was sent",
     "Enable shutter support",
     "Invert PCF8574 ports"
     ],[
@@ -188,7 +188,7 @@ a_features = [[
     "USE_SHUTTER","USE_PCF8574","USE_DDSU666","USE_DEEPSLEEP",
     "USE_SONOFF_SC","USE_SONOFF_RF","USE_SONOFF_L1","USE_EXS_DIMMER",
     "USE_ARDUINO_SLAVE","USE_HIH6","USE_HPMA","USE_TSL2591",
-    "USE_DHT12","USE_DS1624","USE_GPS","",
+    "USE_DHT12","USE_DS1624","USE_GPS","USE_HOTPLUG",
     "","","","",
     "","","",""
     ]]
