21ea26e132e48da9f2e70697fb9aa30a7395ace5
Theo Arends <11044339+arendst@users.noreply.github.com>
2020-01-02 09:54:58 +0100

    Update my_user_config.h

--- tasmota/my_user_config.h
+++ tasmota/my_user_config.h
@@ -439,7 +439,7 @@
 #endif  // USE_I2C
 
 // -- SPI sensors ---------------------------------
-#define USE_SPI                                  // Hardware SPI using GPIO12(MISO), GPIO13(MOSI) and GPIO14(CLK) in addition to two user selectable GPIOs(CS and DC)
+//#define USE_SPI                                  // Hardware SPI using GPIO12(MISO), GPIO13(MOSI) and GPIO14(CLK) in addition to two user selectable GPIOs(CS and DC)
 
 #ifdef USE_SPI
   // #ifndef USE_DISPLAY
