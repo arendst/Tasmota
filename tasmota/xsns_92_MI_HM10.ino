07df4c1dfb4f1718cc3bea655e1f6ce4e137a2d2
Staars <Baars@gmx.de>
2020-01-30 08:39:28 +0100

    start hm-10-driver

---
+++ tasmota/xsns_92_MI_HM10.ino
@@ -0,0 +1,29 @@
+/*
+  xsns_92_Ml_BLE.ino - MI-BLE-sensors via HM-10 support for Tasmota
+
+  Copyright (C) 2020  Christian Baars and Theo Arends
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+  --------------------------------------------------------------------------------------------
+  Version yyyymmdd  Action    Description
+  --------------------------------------------------------------------------------------------
+
+  ---
+  0.9.0.0 20191127  started - further development by Christian Baars
+                    base    - no real base project
+                    forked  - from arendst/tasmota            - https://github.com/arendst/Tasmota
+
+*/
