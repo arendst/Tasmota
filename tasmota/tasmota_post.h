7049c33b40a9dd3b3c7b71130056d1bc9a9b9afd
Ian King <Ian.King@imgtec.com>
2020-03-18 10:58:19 +0000

    Disable ELECTRIQ_MOODL by default as it's a niche product

--- tasmota/tasmota_post.h
+++ tasmota/tasmota_post.h
@@ -134,7 +134,7 @@ extern "C" void custom_crash_callback(struct rst_info * rst_info, uint32_t stack
 #define USE_SM16716                              // Add support for SM16716 RGB LED controller (+0k7 code)
 #define USE_SM2135                               // Add support for SM2135 RGBCW led control as used in Action LSC (+0k6 code)
 #define USE_SONOFF_L1                            // Add support for Sonoff L1 led control
-#define USE_ELECTRIQ_MOODL                       // Add support for ElectriQ iQ-wifiMOODL RGBW LED controller
+//#define USE_ELECTRIQ_MOODL                       // Add support for ElectriQ iQ-wifiMOODL RGBW LED controller
 
 #define USE_COUNTER                              // Enable counters
 #undef USE_ADC_VCC                               // Add Analog input on selected devices
