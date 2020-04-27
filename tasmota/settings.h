bdb5d92d85e92a7468e3cb5c5e3b7fedc314ccc4
Matteo Albinola <malbinola@sonicle.com>
2020-04-27 09:38:39 +0200

    Add variable for tele updates

--- tasmota/settings.h
+++ tasmota/settings.h
@@ -529,8 +529,9 @@ struct {
   uint16_t      windmeter_radius;          // F38
   uint16_t      windmeter_pulse_debounce;  // F3A
   int16_t       windmeter_speed_factor;    // F3C
+  uint8_t       windmeter_tele_on_change;  // F3E
 
-  uint8_t       free_f3e[122];             // F3E - Decrement if adding new Setting variables just above and below
+  uint8_t       free_f3f[121];             // F3F - Decrement if adding new Setting variables just above and below
 
   // Only 32 bit boundary variables below
   uint16_t      pulse_counter_debounce_low;  // FB8
