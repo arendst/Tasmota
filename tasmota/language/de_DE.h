049ddad17bc5e024f232504c99234895933f6b60
Yury Sannikov <yury.sannikov@gmail.com>
2020-05-07 11:54:29 +0300

    update languages

--- tasmota/language/de_DE.h
+++ tasmota/language/de_DE.h
@@ -793,4 +793,8 @@
 #define D_AS3935_CAL_FAIL "Kalibrierung fehlerhaft"
 #define D_AS3935_CAL_OK "Cap gesetzt auf:"
 
+//xsns_68_opentherm.ino
+#define D_SENSOR_BOILER_OT_RX   "OpenTherm RX"
+#define D_SENSOR_BOILER_OT_TX   "OpenTherm TX"
+
 #endif  // _LANGUAGE_DE_DE_H_
--- tasmota/language/it_IT.h
+++ tasmota/language/it_IT.h
@@ -793,4 +793,8 @@
 #define D_AS3935_CAL_FAIL "calibrazione fallita"
 #define D_AS3935_CAL_OK "calibrazione impostata a:"
 
+//xsns_68_opentherm.ino
+#define D_SENSOR_BOILER_OT_RX   "OpenTherm RX"
+#define D_SENSOR_BOILER_OT_TX   "OpenTherm TX"
+
 #endif  // _LANGUAGE_IT_IT_H_
