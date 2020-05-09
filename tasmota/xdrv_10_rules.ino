cc2f7cbf5a15c5a9bca4d4a1197bcb20300d0374
Theo Arends <11044339+arendst@users.noreply.github.com>
2020-05-09 19:35:16 +0200

    Oops remove debug message

--- tasmota/xdrv_10_rules.ino
+++ tasmota/xdrv_10_rules.ino
@@ -491,7 +491,7 @@ bool RulesRuleMatch(uint8_t rule_set, String &event, String &rule)
     rule_name = rule_name.substring(0, pos);           // "SUBTYPE1#CURRENT"
   }
 
-AddLog_P2(LOG_LEVEL_DEBUG, PSTR("RUL: Find Task %s, Name %s"), rule_task.c_str(), rule_name.c_str());
+//AddLog_P2(LOG_LEVEL_DEBUG, PSTR("RUL: Find Task %s, Name %s"), rule_task.c_str(), rule_name.c_str());
 
   StaticJsonBuffer<1024> jsonBuf;
   JsonObject &root = jsonBuf.parseObject(event);
