7bfbe95d0fd5e9f6c9a70932a8e6feca71b6f881
Theo Arends <11044339+arendst@users.noreply.github.com>
2020-05-26 12:15:20 +0200

    Update tasmota.h

--- tasmota/tasmota.h
+++ tasmota/tasmota.h
@@ -124,7 +124,7 @@ const uint16_t SYSLOG_TIMER = 600;          // Seconds to restore syslog_level
 const uint16_t SERIALLOG_TIMER = 600;       // Seconds to disable SerialLog
 const uint8_t OTA_ATTEMPTS = 5;             // Number of times to try fetching the new firmware
 
-const uint16_t INPUT_BUFFER_SIZE = 510;     // Max number of characters in serial command buffer: floor((MIN_MESSZ - len({"SerialReceived":""})) / 2) + 1
+const uint16_t INPUT_BUFFER_SIZE = 520;     // Max number of characters in serial command buffer
 const uint16_t FLOATSZ = 16;                // Max number of characters in float result from dtostrfd (max 32)
 const uint16_t CMDSZ = 24;                  // Max number of characters in command
 const uint16_t TOPSZ = 151;                 // Max number of characters in topic string
