26288c0abced6f743f86f102788d3bf1ea3ffcfd
pawel.koselski <pawel753@gmail.com>
2020-06-07 17:35:05 +0200

    Issue #8637 - followup

--- tasmota/xsns_45_vl53l0x.ino
+++ tasmota/xsns_45_vl53l0x.ino
@@ -102,13 +102,13 @@ void Vl53l0Show(boolean json)
 {
   if (json) {
     ResponseAppend_P(PSTR(",\"VL53L0X\":{\"" D_JSON_DISTANCE "\":%d}"), vl53l0x_distance);
+  } else {
 #ifdef USE_DOMOTICZ
         if (0 == tele_period) {
           DomoticzSensor(DZ_ILLUMINANCE, vl53l0x_distance);
         }
 #endif  // USE_DOMOTICZ
 #ifdef USE_WEBSERVER
-  } else {
     WSContentSend_PD(HTTP_SNS_VL53L0X, vl53l0x_distance);
 #endif
   }
