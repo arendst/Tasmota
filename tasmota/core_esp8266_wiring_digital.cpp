5322238d565028ec7f7f3a659c01100e6d646b63
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2020-06-23 20:02:55 +0200

    Update core_esp8266_wiring_digital.cpp

--- tasmota/core_esp8266_wiring_digital.cpp
+++ tasmota/core_esp8266_wiring_digital.cpp
@@ -18,6 +18,9 @@
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
+
+#ifdef ESP8266
+
 #define ARDUINO_MAIN
 #include "wiring_private.h"
 #include "pins_arduino.h"
@@ -262,3 +265,5 @@ extern void attachInterruptArg(uint8_t pin, voidFuncPtrArg handler, void* arg, i
 extern void detachInterrupt(uint8_t pin) __attribute__ ((weak, alias("__detachInterrupt")));
 
 };
+
+#endif  // ESP8266 
