ff327106be279d51fdb3d1785de05ec22d208348
Theo Arends <11044339+arendst@users.noreply.github.com>
2020-06-24 15:58:56 +0200

    Refactor ESP32 support

--- tasmota/xdrv_01_webserver.ino
+++ tasmota/xdrv_01_webserver.ino
@@ -1728,7 +1728,7 @@ void HandleTemplateConfiguration(void)
       WSContentSend_P(HTTP_MODULE_TEMPLATE_REPLACE_NO_INDEX, AGPIO(GPIO_USER), D_SENSOR_USER);  // }2'255'>User}3
     }
     uint32_t ridx = pgm_read_word(kGpioNiceList + i) & 0xFFE0;
-    uint32_t midx = ridx >> 5;
+    uint32_t midx = BGPIO(ridx);
     WSContentSend_P(HTTP_MODULE_TEMPLATE_REPLACE_NO_INDEX, ridx, GetTextIndexed(stemp, sizeof(stemp), midx, kSensorNames));
 #endif  // ESP8266 - ESP32
   }
@@ -1905,7 +1905,7 @@ void HandleModuleConfiguration(void)
     }
 #else  // ESP32
     uint32_t ridx = pgm_read_word(kGpioNiceList + i) & 0xFFE0;
-    midx = ridx >> 5;
+    midx = BGPIO(ridx);
     if (!GetUsedInModule(midx, cmodule.io)) {
       WSContentSend_P(HTTP_MODULE_TEMPLATE_REPLACE_NO_INDEX, ridx, GetTextIndexed(stemp, sizeof(stemp), midx, kSensorNames));
     }
