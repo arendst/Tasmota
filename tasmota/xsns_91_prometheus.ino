b802d34c42b672fbd9ae9b46803db6fce613ae5c
Mark Hansen <markhansen@google.com>
2020-06-27 12:04:32 +1000

    Move #define USE_PROMETHEUS to my_user_config.h
    This is where all the other #defines live.
    
    This should make it easier to find the #define for enabling prometheus.

--- tasmota/xsns_91_prometheus.ino
+++ tasmota/xsns_91_prometheus.ino
@@ -17,8 +17,6 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-//#define USE_PROMETHEUS                         // Enable retrieval of metrics
-
 #ifdef USE_PROMETHEUS
 /*********************************************************************************************\
  * Prometheus support
