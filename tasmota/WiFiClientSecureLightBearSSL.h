66b3dc1cf2109ea8fa1bb9a5f6d2e53e12f90e97
Theo Arends <11044339+arendst@users.noreply.github.com>
2020-07-16 16:46:30 +0200

    Increase TLS fingerprint security

--- tasmota/WiFiClientSecureLightBearSSL.h
+++ tasmota/WiFiClientSecureLightBearSSL.h
@@ -121,7 +121,12 @@ class WiFiClientSecure_light : public WiFiClient {
     bool _fingerprint_any;           // accept all fingerprints
     const uint8_t *_fingerprint1;          // fingerprint1 to be checked against
     const uint8_t *_fingerprint2;          // fingerprint2 to be checked against
+// **** Start patch Castellucci
+/*
     uint8_t _recv_fingerprint[20];   // fingerprint received
+*/
+    uint8_t _recv_fingerprint[21];   // fingerprint received
+// **** End patch Castellucci
 
     unsigned char *_recvapp_buf;
     size_t _recvapp_len;
