5536e03423ab8f3ab3544b2e87e1342b0f87beac
Janusz <janusz.kostorz@gmail.com>
2020-08-16 22:49:16 +0200

    add DYP ME007 ultrasonic distance sensor

--- tasmota/language/he_HE.h
+++ tasmota/language/he_HE.h
@@ -684,6 +684,7 @@
 #define D_SENSOR_CC1101_GDO0   "CC1101 GDO0"
 #define D_SENSOR_CC1101_GDO2   "CC1101 GDO2"
 #define D_SENSOR_HRXL_RX       "HRXL Rx"
+#define D_SENSOR_DYP_RX        "DYP Rx"
 #define D_SENSOR_ELECTRIQ_MOODL "MOODL Tx"
 #define D_SENSOR_AS3935        "AS3935"
 #define D_SENSOR_WINDMETER_SPEED "WindMeter Spd"
