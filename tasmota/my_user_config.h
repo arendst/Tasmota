a59c56d4b560097e6f96a5c749fc90f8ecd77427
hello-world-dot-c <wizard@gno.de>
2020-10-13 12:02:44 +0200

    Reduce sleep during buzzer cycles to improve on/off period accuracy.

--- tasmota/my_user_config.h
+++ tasmota/my_user_config.h
@@ -262,7 +262,7 @@
 
 #define APP_NORMAL_SLEEP       false             // [SetOption60] Enable normal sleep instead of dynamic sleep
 #define APP_SLEEP              0                 // [Sleep] Sleep time to lower energy consumption (0 = Off, 1 - 250 mSec),
-#define PWM_MAX_SLEEP          10                // Sleep will be lowered to this value when light is on, to avoid flickering
+#define PWM_MAX_SLEEP          10                // Sleep will be lowered to this value when light is on, to avoid flickering, and when buzzer is on for better on/off period accuracy
 
 #define KEY_DEBOUNCE_TIME      50                // [ButtonDebounce] Number of mSeconds button press debounce time
 #define KEY_HOLD_TIME          40                // [SetOption32] Number of 0.1 seconds to hold Button or external Pushbutton before sending HOLD message
