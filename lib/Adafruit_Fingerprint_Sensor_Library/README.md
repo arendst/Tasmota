ea752451eea2c0edad3efe972491b06364b499a1
Boaschti <sebastian-weisser@gmx.de>
2020-10-18 12:28:53 +0200

    Add Adafruit Fingerprint library

---
+++ lib/Adafruit_Fingerprint_Sensor_Library/README.md
@@ -0,0 +1,19 @@
+# Adafruit Fingerprint Sensor Library [![Build Status](https://travis-ci.org/adafruit/Adafruit-Fingerprint-Sensor-Library.svg?branch=master)](https://travis-ci.org/adafruit/Adafruit-Fingerprint-Sensor-Library)
+
+<img src="https://cdn-shop.adafruit.com/970x728/751-03.jpg" height="300"/>
+
+Secure your project with biometrics - this all-in-one optical fingerprint sensor will make adding fingerprint detection and verification super simple. These modules are typically used in safes - there's a high powered DSP chip that does the image rendering, calculation, feature-finding and searching. Connect to any microcontroller or system with TTL serial, and send packets of data to take photos, detect prints, hash and search. You can also enroll new fingers directly - up to 162 finger prints can be stored in the onboard FLASH memory. There's a red LED in the lens that lights up during a photo so you know its working.
+
+
+Designed specifically to work with the Adafruit Fingerprint sensor
+  * http://www.adafruit.com/products/751
+
+Pick one up today in the adafruit shop!
+
+These sensors use TTL Serial to communicate, 2 pins are required to interface
+
+Adafruit invests time and resources providing this open source code, please support Adafruit and open-source hardware by purchasing products from Adafruit!
+
+Written by Limor Fried/Ladyada  for Adafruit Industries.  
+BSD license, check license.txt for more information
+All text above must be included in any redistribution
---
+++ lib/Adafruit_Fingerprint_Sensor_Library/examples/show_fingerprint_templates/show_fingerprint_templates.ino
@@ -0,0 +1,156 @@
+/*************************************************** 
+  This is an example sketch for our optical Fingerprint sensor
+
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+
+#include <Adafruit_Fingerprint.h>
+
+int getFingerprintIDez();
+
+// pin #2 is IN from sensor (GREEN wire)
+// pin #3 is OUT from arduino  (WHITE wire)
+SoftwareSerial mySerial(2, 3);
+
+
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+
+void setup()  
+{
+  while(!Serial);
+  Serial.begin(9600);
+  Serial.println("Fingerprint template extractor");
+
+  // set the data rate for the sensor serial port
+  finger.begin(57600);
+  
+  if (finger.verifyPassword()) {
+    Serial.println("Found fingerprint sensor!");
+  } else {
+    Serial.println("Did not find fingerprint sensor :(");
+    while (1);
+  }
+
+  // Try to get the templates for fingers 1 through 10
+  for (int finger = 1; finger < 10; finger++) {
+    downloadFingerprintTemplate(finger);
+  }
+}
+
+uint8_t downloadFingerprintTemplate(uint16_t id)
+{
+  Serial.println("------------------------------------");
+  Serial.print("Attempting to load #"); Serial.println(id);
+  uint8_t p = finger.loadModel(id);
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.print("Template "); Serial.print(id); Serial.println(" loaded");
+      break;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    default:
+      Serial.print("Unknown error "); Serial.println(p);
+      return p;
+  }
+
+  // OK success!
+
+  Serial.print("Attempting to get #"); Serial.println(id);
+  p = finger.getModel();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.print("Template "); Serial.print(id); Serial.println(" transferring:");
+      break;
+   default:
+      Serial.print("Unknown error "); Serial.println(p);
+      return p;
+  }
+  
+  // one data packet is 267 bytes. in one data packet, 11 bytes are 'usesless' :D
+  uint8_t bytesReceived[534]; // 2 data packets
+  memset(bytesReceived, 0xff, 534);
+
+  uint32_t starttime = millis();
+  int i = 0;
+  while (i < 534 && (millis() - starttime) < 20000) {
+      if (mySerial.available()) {
+          bytesReceived[i++] = mySerial.read();
+      }
+  }
+  Serial.print(i); Serial.println(" bytes read.");
+  Serial.println("Decoding packet...");
+
+  uint8_t fingerTemplate[512]; // the real template
+  memset(fingerTemplate, 0xff, 512);
+
+  // filtering only the data packets
+  int uindx = 9, index = 0;
+  while (index < 534) {
+      while (index < uindx) ++index;
+      uindx += 256;
+      while (index < uindx) {
+          fingerTemplate[index++] = bytesReceived[index];
+      }
+      uindx += 2;
+      while (index < uindx) ++index;
+      uindx = index + 9;
+  }
+  for (int i = 0; i < 512; ++i) {
+      //Serial.print("0x");
+      printHex(fingerTemplate[i], 2);
+      //Serial.print(", ");
+  }
+  Serial.println("\ndone.");
+
+  /*
+  uint8_t templateBuffer[256];
+  memset(templateBuffer, 0xff, 256);  //zero out template buffer
+  int index=0;
+  uint32_t starttime = millis();
+  while ((index < 256) && ((millis() - starttime) < 1000))
+  {
+    if (mySerial.available())
+    {
+      templateBuffer[index] = mySerial.read();
+      index++;
+    }
+  }
+  
+  Serial.print(index); Serial.println(" bytes read");
+  
+  //dump entire templateBuffer.  This prints out 16 lines of 16 bytes
+  for (int count= 0; count < 16; count++)
+  {
+    for (int i = 0; i < 16; i++)
+    {
+      Serial.print("0x");
+      Serial.print(templateBuffer[count*16+i], HEX);
+      Serial.print(", ");
+    }
+    Serial.println();
+  }*/
+}
+
+
+
+void printHex(int num, int precision) {
+    char tmp[16];
+    char format[128];
+ 
+    sprintf(format, "%%.%dX", precision);
+ 
+    sprintf(tmp, format, num);
+    Serial.print(tmp);
+}
+
+void loop()
+{}
+
+
