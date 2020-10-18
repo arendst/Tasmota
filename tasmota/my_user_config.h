1af2277ff39dfa458d5968dc6952c852000760bc
Boaschti <sebastian-weisser@gmx.de>
2020-10-18 12:30:50 +0200

    set define USE_AS608 in my_user_config

--- tasmota/my_user_config.h
+++ tasmota/my_user_config.h
@@ -608,6 +608,7 @@
 #endif  // USE_SPI
 
 // -- Serial sensors ------------------------------
+#define USE_AS608                                // Add support for AS 608 optical fingerprint sensor
 //#define USE_MHZ19                                // Add support for MH-Z19 CO2 sensor (+2k code)
 //#define USE_SENSEAIR                             // Add support for SenseAir K30, K70 and S8 CO2 sensor (+2k3 code)
   #define CO2_LOW              800               // Below this CO2 value show green light (needs PWM or WS2812 RG(B) led and enable with SetOption18 1)
