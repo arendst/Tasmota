c6b3864d993977c7bb8c23b68be6e664b8b6c990
Stephan Hadinger <stephan.hadinger@gmail.com>
2020-10-29 19:34:46 +0100

    Zigbee display modelId and ManufacturerId in tooltip

--- tasmota/xdrv_23_zigbee_A_impl.ino
+++ tasmota/xdrv_23_zigbee_A_impl.ino
@@ -1715,10 +1715,13 @@ void ZigbeeShow(bool json)
 
       WSContentSend_PD(PSTR(
         "<tr class='ztd htr'>"
-          "<td><b>%s</b></td>" // name
+          "<td><b title='0x%04X %s - %s'>%s</b></td>" // name
           "<td>%s</td>" // sbatt (Battery Indicator)
           "<td><div title='" D_LQI " %s' class='ssi'>" // slqi
-      ), name, sbatt, slqi);
+      ), shortaddr, 
+      device.modelId ? device.modelId : "", 
+      device.manufacturerId ? device.manufacturerId : "", 
+      name, sbatt, slqi);
 
       if(device.validLqi()) {
           for(uint32_t j = 0; j < 4; ++j) {
