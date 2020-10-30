8249071504107291a4acfc981ad9c1496d10e200
Boaschti <sebastian-weisser@gmx.de>
2020-10-30 14:27:40 +0100

    Fix latest code for latest changes on dev branch

--- tasmota/xsns_80_as608.ino
+++ tasmota/xsns_80_as608.ino
@@ -31,12 +31,11 @@ void as608init(){
         if (finger->verifyPassword()){
           finger->getTemplateCount();
           as608selected = true;
-          snprintf_P(log_data, sizeof(log_data), PSTR(D_LOG_LOG "AS60x: found. %i fingerprints stored."), finger->templateCount);
+          AddLog_P2(LOG_LEVEL_INFO, PSTR("AS60x: Detected. %i fingerprints stored."), finger->templateCount);
         }else{
-          snprintf_P(log_data, sizeof(log_data), PSTR(D_LOG_LOG "AS60x: not found! Got data: %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X"), finger->mydata[0], finger->mydata[1], finger->mydata[2], finger->mydata[3], finger->mydata[4], finger->mydata[5], finger->mydata[6], finger->mydata[7], finger->mydata[8], finger->mydata[9], finger->mydata[10], finger->mydata[11]);
+          AddLog_P2(LOG_LEVEL_INFO, PSTR("AS60x: not found! Got data: %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X"), finger->mydata[0], finger->mydata[1], finger->mydata[2], finger->mydata[3], finger->mydata[4], finger->mydata[5], finger->mydata[6], finger->mydata[7], finger->mydata[8], finger->mydata[9], finger->mydata[10], finger->mydata[11]);
+          //snprintf_P(log_data, sizeof(log_data), PSTR(D_LOG_LOG "AS60x: not found! Got data: %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X"), finger->mydata[0], finger->mydata[1], finger->mydata[2], finger->mydata[3], finger->mydata[4], finger->mydata[5], finger->mydata[6], finger->mydata[7], finger->mydata[8], finger->mydata[9], finger->mydata[10], finger->mydata[11]);
         }
-        AddLog(LOG_LEVEL_INFO);
-        mqtt_data[0] = '\0';
     }
 
 }
@@ -57,23 +56,23 @@ int deleteFingerprint(uint8_t id) {
 
   if (p == FINGERPRINT_OK) {
     AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: Delete ok"));
-    mqtt_data[0] = '\0';
+    TasmotaGlobal.mqtt_data[0] = '\0';
     return true;
   } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
     AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: Comm error"));
-    mqtt_data[0] = '\0';
+    TasmotaGlobal.mqtt_data[0] = '\0';
     return false;
   } else if (p == FINGERPRINT_BADLOCATION) {
     AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: Could not delete in that location"));
-    mqtt_data[0] = '\0';
+    TasmotaGlobal.mqtt_data[0] = '\0';
     return false;
   } else if (p == FINGERPRINT_FLASHERR) {
     AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: Error writing to flash"));
-    mqtt_data[0] = '\0';
+    TasmotaGlobal.mqtt_data[0] = '\0';
     return false;
   } else {
     AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: any error"));
-    mqtt_data[0] = '\0';
+    TasmotaGlobal.mqtt_data[0] = '\0';
     return false;
   }
 }
@@ -83,21 +82,21 @@ int getFingerImage(){
   switch (p) {
     case FINGERPRINT_OK:
       AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: Image taken"));
-      mqtt_data[0] = '\0';
+      TasmotaGlobal.mqtt_data[0] = '\0';
       break;
     case FINGERPRINT_NOFINGER:
       break;
     case FINGERPRINT_PACKETRECIEVEERR:
       AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: Comm error"));
-      mqtt_data[0] = '\0';
+      TasmotaGlobal.mqtt_data[0] = '\0';
       break;
     case FINGERPRINT_IMAGEFAIL:
       AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: Imaging error"));
-      mqtt_data[0] = '\0';
+      TasmotaGlobal.mqtt_data[0] = '\0';
       break;
     default:
       AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: any error"));
-      mqtt_data[0] = '\0';
+      TasmotaGlobal.mqtt_data[0] = '\0';
       break;
     }
 
@@ -110,27 +109,27 @@ int convertFingerImage(uint8_t slot){
     switch (p) {
       case FINGERPRINT_OK:
         AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: Image converted"));
-        mqtt_data[0] = '\0';
+        TasmotaGlobal.mqtt_data[0] = '\0';
         break;
       case FINGERPRINT_IMAGEMESS:
         AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: Image too messy"));
-        mqtt_data[0] = '\0';
+        TasmotaGlobal.mqtt_data[0] = '\0';
         break;
       case FINGERPRINT_PACKETRECIEVEERR:
         AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: Comm error"));
-        mqtt_data[0] = '\0';
+        TasmotaGlobal.mqtt_data[0] = '\0';
         break;
       case FINGERPRINT_FEATUREFAIL:
         AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: Could not find fingerprint features"));
-        mqtt_data[0] = '\0';
+        TasmotaGlobal.mqtt_data[0] = '\0';
         break;
       case FINGERPRINT_INVALIDIMAGE:
         AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: Image invalid"));
-        mqtt_data[0] = '\0';
+        TasmotaGlobal.mqtt_data[0] = '\0';
         break;
       default:
         AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: any error"));
-        mqtt_data[0] = '\0';
+        TasmotaGlobal.mqtt_data[0] = '\0';
         break;
     }
 
@@ -155,23 +154,22 @@ void as608Main(){
         p = finger->fingerFastSearch();
         if (p != FINGERPRINT_OK){
             AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: no matching finger!"));
-            mqtt_data[0] = '\0';
+            TasmotaGlobal.mqtt_data[0] = '\0';
             return;
         }
 
         // found a match!
-        snprintf_P(log_data, sizeof(log_data), PSTR(D_LOG_LOG "AS60x: Finger #%i found with confidence of %i"), finger->fingerID, finger->confidence);
-        AddLog(LOG_LEVEL_INFO);
-        mqtt_data[0] = '\0';
-        snprintf_P(mqtt_data, sizeof(mqtt_data), PSTR("{\"AS680_Finger\":%i, AS608_Confidence\":%i}"), finger->fingerID, finger->confidence);
-        MqttPublishPrefixTopic_P(RESULT_OR_STAT, mqtt_data);
+        AddLog_P2(LOG_LEVEL_INFO, PSTR("AS60x: Finger #%i found with confidence of %i"), finger->fingerID, finger->confidence);
+        TasmotaGlobal.mqtt_data[0] = '\0';
+        snprintf_P(TasmotaGlobal.mqtt_data, sizeof(TasmotaGlobal.mqtt_data), PSTR("{\"AS680_Finger\":%i, AS608_Confidence\":%i}"), finger->fingerID, finger->confidence);
+        MqttPublishPrefixTopic_P(RESULT_OR_STAT, TasmotaGlobal.mqtt_data);
         return;
     }else{
         // enroll is active
         switch (enrollstep){
         case 1:
             AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: place finger and wait"));
-            mqtt_data[0] = '\0';
+            TasmotaGlobal.mqtt_data[0] = '\0';
             enrollstep++;
             break;
         case 2:
@@ -192,7 +190,7 @@ void as608Main(){
             break;
         case 4:
             AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: remove finger and wait"));
-            mqtt_data[0] = '\0';
+            TasmotaGlobal.mqtt_data[0] = '\0';
             enrollstep++;
             break;
         case 5:
@@ -204,7 +202,7 @@ void as608Main(){
             break;
         case 6:
             AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: place same finger again and wait"));
-            mqtt_data[0] = '\0';
+            TasmotaGlobal.mqtt_data[0] = '\0';
             enrollstep++;
             break;
         case 7:
@@ -217,30 +215,30 @@ void as608Main(){
         // convert second image
             if(convertFingerImage(2) != FINGERPRINT_OK)  {
                 AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: Not Ok. Try again."));
-                mqtt_data[0] = '\0';
+                TasmotaGlobal.mqtt_data[0] = '\0';
                 enrollstep -= 2;
             }
 
         // create modell
 
             AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: creating model"));
-            mqtt_data[0] = '\0';
+            TasmotaGlobal.mqtt_data[0] = '\0';
 
             p = finger->createModel();
             if (p == FINGERPRINT_OK) {
               AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: Prints matched"));
-              mqtt_data[0] = '\0';
+              TasmotaGlobal.mqtt_data[0] = '\0';
             } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
               AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: Communication error"));
-              mqtt_data[0] = '\0';
+              TasmotaGlobal.mqtt_data[0] = '\0';
               enrollstep = 99;
             } else if (p == FINGERPRINT_ENROLLMISMATCH) {
               AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: Fingerprints did not match"));
-              mqtt_data[0] = '\0';
+              TasmotaGlobal.mqtt_data[0] = '\0';
               enrollstep = 99;
             } else {
               AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: any error"));
-              mqtt_data[0] = '\0';
+              TasmotaGlobal.mqtt_data[0] = '\0';
               enrollstep = 99;
             }
 
@@ -249,37 +247,37 @@ void as608Main(){
             p = finger->storeModel(ModellNumber);
             if (p == FINGERPRINT_OK) {
               AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: Stored!"));
-              mqtt_data[0] = '\0';
+              TasmotaGlobal.mqtt_data[0] = '\0';
               enrollstep = 0;
               ModellNumber = 0;
             } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
               AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: Communication error"));
-              mqtt_data[0] = '\0';
+              TasmotaGlobal.mqtt_data[0] = '\0';
               enrollstep = 99;
             } else if (p == FINGERPRINT_BADLOCATION) {
               AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: Could not store in that location"));
-              mqtt_data[0] = '\0';
+              TasmotaGlobal.mqtt_data[0] = '\0';
               enrollstep = 99;
             } else if (p == FINGERPRINT_FLASHERR) {
               AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: Error writing to flash"));
-              mqtt_data[0] = '\0';
+              TasmotaGlobal.mqtt_data[0] = '\0';
               enrollstep = 99;
             } else {
               AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: any error"));
-              mqtt_data[0] = '\0';
+              TasmotaGlobal.mqtt_data[0] = '\0';
               enrollstep = 99;
             }
             break;
         case 99:
             enrollstep = 1;
             AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: enroll starts again!"));
-            mqtt_data[0] = '\0';
+            TasmotaGlobal.mqtt_data[0] = '\0';
             break;
         default:
             enrollstep = 0;
             ModellNumber = 0;
             AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: oops sth went wrong"));
-            mqtt_data[0] = '\0';
+            TasmotaGlobal.mqtt_data[0] = '\0';
             break;
         }
     }
@@ -300,14 +298,13 @@ bool as608Command(void){
         if (!strcmp(subStr(sub_string, XdrvMailbox.data, ",", 1),"enroll")) {
           if (!enrollstep){
                 uint8_t ModellNumber = atoi(subStr(sub_string, XdrvMailbox.data, ",", 2));  // Note 2 used for param number
-                snprintf_P(log_data, sizeof(log_data), PSTR(D_LOG_LOG "AS60x: Enroll called #%i"), ModellNumber);
-                AddLog(LOG_LEVEL_INFO);
-                mqtt_data[0] = '\0';
+                AddLog_P2(LOG_LEVEL_INFO, PSTR("AS60x: Enroll called #%i"), ModellNumber);
+                TasmotaGlobal.mqtt_data[0] = '\0';
                 as608Enroll(ModellNumber);
                 return true;
           }else{
                 AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: Enroll active! Cancel with: enrollReset"));
-                mqtt_data[0] = '\0';
+                TasmotaGlobal.mqtt_data[0] = '\0';
                 return true;
           }
         }
@@ -316,7 +313,7 @@ bool as608Command(void){
         if (!strcmp(subStr(sub_string, XdrvMailbox.data, ",", 1),"enrollReset")) { // Note 1 used for param number
           enrollstep = 0;
           AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: enrollReset Ok!"));
-          mqtt_data[0] = '\0';
+          TasmotaGlobal.mqtt_data[0] = '\0';
           return true;
         }
 
@@ -332,11 +329,11 @@ bool as608Command(void){
           finger->getTemplateCount();
           if(!finger->templateCount){
               AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: Empty database Ok!"));
-              mqtt_data[0] = '\0';
+              TasmotaGlobal.mqtt_data[0] = '\0';
               return true;
           }else{
               AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: Empty database Error!"));
-              mqtt_data[0] = '\0';
+              TasmotaGlobal.mqtt_data[0] = '\0';
               return true;
           }
         }
@@ -344,20 +341,20 @@ bool as608Command(void){
         // get number of Fingerprints
         if (!strcmp(subStr(sub_string, XdrvMailbox.data, ",", 1),"getNumber")) { // Note 1 used for param number
           finger->getTemplateCount();
-          snprintf_P(log_data, sizeof(log_data), PSTR(D_LOG_LOG "AS60x: %i fingerprints stored."), finger->templateCount);
-          AddLog(LOG_LEVEL_INFO);
-          mqtt_data[0] = '\0';
+          AddLog_P2(LOG_LEVEL_INFO, PSTR("AS60x: %i fingerprints stored."), finger->templateCount);
+
+          TasmotaGlobal.mqtt_data[0] = '\0';
           return true;
         }
 
         if (!strcmp(subStr(sub_string, XdrvMailbox.data, ",", 1),"help")) { // Note 1 used for param number
           AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x Commands: enroll x, enrollReset, delete x, deleteAll, getNumber"));
-          mqtt_data[0] = '\0';
+          TasmotaGlobal.mqtt_data[0] = '\0';
           return true;
         }
 
         AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_LOG "AS60x: Command excepted. But not supported!"));
-        mqtt_data[0] = '\0';
+        TasmotaGlobal.mqtt_data[0] = '\0';
         return true;
     }
 
