1694087a288d8e23e0a42da17e418c0bad251927
Barbudor <barbudor@barbudor.net>
2020-11-17 22:01:42 +0100

    Rename pio folder to pio-tools solve linux build errors
    When vscode is ran from the Tasmota folder in Linux, build fails with message :
    ```Executing task: pio run <
        The terminal process failed to launch: Path to shell executable "pio" is not a file of a symlink.```
    Renaming pio folder into pio-tools solve the problem.
    
    Note: the problem can still occur if the user have a pio folder in his PATH...

--- pio-tools/http-uploader.py
+++ pio-tools/http-uploader.py
@@ -3,11 +3,11 @@ import os
 
 # pio < 4.0.0
 # from base64 import b64decode
-# env.Replace(UPLOADER="pio\espupload.py")
+# env.Replace(UPLOADER="pio-tools\espupload.py")
 # env.Replace(UPLOADERFLAGS="")
 # env.Replace(UPLOADCMD="$UPLOADER -u " + b64decode(ARGUMENTS.get("UPLOAD_PORT")) + " -f $SOURCES")
 
 # pio >= 4.0.0
-env.Replace(UPLOADER=os.path.join("pio", "espupload.py"))
+env.Replace(UPLOADER=os.path.join("pio-tools", "espupload.py"))
 env.Replace(UPLOADERFLAGS="")
 env.Replace(UPLOADCMD="$UPLOADER -u $UPLOAD_PORT -f $SOURCES")
