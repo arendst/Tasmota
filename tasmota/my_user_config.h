1f400d34a941b65626dd96569bab91350938a5e2
Stephan Hadinger <stephan.hadinger@gmail.com>
2020-12-01 10:32:14 +0100

    Make it stealth mode

--- tasmota/my_user_config.h
+++ tasmota/my_user_config.h
@@ -45,7 +45,6 @@
 #define CFG_HOLDER             4617              // [Reset 1] Change this value (max 32000) to load SECTION1 configuration parameters to flash
                                                  // If following define is disabled it increases configuration corruption detection BUT
                                                  //  it only allows firmware upgrades starting from version 6.6.0.11
-// #define UPGRADE_V8_MIN                           // do not support upgrading from version below 8.0.0
 
 // -- Project -------------------------------------
 #define PROJECT                "tasmota"         // PROJECT is used as the default topic delimiter
