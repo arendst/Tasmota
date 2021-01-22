465ace923b6d56110bf5569aa2702af0022b9613
Theo Arends <11044339+arendst@users.noreply.github.com>
2021-01-22 18:24:26 +0100

    Update xsns_22_sr04.ino
    Fix compilation

--- tasmota/xsns_22_sr04.ino
+++ tasmota/xsns_22_sr04.ino
@@ -156,7 +156,7 @@ void Sr04TReading(void) {
 }
 
 #ifdef USE_WEBSERVER
-const char HTTP_SNS_DISTANCE[] PROGMEM =
+const char HTTP_SNS_DISTANCE_CM[] PROGMEM =
   "{s}SR04 " D_DISTANCE "{m}%s" D_UNIT_CENTIMETER "{e}";  // {s} = <tr><th>, {m} = </th><td>, {e} = </td></tr>
 #endif  // USE_WEBSERVER
 
@@ -176,7 +176,7 @@ void Sr04Show(bool json)
 #endif  // USE_DOMOTICZ
 #ifdef USE_WEBSERVER
     } else {
-      WSContentSend_PD(HTTP_SNS_DISTANCE, distance_chr);
+      WSContentSend_PD(HTTP_SNS_DISTANCE_CM, distance_chr);
 #endif  // USE_WEBSERVER
     }
   }
