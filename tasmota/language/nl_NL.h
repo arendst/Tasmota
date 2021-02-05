c6bf7a5521a326349094198a5729297dae8c4e6e
crispy78 <42983001+crispy78@users.noreply.github.com>
2021-02-05 20:39:10 +0100

    Update nl_NL.h

--- tasmota/language/nl_NL.h
+++ tasmota/language/nl_NL.h
@@ -28,7 +28,7 @@
  * Use online command StateText to translate ON, OFF, HOLD and TOGGLE.
  * Use online command Prefix to translate cmnd, stat and tele.
  *
- * Updated until v9.1.0
+ * Updated until v9.2.0.4
 \*********************************************************************/
 
 //#define LANGUAGE_MODULE_NAME         // Enable to display "Module Generic" (ie Spanish), Disable to display "Generic Module" (ie English)
