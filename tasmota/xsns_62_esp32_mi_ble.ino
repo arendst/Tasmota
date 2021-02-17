e7ef60aa3d9ef084f6458759840a26e381199e5f
Simon Hailes <simon@yellaumbrella.tv>
2021-02-17 10:52:02 +0000

    better responses for Mi32Optionx n

--- tasmota/xsns_62_esp32_mi_ble.ino
+++ tasmota/xsns_62_esp32_mi_ble.ino
@@ -2171,22 +2171,22 @@ void CmndMi32Option(void){
   switch(XdrvMailbox.index) {
     case 0:
       MI32.option.allwaysAggregate = onOff;  
-      ResponseCmndNumber(onOff);
+      ResponseCmndIdxNumber(onOff);
       return;
       break;
     case 1:
       MI32.option.noSummary = onOff;
-      ResponseCmndNumber(onOff);
+      ResponseCmndIdxNumber(onOff);
       return;
       break;
     case 2:
       MI32.option.directBridgeMode = onOff;
-      ResponseCmndNumber(onOff);
+      ResponseCmndIdxNumber(onOff);
       return;
       break;
     case 4:{
       MI32.option.ignoreBogusBattery = onOff;
-      ResponseCmndNumber(onOff);
+      ResponseCmndIdxNumber(onOff);
       return;
     } break;
     case 5:{
@@ -2195,17 +2195,17 @@ void CmndMi32Option(void){
         // discard all sensors for a restart
         MIBLEsensors.clear();
       }
-      ResponseCmndNumber(onOff);
+      ResponseCmndIdxNumber(onOff);
       return;
     } break;
     case 6:{
       MI32.option.MQTTType = onOff;
-      ResponseCmndNumber(onOff);
+      ResponseCmndIdxNumber(onOff);
       return;
     } break;
     
   }
-  ResponseCmndDone();
+  ResponseCmndIdxError();
 }
 
 void MI32KeyListResp(){
