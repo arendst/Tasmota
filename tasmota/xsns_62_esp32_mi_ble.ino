10ee56b9da6061f67ed48782c7d9a9d60077a275
Simon Hailes <simon@yellaumbrella.tv>
2021-02-17 10:47:34 +0000

    change MQTT display of TRIGGERED sensors.

--- tasmota/xsns_62_esp32_mi_ble.ino
+++ tasmota/xsns_62_esp32_mi_ble.ino
@@ -2835,7 +2835,8 @@ void MI32ShowTriggeredSensors(){
 
       cnt++;
       ResponseAppend_P(PSTR(","));
-      MI32GetOneSensorJson(sensor, 0);
+      // hide sensor name if HASS or option6
+      MI32GetOneSensorJson(sensor, (maxcnt == 1));
       int mlen = strlen(TasmotaGlobal.mqtt_data);
 
       // if we ran out of room, leave here.
