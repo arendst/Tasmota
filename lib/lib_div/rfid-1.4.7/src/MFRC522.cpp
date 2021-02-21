b80454e6dc8e80e3b4a2e3c26f93c858742db8a8
rapi3 <18531150+rapi3@users.noreply.github.com>
2021-02-21 15:57:38 +0000

    Update MFRC522.cpp

--- lib/lib_div/rfid-1.4.7/src/MFRC522.cpp
+++ lib/lib_div/rfid-1.4.7/src/MFRC522.cpp
@@ -233,6 +233,7 @@ void MFRC522::PCD_Init() {
 	
 	PCD_WriteRegister(TxASKReg, 0x40);		// Default 0x00. Force a 100 % ASK modulation independent of the ModGsPReg register setting
 	PCD_WriteRegister(ModeReg, 0x3D);		// Default 0x3F. Set the preset value for the CRC coprocessor for the CalcCRC command to 0x6363 (ISO 14443-3 part 6.2.4)
+	PCD_WriteRegister(RFCfgReg, (0x07<<4));         // Set Rx Gain to max
 	PCD_AntennaOn();						// Enable the antenna driver pins TX1 and TX2 (they were disabled by the reset)
 } // End PCD_Init()
 
