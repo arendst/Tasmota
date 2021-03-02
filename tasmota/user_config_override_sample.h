18ed1d900783b1bab70d54e0a6e477a081940be8
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2021-03-02 12:40:14 +0100

    Fix comment error

--- tasmota/user_config_override_sample.h
+++ tasmota/user_config_override_sample.h
@@ -82,7 +82,7 @@ Examples :
 #define WIFI_DNS            MY_DNS               // If not using DHCP set DNS IP address (might be equal to WIFI_GATEWAY)
 #endif
 
-// !!! Place your changes AFTER the line with "*/" !!! 
+// !!! Remember that your changes GOES AT THE BOTTOM OF THIS FILE right before the last #endif !!! 
 */
 
 
