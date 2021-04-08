e9e0d3536871d22a9a69eb8644f5dd47adb700f4
Barbudor <barbudor@barbudor.net>
2021-04-08 22:59:44 +0200

    command EnergyConfig

--- tasmota/xnrg_20_dummy.ino
+++ tasmota/xnrg_20_dummy.ino
@@ -99,6 +99,9 @@ bool NrgDummyCommand(void) {
       }
     }
   }
+  else if (CMND_NRGCONFIG == Energy.command_code) {
+    AddLog_P(LOG_LEVEL_DEBUG, PSTR("NRG: Config payload:%d, data:'%s'"), XdrvMailbox.payload, XdrvMailbox.data ? XdrvMailbox.data : "null" );
+  }
   else serviced = false;  // Unknown command
 
   return serviced;
