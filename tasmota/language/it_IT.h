915f2e2cef29157b07cfec2b7dc203e1dbf759a3
Adrian Scillato <39969427+ascillato2@users.noreply.github.com>
2021-04-10 11:16:49 -0300

    Update it_IT.h

--- tasmota/language/it_IT.h
+++ tasmota/language/it_IT.h
@@ -28,7 +28,7 @@
  * Use online command StateText to translate ON, OFF, HOLD and TOGGLE.
  * Use online command Prefix to translate cmnd, stat and tele.
  *
- * Updated until v9.3.1.1 - Last update 10.04.2021
+ * Updated until v9.3.1.2 - Last update 10.04.2021
 \*********************************************************************/
 
 #define LANGUAGE_MODULE_NAME         // Enable to display "Module Generic" (ie Spanish), Disable to display "Generic Module" (ie English)
