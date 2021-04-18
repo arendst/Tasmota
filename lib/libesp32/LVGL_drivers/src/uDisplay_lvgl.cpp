916c4e7a58fced6852f49bd9db0bada397057a83
Stephan Hadinger <stephan.hadinger@gmail.com>
2021-04-18 20:06:04 +0200

    Fix compilation

--- lib/libesp32/LVGL_drivers/src/uDisplay_lvgl.cpp
+++ lib/libesp32/LVGL_drivers/src/uDisplay_lvgl.cpp
@@ -17,6 +17,7 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#ifdef USE_LVGL
 #include <Arduino.h>
 #include "uDisplay_lvgl.h"
 
@@ -1579,3 +1580,4 @@ void uDisplay_lvgl::drawFastHLine_EPD(int16_t x, int16_t y, int16_t w, uint16_t 
     x++;
   }
 }
+#endif // USE_LVGL
