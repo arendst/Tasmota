418f3091049803145ff454a67c63b458d9cf8998
Theo Arends <11044339+arendst@users.noreply.github.com>
2021-05-09 15:34:59 +0200

    Another MQTT file upload example using acknowledge

--- tools/mqtt-file/upload-example2.py
+++ tools/mqtt-file/upload-example2.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-   upload-example1.py - Upload Tasmota settings file
+   upload-example2.py - Upload Tasmota settings file
 
    Copyright (C) 2021  Theo Arends
 
@@ -27,7 +27,7 @@ Requirements:
 Instructions:
    Edit file and change parameters in User Configuration Section
 
-   Then execute command upload-example1.py
+   Then execute command upload-example2.py
 
 """
 
@@ -55,39 +55,65 @@ myid = 1620484815
 
 # Derive fulltopic from broker LWT message
 mypublish = "cmnd/"+mytopic+"/fileupload"
+mysubscribe = "stat/"+mytopic+"/FILEUPLOAD"  # Case sensitive
 
 # Tasmota currently supports MQTT message size of 1040 characters. Base64 adds 0.25 chars
 chucksize = 700                        # Tasmota max chunk size
 
-# Example does not use feedback Acknowledge
-sleeptime = 0.2
+# Example does use feedback Acknowledge
+Ack_flag = False
+
+# The callback for when mysubscribe message is received
+def on_message(client, userdata, msg):
+   global Ack_flag
+#   print("Received message =",str(msg.payload.decode("utf-8")))
+   Ack_flag = False
+
+def wait_for_ack():
+   global Ack_flag
+   timeout = 100
+   while Ack_flag and timeout > 0:
+      time.sleep(0.01)
+      timeout = timeout -1
+
+   if Ack_flag:
+      print("Error: Ack timeout")
+
+   return Ack_flag
 
 client = mqtt.Client()
+client.on_message = on_message
 client.connect(broker, broker_port)
+client.loop_start()                    # Start loop to process received messages
+client.subscribe(mysubscribe)
 
 time_start = time.time()
 print("Uploading file "+myfile+" to "+mytopic+" ...")
 
 client.publish(mypublish, "{\"File\":\""+myfile+"\",\"Id\":"+str(myid)+",\"Type\":"+str(myfiletype)+",\"Size\":"+str(myfilesize)+"}")
+Ack_flag = True
 
 out_hash_md5 = hashlib.md5()
 
 fo = open(myfile,"rb")
 Run_flag = True
 while Run_flag:
-   time.sleep(sleeptime)               # We do not use Ack here
-
-   chunk = fo.read(chucksize)
-   if chunk:
-      out_hash_md5.update(chunk)       # Update hash
-      base64_encoded_data = base64.b64encode(chunk)
-      base64_data = base64_encoded_data.decode('utf-8')
-      client.publish(mypublish, "{\"Id\":"+str(myid)+",\"Data\":\""+base64_data+"\"}")
+   if wait_for_ack():                  # We use Ack here
+      Run_flag = False
 
    else:
-      md5_hash = out_hash_md5.hexdigest()
-      client.publish(mypublish, "{\"Id\":"+str(myid)+",\"Md5\":\""+md5_hash+"\"}")
-      Run_flag=False
+      chunk = fo.read(chucksize)
+      if chunk:
+         out_hash_md5.update(chunk)    # Update hash
+         base64_encoded_data = base64.b64encode(chunk)
+         base64_data = base64_encoded_data.decode('utf-8')
+         client.publish(mypublish, "{\"Id\":"+str(myid)+",\"Data\":\""+base64_data+"\"}")
+         Ack_flag = True
+
+      else:
+         md5_hash = out_hash_md5.hexdigest()
+         client.publish(mypublish, "{\"Id\":"+str(myid)+",\"Md5\":\""+md5_hash+"\"}")
+         Run_flag = False
 
 fo.close()
 
@@ -95,3 +121,4 @@ time_taken = time.time() - time_start
 print("Done in "+str("%.2f"%time_taken)+" seconds")
 
 client.disconnect()                    # Disconnect
+client.loop_stop()                     # Stop loop
