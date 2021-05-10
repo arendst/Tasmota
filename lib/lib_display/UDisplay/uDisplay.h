b71437024ad833457c982b4a45cab42b9a29293a
mikep1998 <44448320+mikep1998@users.noreply.github.com>
2021-05-10 13:26:59 -0700

    Adding support for ILI9488/XPT2046 display

--- lib/lib_display/UDisplay/uDisplay.h
+++ lib/lib_display/UDisplay/uDisplay.h
@@ -214,6 +214,10 @@ class uDisplay : public Renderer {
    uint16_t seta_xp2;
    uint16_t seta_yp1;
    uint16_t seta_yp2;
+   int16_t rotmap_xmin;
+   int16_t rotmap_xmax;
+   int16_t rotmap_ymin;
+   int16_t rotmap_ymax;
    void pushColorsMono(uint16_t *data, uint16_t len);
 #ifdef ESP32
    // dma section
