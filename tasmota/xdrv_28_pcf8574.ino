005f26e4582b12cb88d915867d1592c531930238
Theo Arends <11044339+arendst@users.noreply.github.com>
2021-05-12 17:17:35 +0200

    Fix PCF8574 JSON (#12088)
    Fix PCF8574 JSON (#12088)

--- tasmota/xdrv_28_pcf8574.ino
+++ tasmota/xdrv_28_pcf8574.ino
@@ -219,7 +219,7 @@ void Pcf8574Show(bool json)
     for (int idx = 0 ; idx < Pcf8574.max_devices ; idx++)
     {
       uint8_t gpio = Pcf8574Read(idx);
-      ResponseAppend_P(PSTR(",\"PCF8574%c%d\":{\"D0\":%i,\"D1\":%i,\"D2\":%i,\"D3\":%i,\"D4\":%i,\"D5\":%i,\"D6\":%i,\"D7\":%i"),
+      ResponseAppend_P(PSTR(",\"PCF8574%c%d\":{\"D0\":%i,\"D1\":%i,\"D2\":%i,\"D3\":%i,\"D4\":%i,\"D5\":%i,\"D6\":%i,\"D7\":%i}"),
                 IndexSeparator(), idx +1,
                 (gpio>>0)&1,(gpio>>1)&1,(gpio>>2)&1,(gpio>>3)&1,(gpio>>4)&1,(gpio>>5)&1,(gpio>>6)&1,(gpio>>7)&1);
     }
