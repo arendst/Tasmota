2a9d80bea23eae248829e932400a37b103735e57
Adrian Scillato <39969427+ascillato2@users.noreply.github.com>
2021-07-19 13:59:56 -0300

    Update IT-Localization File version

--- tasmota/language/it_IT.h
+++ tasmota/language/it_IT.h
@@ -28,7 +28,7 @@
  * Use online command StateText to translate ON, OFF, HOLD and TOGGLE.
  * Use online command Prefix to translate cmnd, stat and tele.
  *
- * Updated until v9.4.0.1 - Last update 19.07.2021
+ * Updated until v9.5.0.3 - Last update 19.07.2021
 \*********************************************************************/
 
 #define LANGUAGE_MODULE_NAME         // Enable to display "Module Generic" (ie Spanish), Disable to display "Generic Module" (ie English)
