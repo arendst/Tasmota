475b71e848d4d6873d1f48c951c52e8888932f84
Adrian Scillato <ascillato@gmail.com>
2021-08-05 17:34:29 -0300

    Update uk_UA.h

--- tasmota/language/uk_UA.h
+++ tasmota/language/uk_UA.h
@@ -240,6 +240,7 @@
 #define D_ERASED_SECTOR "Стерто сектор"
 
 // xdrv_02_webserver.ino
+#define D_NO_PASSWORD_SET "Device Insecure<br>Please set a Web Admin Password"
 #define D_NOSCRIPT "Для використання Tasmota треба увімкнути JavaScript"
 #define D_MINIMAL_FIRMWARE_PLEASE_UPGRADE "Прошивка MINIMAL<br>будь-ласка оновіть"
 #define D_WEBSERVER_ACTIVE_ON "Веб-сервер активний"
