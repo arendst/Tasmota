d9560d0f4cd24a8760d1466cea5faf723c87afb3
Adrian Scillato <35405447+ascillato@users.noreply.github.com>
2021-08-16 19:05:15 -0300

    Update nl_NL.h

--- tasmota/language/nl_NL.h
+++ tasmota/language/nl_NL.h
@@ -328,6 +328,8 @@
 #define D_WEB_ADMIN_PASSWORD "Web Admin Wachtwoord"
 #define D_MQTT_ENABLE "MQTT ingeschakeld"
 #define D_MQTT_TLS_ENABLE "MQTT TLS"
+#define D_HTTP_API "HTTP API"
+#define D_HTTP_API_ENABLE "HTTP API ingeschakeld"
 #define D_FRIENDLY_NAME "Beschrijvende naam"
 #define D_BELKIN_WEMO "Belkin WeMo"
 #define D_HUE_BRIDGE "Hue Bridge"
