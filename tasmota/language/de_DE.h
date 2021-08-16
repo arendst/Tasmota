2c2e810daef441a02555219128a7e5096cdb6701
Adrian Scillato <35405447+ascillato@users.noreply.github.com>
2021-08-16 19:00:14 -0300

    Update de_DE.h

--- tasmota/language/de_DE.h
+++ tasmota/language/de_DE.h
@@ -328,6 +328,8 @@
 #define D_WEB_ADMIN_PASSWORD "Passwort für Web Oberfläche"
 #define D_MQTT_ENABLE "MQTT aktivieren"
 #define D_MQTT_TLS_ENABLE "MQTT TLS"
+#define D_HTTP_API "HTTP API"
+#define D_HTTP_API_ENABLE "HTTP API aktivieren"
 #define D_FRIENDLY_NAME "Name [friendly name]"
 #define D_BELKIN_WEMO "Belkin WeMo"
 #define D_HUE_BRIDGE "Hue Bridge"
