209724944a74be68f9083b672d3536c71880c76f
Adrian Scillato <35405447+ascillato@users.noreply.github.com>
2021-08-16 19:08:55 -0300

    Update tr_TR.h

--- tasmota/language/tr_TR.h
+++ tasmota/language/tr_TR.h
@@ -328,6 +328,8 @@
 #define D_WEB_ADMIN_PASSWORD "Web Yönetici Şifresi"
 #define D_MQTT_ENABLE "MQTT aktif"
 #define D_MQTT_TLS_ENABLE "MQTT TLS"
+#define D_HTTP_API "HTTP API"
+#define D_HTTP_API_ENABLE "HTTP API aktif"
 #define D_FRIENDLY_NAME "Kullanıcı Dostu İsim"
 #define D_BELKIN_WEMO "Belkin WeMo"
 #define D_HUE_BRIDGE "Hue Bridge"
