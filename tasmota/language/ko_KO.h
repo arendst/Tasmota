a9e8b764a61be40f3d561241bc6a4813204010e6
Adrian Scillato <35405447+ascillato@users.noreply.github.com>
2021-08-16 19:04:48 -0300

    Update ko_KO.h

--- tasmota/language/ko_KO.h
+++ tasmota/language/ko_KO.h
@@ -328,6 +328,8 @@
 #define D_WEB_ADMIN_PASSWORD "Web Admin 비밀번호"
 #define D_MQTT_ENABLE "MQTT 사용"
 #define D_MQTT_TLS_ENABLE "MQTT TLS"
+#define D_HTTP_API "HTTP API"
+#define D_HTTP_API_ENABLE "HTTP API 사용"
 #define D_FRIENDLY_NAME "Friendly Name"
 #define D_BELKIN_WEMO "Belkin WeMo"
 #define D_HUE_BRIDGE "Hue Bridge"
