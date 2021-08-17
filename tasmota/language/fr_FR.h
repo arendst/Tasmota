fab10999e11fde94c192dcb1fbe45a4318eb7594
Adrian Scillato <35405447+ascillato@users.noreply.github.com>
2021-08-17 17:11:28 -0300

    Update fr_FR.h

--- tasmota/language/fr_FR.h
+++ tasmota/language/fr_FR.h
@@ -250,6 +250,7 @@
 #define D_WIFIMANAGER_SET_ACCESSPOINT_AND_STATION "Wifimanager en mode Point d'Accès et Station"
 #define D_WIFIMANAGER_SET_ACCESSPOINT "Wifimanager en mode Point d'Accès"
 #define D_TRYING_TO_CONNECT "Tentative de connexion du module au réseau"
+#define D_REFERER_DENIED "Referer denied. Use 'SO128 1' for HTTP API commands. 'Webpassword' is recommended."
 
 #define D_RESTART_IN "Redémarrage dans"
 #define D_SECONDS "secondes"
