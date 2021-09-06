605f1f2d4b50fcaddf7eb421866fd674129ed54c
Simon Hailes <simon@yellaumbrella.tv>
2021-09-06 22:23:03 +0100

    change link displayed in TAS for MI BLE sensors which need a key to point to the new repo https://github.com/tasmota/ble_key_extractor/blob/gh-pages/index.html

--- tasmota/xsns_62_esp32_mi_ble.ino
+++ tasmota/xsns_62_esp32_mi_ble.ino
@@ -2389,7 +2389,7 @@ const char HTTP_MI32_LIGHT[] PROGMEM = "{s}%s" " Light" "{m}%d{e}";
 //  "http://127.0.0.1:8887/keys/TelinkFlasher.html?mac=%s&cb=http%%3A%%2F%%2F%s%%2Fmikey"
 //  "\">%s</a>{m} {e}";
 const char HTTP_NEEDKEY[] PROGMEM = "{s}%s <a target=\"_blank\" href=\""
-  "https://btsimonh.github.io/atc1441.github.io/TelinkFlasherTasmota.html?mac=%s&cb=http%%3A%%2F%%2F%s%%2Fmikey"
+  "https://tasmota.github.io/ble_key_extractor?mac=%s&cb=http%%3A%%2F%%2F%s%%2Fmikey"
   "\">%s</a>{m} {e}";
 
 
