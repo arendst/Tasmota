805b199255855ec3e6fa1721a0f8262d9aa9ad8a
Adrian Scillato <ascillato@gmail.com>
2021-10-11 19:42:04 -0300

    Update Version of FR translation File

--- tasmota/language/fr_FR.h
+++ tasmota/language/fr_FR.h
@@ -28,7 +28,7 @@
  * Use online command StateText to translate ON, OFF, HOLD and TOGGLE.
  * Use online command Prefix to translate cmnd, stat and tele.
  *
- * Updated until v9.3.1.1
+ * Updated until v9.5.0.9
 \*********************************************************************/
 
 #define LANGUAGE_MODULE_NAME         // Enable to display "Module Generic" (ie Spanish), Disable to display "Generic Module" (ie English)
