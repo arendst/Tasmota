c8c21e9e3789a80d29605596ebc8634a1895a255
s-hadinger <49731213+s-hadinger@users.noreply.github.com>
2021-11-12 09:43:55 +0100

    Tcpbridge fix warning (#13651)
    * Fix compiler warning
    
    * Fix compiler warning

--- tasmota/support.ino
+++ tasmota/support.ino
@@ -1840,18 +1840,15 @@ int8_t ParseSerialConfig(const char *pstr)
   return serial_config;
 }
 
+uint32_t ConvertSerialConfig(uint8_t serial_config) {
 #ifdef ESP8266
-SerConfu8 ConvertSerialConfigESP8286(uint8_t serial_config) {
-  return (SerConfu8)pgm_read_byte(kTasmotaSerialConfig + serial_config);
-}
-#define ConvertSerialConfig(a) ConvertSerialConfigESP8286(a)
-#endif  // ESP8266
-#ifdef ESP32
-uint32_t ConvertSerialConfigESP32(uint8_t serial_config) {
+  return (uint32_t)pgm_read_byte(kTasmotaSerialConfig + serial_config);
+#elif defined(ESP32)
   return (uint32_t)pgm_read_dword(kTasmotaSerialConfig + serial_config);
+#else
+  #error "platform not supported"
+#endif
 }
-#define ConvertSerialConfig(a) ConvertSerialConfigESP32(a)
-#endif  // ESP32
 
 // workaround disabled 05.11.2021 solved with https://github.com/espressif/arduino-esp32/pull/5549
 //#if defined(ESP32) && CONFIG_IDF_TARGET_ESP32C3
