02346b4e49290f54713904a5c7c92da8d4bb3c26
Theo Arends <11044339+arendst@users.noreply.github.com>
2021-11-13 12:49:38 +0100

    Update comments

--- tasmota/tasmota_template.h
+++ tasmota/tasmota_template.h
@@ -1076,11 +1076,11 @@ const char PINS_WEMOS[] PROGMEM = "IOTXIORXIOIOFLFLFLFLFLFLIOIOIOIOIOIOIOIOIOIOI
 
 typedef struct MYIO {
   uint16_t      io[MAX_GPIO_PIN];
-} myio;                         // ESP8266: 18 * 2 = 36 bytes / ESP32: 40 * 2 = 80 bytes / ESP32-C3: 22 * 2 = 44 bytes
+} myio;                         // ESP8266: 18*2 = 36 bytes / ESP32: 40*2 = 80 bytes / ESP32-C3: 22*2 = 44 bytes / ESP32-S2: 47*2 = 94 bytes
 
 typedef struct MYCFGIO {
   uint16_t      io[MAX_USER_PINS];
-} mycfgio;                      // ESP8266: 14 * 2 = 28 bytes / ESP32: 36 * 2 = 72 bytes / ESP32-C3: 22 * 2 = 44 bytes
+} mycfgio;                      // ESP8266: 14*2 = 28 bytes / ESP32: 36*2 = 72 bytes / ESP32-C3: 22*2 = 44 bytes / ESP32-S2: 36*2 = 72 bytes
 
 #define GPIO_FLAG_USED       0  // Currently no flags used
 
@@ -1107,9 +1107,9 @@ typedef union {
 } gpio_flag;                    // 2 bytes
 
 typedef struct MYTMPLT {
-  mycfgio      gp;              // 28 / 72 / 44 bytes
+  mycfgio      gp;              // 28 / 72 / 44 / 72 bytes
   gpio_flag    flag;            // 2 bytes
-} mytmplt;                      // 30 / 74 / 46 bytes
+} mytmplt;                      // 30 / 74 / 46 / 74 bytes
 
 //********************************************************************************************
 
