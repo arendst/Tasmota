aca519ddecf66e1255d7be8ac6ae6a2ef3b8822d
Alexey Pavlov <ap@profi.ru>
2021-12-17 15:27:34 +0300

    format fixes

--- tasmota/xsns_69_opentherm_protocol.ino
+++ tasmota/xsns_69_opentherm_protocol.ino
@@ -16,6 +16,7 @@
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 #ifdef USE_OPENTHERM
 
 #include "OpenTherm.h"
