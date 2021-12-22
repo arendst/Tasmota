b8e9d9401e6d3da4e3e5ee19d29494f3d1620199
wir3z <lpakula@velocitydevices.com>
2021-12-22 09:50:35 -0700

    Added back missing external.

--- lib/default/TasmotaSerial-3.3.0/src/TasmotaSerial.h
+++ lib/default/TasmotaSerial-3.3.0/src/TasmotaSerial.h
@@ -35,6 +35,7 @@
 #include <HardwareSerial.h>
 #endif
 
+extern uint32_t ConvertSerialConfig(uint8_t serial_config);
 class TasmotaSerial : public Stream {
   public:
     TasmotaSerial(int receive_pin, int transmit_pin, int hardware_fallback = 0, int nwmode = 0, int buffer_size = TM_SERIAL_BUFFER_SIZE);
