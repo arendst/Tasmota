a4cd168681b7058181ab36ba701b1d7ab3c9c404
Stephan Hadinger <stephan.hadinger@gmail.com>
2021-12-27 18:12:38 +0100

    LVGL modularization

--- lib/libesp32/berry/gen.sh
+++ lib/libesp32/berry/gen.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 tools/pycoc/main.py -o generate src default ../berry_mapping/src ../../libesp32_lvgl/lv_berry/src -c default/berry_conf.h
+python3 tools/pycoc/main.py -o generate src default ../berry_mapping/src ../../libesp32_lvgl/lv_berry/src ../../libesp32_lvgl/lv_berry/generate -c default/berry_conf.h
