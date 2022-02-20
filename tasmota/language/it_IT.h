877966d1acbc133fa6b36818f73bad6c7d69fddb
bovirus <1262554+bovirus@users.noreply.github.com>
2022-02-20 13:32:57 +0100

    Italian language update

--- tasmota/language/it_IT.h
+++ tasmota/language/it_IT.h
@@ -28,7 +28,7 @@
  * Use online command StateText to translate ON, OFF, HOLD and TOGGLE.
  * Use online command Prefix to translate cmnd, stat and tele.
  *
- * Updated until v10.1.0.6 - Last update 04.02.2022
+ * Updated until v10.1.0.6 - Last update 20.02.2022
 \*********************************************************************/
 
 #define LANGUAGE_MODULE_NAME         // Enable to display "Module Generic" (ie Spanish), Disable to display "Generic Module" (ie English)
@@ -847,8 +847,8 @@
 #define D_SENSOR_HRG15_RX               "HRG15 - RX"
 #define D_SENSOR_HRG15_TX               "HRG15 - TX"
 #define D_SENSOR_VINDRIKTNING_RX        "VINDRIKTNING"
-#define D_SENSOR_BL6523_TX "BL6523 Tx"
-#define D_SENSOR_BL6523_RX "BL6523 Rx"
+#define D_SENSOR_BL6523_TX              "BL6523 - TX"
+#define D_SENSOR_BL6523_RX              "BL6523 - RX"
 #define D_SENSOR_HEARTBEAT              "Battito cardiaco"
 #define D_GPIO_SHIFT595_SRCLK           "74x595 - SRCLK"
 #define D_GPIO_SHIFT595_RCLK            "74x595 - RCLK"
