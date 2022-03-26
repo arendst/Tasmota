3b10e0ef5c2ad2af53fa2c928d34e92fc44e79f9
barbudor <barbudor@barbudor.net>
2022-03-26 14:10:08 +0100

    timerx returns absolute time of day 0:00 to 23:59

--- tasmota/xdrv_10_rules.ino
+++ tasmota/xdrv_10_rules.ino
@@ -477,11 +477,7 @@ bool RulesRuleMatch(uint8_t rule_set, String &event, String &rule, bool stop_all
       if ((index > 0) && (index <= MAX_TIMERS)) {
         snprintf_P(stemp, sizeof(stemp), PSTR("%%TIMER%d%%"), index);
         if (rule_param.startsWith(stemp)) {
-          int32_t timer = Settings->timer[index -1].time;
-          if (Settings->timer[index -1].mode && (timer >= 12*60)) {
-            timer = -(timer - (12*60));
-          }
-          rule_param = String(timer);
+          rule_param = String(TimerGetTimeOfDay(index -1));
         }
       }
     }
@@ -800,11 +796,7 @@ bool RuleSetProcess(uint8_t rule_set, String &event_saved)
 #if defined(USE_TIMERS)
       for (uint32_t i = 0; i < MAX_TIMERS; i++) {
         snprintf_P(stemp, sizeof(stemp), PSTR("%%TIMER%d%%"), i +1);
-        int32_t timer = Settings->timer[i].time;
-        if (Settings->timer[i].mode && (timer >= 12*60)) {
-          timer = -(timer - 12*60);
-        }
-        RulesVarReplace(commands, stemp, String(timer));
+        RulesVarReplace(commands, stemp, String(TimerGetTimeOfDay(i)));
       }
 #if defined(USE_SUNRISE)
       RulesVarReplace(commands, F("%SUNRISE%"), String(SunMinutes(0)));
