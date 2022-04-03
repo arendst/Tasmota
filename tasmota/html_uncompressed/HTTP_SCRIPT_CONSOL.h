7ac91d4e341e7184d25116ed06689f151ce0293b
Bernhard Kirchen <schlimmchen@posteo.net>
2022-04-03 10:16:55 +0200

    console cursor: use lambda function

--- tasmota/html_uncompressed/HTTP_SCRIPT_CONSOL.h
+++ tasmota/html_uncompressed/HTTP_SCRIPT_CONSOL.h
@@ -47,7 +47,7 @@ const char HTTP_SCRIPT_CONSOL[] PROGMEM =
       "var b=eb('c1'),c=e.keyCode;"       // c1 = Console command id
       "if(38==c||40==c){" // ArrowUp or ArrowDown
         "b.autocomplete='off';" // ArrowUp or ArrowDown must be a keyboard so stop browser autocomplete
-        "setTimeout(function(b){" // for best compatibility (chrome) we need to schedule this function
+        "setTimeout(b=>{" // for best compatibility (chrome) we need to schedule this function
           "b.focus();" // for best compatibility (chrome) we need to (re)focus the input element
           "b.setSelectionRange(1e9,1e9);" // move cursor to the end (hopefully) of the command inserted from history
         "},0,b);"
