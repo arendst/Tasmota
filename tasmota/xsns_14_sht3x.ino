53856466fa8939c161faec229b4d8a99970ddeb4
Stefan Tibus <38475243+stibus@users.noreply.github.com>
2022-04-18 18:43:52 +0200

    Add support of SHT4x to SHT3x driver

--- tasmota/xsns_14_sht3x.ino
+++ tasmota/xsns_14_sht3x.ino
@@ -1,7 +1,7 @@
 /*
   xsns_14_sht3x.ino - SHT3X temperature and humidity sensor support for Tasmota
 
-  Copyright (C) 2021  Theo Arends
+  Copyright (C) 2022  Theo Arends, Stefan Tibus
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -20,73 +20,249 @@
 #ifdef USE_I2C
 #ifdef USE_SHT3X
 /*********************************************************************************************\
- * SHT3X and SHTC3 - Temperature and Humidity
+ * Sensirion I2C temperature and humidity sensors
  *
- * I2C Address: 0x44, 0x45 or 0x70 (SHTC3)
+ * This driver supports the following sensors:
+ * - SHT3x series: SHT30, SHT31, SHT35 (adresses: A: 0x44, B: 0x45)
+ * - SHTC series: SHTC1, SHTC3 (address: 0x70)
+ * - SHT4x series: SHT40, SHT41, SHT45 (addresses: A: 0x44, B: 0x45)
 \*********************************************************************************************/
 
 #define XSNS_14             14
 #define XI2C_15             15         // See I2CDEVICES.md
 
-#define SHT3X_ADDR_GND      0x44       // address pin low (GND)
-#define SHT3X_ADDR_VDD      0x45       // address pin high (VDD)
-#define SHTC3_ADDR          0x70       // address for shtc3 sensor
+#define SHT3X_ADDRESSES     2          // 2 addresses for SHT3x
+#define SHT3X_ADDR_GND      0x44       // Address A pin low (GND)
+#define SHT3X_ADDR_VDD      0x45       // Address B pin high (VDD)
+#define SHTCX_ADDRESSES     1          // 1 address for SHTCx
+#define SHTCX_ADDR          0x70       // Address for SHTCx sensors
+#define SHT4X_ADDRESSES     2          // 2 addresses for SHT4x
+#define SHT4X_ADDR_A        0x44       // Address SHT4x A
+#define SHT4X_ADDR_B        0x45       // Address SHT4x B
+#define SHT3X_MAX_SENSORS   SHT3X_ADDRESSES + SHTC3_ADDRESSES + SHT4X_ADDRESSES
 
-#define SHT3X_MAX_SENSORS   3
+enum SHT3X_Types {
+  SHT3X_TYPE_SHT3X,
+  SHT3X_TYPE_SHTCX,
+  SHT3X_TYPE_SHT4X
+};
+const char kSht3xTypes[] PROGMEM = "SHT3X|SHTC3|SHT4X";
 
-const char kShtTypes[] PROGMEM = "SHT3X|SHT3X|SHTC3";
-uint8_t sht3x_addresses[] = { SHT3X_ADDR_GND, SHT3X_ADDR_VDD, SHTC3_ADDR };
+uint8_t sht3x_addresses[] = { SHT3X_ADDR_GND, SHT3X_ADDR_VDD };
+uint8_t shtcx_addresses[] = { SHTCX_ADDR };
+uint8_t sht4x_addresses[] = { SHT4X_ADDR_A, SHT4X_ADDR_B };
 
 uint8_t sht3x_count = 0;
 struct SHT3XSTRUCT {
+  uint8_t type;       // Sensor type
   uint8_t address;    // I2C bus address
-  char types[6];      // Sensor type name and address - "SHT3X-0xXX"
-} sht3x_sensors[SHT3X_MAX_SENSORS];
+  char typeString[6]; // Sensor type name and address, e.g. "SHT3X-0xXX"
+} sht3x_sensors[SHT3X_ADDRESSES];
+
+uint8_t computeCrc(uint8_t data[], uint8_t len)
+// Compute CRC as per datasheet
+{
+  uint8_t crc = 0xFF;
+
+  for (uint8_t x = 0 ; x < len ; x++)
+  {
+    crc ^= data[x];
+    for (uint8_t i = 0 ; i < 8 ; i++)
+    {
+      if (crc & 0x80)
+        crc = (crc << 1) ^ 0x31;
+      else
+        crc <<= 1;
+    }
+  }
+  return crc;
+}
+
+float convertValue(uint8_t data[], float A, float B, float C)
+{
+  return A + (B * (float)((data[0] << 8) | data[1])) / C;
+}
 
 bool Sht3xRead(float &t, float &h, uint8_t sht3x_address)
+// Read temperature and humidity for SHT3x series
 {
-  unsigned int data[6];
+  uint8_t data[6];
 
   t = NAN;
   h = NAN;
 
   Wire.beginTransmission(sht3x_address);
-  if (SHTC3_ADDR == sht3x_address) {
-    Wire.write(0x35);                  // Wake from
-    Wire.write(0x17);                  // sleep
-    Wire.endTransmission();
-    Wire.beginTransmission(sht3x_address);
-    Wire.write(0x78);                  // Disable clock stretching ( I don't think that wire library support clock stretching )
-    Wire.write(0x66);                  // High resolution
-  } else {
-    Wire.write(0x2C);                  // Enable clock stretching
-    Wire.write(0x06);                  // High repeatability
+  // TODO: Clock stretching is used for SHT3x but not for SHTC3. Why?
+  Wire.write(0x2C);                     // Enable clock stretching
+  Wire.write(0x06);                     // High repeatability measurement
+  if (Wire.endTransmission() != 0) {    // Stop I2C transmission
+    return false;
   }
+  delay(15); // Max. duration as per datasheet and example code
+  Wire.requestFrom(sht3x_address, (uint8_t)6); // Request 6 bytes of data
+  for (uint32_t i = 0; i < 6; i++) {
+    data[i] = Wire.read(); // temperature (MSB, LSB, CRC), humidity (MSB, LSB, CRC)
+  };
+  // Check CRCs
+  if ((computeCrc(&data[0], 2) != data[2]) || (computeCrc(&data[3], 2) != data[5])) {
+    return false;
+  }
+  t = convertValue(&data[0], -45.0, 175.0, 65535.0);
+  h = convertValue(&data[3], 0.0, 100.0, 65535.0);
+  return (!isnan(t) && !isnan(h));
+}
+
+bool ShtcxReadId(uint16_t &id, uint8_t shtcx_address)
+// Read id register of SHTCx series
+{
+  uint8_t data[3];
+
+  Wire.beginTransmission(shtcx_address);
+  Wire.write(0x35);                    // Wake from
+  Wire.write(0x17);                    // sleep
+  Wire.endTransmission();
+  Wire.beginTransmission(shtcx_address);
+  Wire.write(0xEF);                    // Read-out
+  Wire.write(0xC8);                    // id register
   if (Wire.endTransmission() != 0) {   // Stop I2C transmission
     return false;
   }
-  delay(30);                           // Timing verified with logic analyzer (10 is to short)
-  Wire.requestFrom(sht3x_address, (uint8_t)6);   // Request 6 bytes of data
-  for (uint32_t i = 0; i < 6; i++) {
-    data[i] = Wire.read();             // cTemp msb, cTemp lsb, cTemp crc, humidity msb, humidity lsb, humidity crc
+  delay(1);
+  Wire.requestFrom(shtcx_address, (uint8_t)3); // Request 3 bytes of data
+  for (uint32_t i = 0; i < 3; i++) {
+    data[i] = Wire.read(); // Id (MSB, LSB, CRC)
   };
-  t = ConvertTemp((float)((((data[0] << 8) | data[1]) * 175) / 65535.0) - 45);
-  h = ConvertHumidity((float)((((data[3] << 8) | data[4]) * 100) / 65535.0));
-  return (!isnan(t) && !isnan(h) && (h != 0));
+  // Check CRC
+  if (computeCrc(&data[0], 2) != data[2]) {
+    return false;
+  }
+  id = (data[0] << 8) | data[1];
+  return true;
+}
+
+bool ShtcxRead(float &t, float &h, uint8_t shtcx_address)
+// Read temperature and humidity for SHTCx series
+{
+  uint8_t data[6];
+
+  t = NAN;
+  h = NAN;
+
+  Wire.beginTransmission(shtcx_address);
+  Wire.write(0x35);                    // Wake from
+  Wire.write(0x17);                    // sleep
+  Wire.endTransmission();
+  Wire.beginTransmission(shtcx_address);
+  // TODO: Clock stretching is used for SHT3x but not for SHTC3. Why?
+  Wire.write(0x78);                    // Disable clock stretching
+  Wire.write(0x66);                    // Normal mode measurement
+  if (Wire.endTransmission() != 0) {   // Stop I2C transmission
+    return false;
+  }
+  delay(15); // Max. duration as per datasheet and sample code
+  Wire.requestFrom(shtcx_address, (uint8_t)6); // Request 6 bytes of data
+  for (uint32_t i = 0; i < 6; i++) {
+    data[i] = Wire.read(); // temperature (MSB, LSB, CRC), humidity (MSB, LSB, CRC)
+  };
+  // Check CRCs
+  if ((computeCrc(&data[0], 2) != data[2]) || (computeCrc(&data[3], 2) != data[5])) {
+    return false;
+  }
+  t = convertValue(&data[0], -45.0, 175.0, 65535.0);
+  h = convertValue(&data[3], 0.0, 100.0, 65535.0);
+  return (!isnan(t) && !isnan(h));
+}
+
+bool Sht4xReadSerial(uint32_t &serial, uint8_t sht4x_address)
+// Read serial number of SHT4x series
+{
+  uint8_t data[6];
+
+  Wire.beginTransmission(sht4x_address);
+  Wire.write(0x89);                    // Send command: read serial number
+  if (Wire.endTransmission() != 0) {   // Stop I2C transmission
+    return false;
+  }
+  delay(1);
+  Wire.requestFrom(sht4x_address, (uint8_t)6); // Request 6 bytes of data
+  for (uint32_t i = 0; i < 6; i++) {
+    data[i] = Wire.read(); // serial (MSB1, LSB1, CRC1, MSB2, LSB2, CRC2)
+  };
+  // Check CRCs
+  if ((computeCrc(&data[0], 2) != data[2]) || (computeCrc(&data[3], 2) != data[5])) {
+    return false;
+  }
+  // Convert to 32-bit integer
+  serial = (data[0] << 24) | (data[1] << 16) | (data[2] << 8) | data[3];
+  return true;
+}
+
+bool Sht4xRead(float &t, float &h, uint8_t sht4x_address)
+// Read temperature and humidity for SHT4x series
+{
+  uint8_t data[6];
+
+  t = NAN;
+  h = NAN;
+
+  Wire.beginTransmission(sht4x_address);
+  Wire.write(0xFD);                    // High repeatability measurement
+  if (Wire.endTransmission() != 0) {   // Stop I2C transmission
+    return false;
+  }
+  delay(10); // Max. duration as per datasheet and example code
+  Wire.requestFrom(sht4x_address, (uint8_t)6); // Request 6 bytes of data
+  for (uint32_t i = 0; i < 6; i++) {
+    data[i] = Wire.read(); // temperature (MSB, LSB, CRC), humidity (MSB, LSB, CRC)
+  };
+  // Check CRCs
+  if ((computeCrc(&data[0], 2) != data[2]) || (computeCrc(&data[3], 2) != data[5])) {
+    return false;
+  }
+  t = convertValue(&data[0], -45.0, 175.0, 65535.0);
+  h = convertValue(&data[3], -6.0, 125.0, 65535.0);
+  return (!isnan(t) && !isnan(h));
 }
 
 /********************************************************************************************/
 
 void Sht3xDetect(void)
 {
-  for (uint32_t i = 0; i < SHT3X_MAX_SENSORS; i++) {
+  uint32_t i;
+  // Probe SHT3x: Try reading temperature and humidity
+  for (i = 0; i < SHT3X_ADDRESSES; i++) {
     if (!I2cSetDevice(sht3x_addresses[i])) { continue; }
     float t;
     float h;
     if (Sht3xRead(t, h, sht3x_addresses[i])) {
+      sht3x_sensors[sht3x_count].type = SHT3X_TYPE_SHT3X;
       sht3x_sensors[sht3x_count].address = sht3x_addresses[i];
-      GetTextIndexed(sht3x_sensors[sht3x_count].types, sizeof(sht3x_sensors[sht3x_count].types), i, kShtTypes);
-      I2cSetActiveFound(sht3x_sensors[sht3x_count].address, sht3x_sensors[sht3x_count].types);
+      GetTextIndexed(sht3x_sensors[sht3x_count].typeString, sizeof(sht3x_sensors[sht3x_count].typeString), SHT3X_TYPE_SHT3X, kSht3xTypes);
+      I2cSetActiveFound(sht3x_sensors[sht3x_count].address, sht3x_sensors[sht3x_count].typeString);
+      sht3x_count++;
+    }
+  }
+  // Probe SHTCx: Try reading id
+  for (i = 0; i < SHTCX_ADDRESSES; i++) {
+    if (!I2cSetDevice(shtcx_addresses[i])) { continue; }
+    uint16_t id;
+    if (ShtcxReadId(id, shtcx_addresses[i])) {
+      sht3x_sensors[sht3x_count].type = SHT3X_TYPE_SHTCX;
+      sht3x_sensors[sht3x_count].address = shtcx_addresses[i];
+      GetTextIndexed(sht3x_sensors[sht3x_count].typeString, sizeof(sht3x_sensors[sht3x_count].typeString), SHT3X_TYPE_SHTCX, kSht3xTypes);
+      I2cSetActiveFound(sht3x_sensors[sht3x_count].address, sht3x_sensors[sht3x_count].typeString);
+      sht3x_count++;
+    }
+  }
+  // Probe SHT4x: Try reading serial number
+  for (i = 0; i < SHT4X_ADDRESSES; i++) {
+    if (!I2cSetDevice(sht4x_addresses[i])) { continue; }
+    uint32_t serial;
+    if (Sht4xReadSerial(serial, sht4x_addresses[i])) {
+      sht3x_sensors[sht3x_count].type = SHT3X_TYPE_SHT4X;
+      sht3x_sensors[sht3x_count].address = sht4x_addresses[i];
+      GetTextIndexed(sht3x_sensors[sht3x_count].typeString, sizeof(sht3x_sensors[sht3x_count].typeString), SHT3X_TYPE_SHT4X, kSht3xTypes);
+      I2cSetActiveFound(sht3x_sensors[sht3x_count].address, sht3x_sensors[sht3x_count].typeString);
       sht3x_count++;
     }
   }
@@ -94,14 +270,29 @@ void Sht3xDetect(void)
 
 void Sht3xShow(bool json)
 {
+  bool result;
+  float t;
+  float h;
+  char types[11];
+
   for (uint32_t i = 0; i < sht3x_count; i++) {
-    float t;
-    float h;
-    if (Sht3xRead(t, h, sht3x_sensors[i].address)) {
-      char types[11];
-      strlcpy(types, sht3x_sensors[i].types, sizeof(types));
+    switch (sht3x_sensors[i].type) {
+      case SHT3X_TYPE_SHT3X:
+        result = Sht3xRead(t, h, sht3x_sensors[i].address);
+        break;
+      case SHT3X_TYPE_SHTCX:
+        result = ShtcxRead(t, h, sht3x_sensors[i].address);
+        break;
+      case SHT3X_TYPE_SHT4X:
+        result = Sht4xRead(t, h, sht3x_sensors[i].address);
+        break;
+    }
+    if (result) {
+      t = ConvertTemp(t);
+      h = ConvertHumidity(h);
+      strlcpy(types, sht3x_sensors[i].typeString, sizeof(types));
       if (sht3x_count > 1) {
-        snprintf_P(types, sizeof(types), PSTR("%s%c%02X"), sht3x_sensors[i].types, IndexSeparator(), sht3x_sensors[i].address);  // "SHT3X-0xXX"
+        snprintf_P(types, sizeof(types), PSTR("%s%c%02X"), sht3x_sensors[i].typeString, IndexSeparator(), sht3x_sensors[i].address);  // "SHT3X-0xXX"
       }
       TempHumDewShow(json, ((0 == TasmotaGlobal.tele_period) && (0 == i)), types, t, h);
     }
