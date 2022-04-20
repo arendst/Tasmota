71010423eb8a9f5024d91032b8e8ddbd77ae7d5c
Stephan Hadinger <stephan.hadinger@gmail.com>
2022-04-20 22:57:07 +0200

    Sync with latest Berry PRs

--- lib/libesp32/berry/src/be_object.h
+++ lib/libesp32/berry/src/be_object.h
@@ -44,7 +44,6 @@
 /* values for bproto.varg */
 #define BE_VA_VARARG    (1 << 0)    /* function has variable number of arguments */
 #define BE_VA_METHOD    (1 << 1)    /* function is a method (this is only a hint) */
-
 #define array_count(a)   (sizeof(a) / sizeof((a)[0]))
 
 #define bcommon_header          \
