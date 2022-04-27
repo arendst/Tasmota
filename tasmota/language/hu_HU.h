b5a0c5ca79f50e0fc29daa9e77e4ae73f357564c
Norbert Richter <norbert.richter@prsolution.eu>
2022-04-27 17:23:34 +0200

    Fix missing lang definition

--- tasmota/language/hu_HU.h
+++ tasmota/language/hu_HU.h
@@ -866,6 +866,7 @@
 #define D_GPIO_SHIFT595_SER    "74x595 SER"
 #define D_SENSOR_CM11_TX       "CM110x TX"
 #define D_SENSOR_CM11_RX       "CM110x RX"
+#define D_SENSOR_FLOWRATEMETER "Flowrate"
 
 // Units
 #define D_UNIT_AMPERE "A"
