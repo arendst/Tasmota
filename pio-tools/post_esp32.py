f789225e024f8b0d27634c76caa797700f725808
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2022-05-02 23:24:52 +0200

    Safemode refactor
    * Safemode refactor
    
    * Looks nicer
    
    * rm littlefs upload
    
    * Delete littlefs.bin
    
    * Create placeholder
    
    * Delete tasmota32-minicustom.bin
    
    * Delete tasmota32c3-minicustom.bin
    
    * rm littlefs
    
    * rename to *-safemode.bin

--- pio-tools/post_esp32.py
+++ pio-tools/post_esp32.py
@@ -18,21 +18,47 @@
 
 Import("env")
 
+env = DefaultEnvironment()
 platform = env.PioPlatform()
 
+from genericpath import exists
+import os
 import sys
 from os.path import join
 import csv
+import requests
+import shutil
 
 sys.path.append(join(platform.get_package_dir("tool-esptoolpy")))
 import esptool
+
+FRAMEWORK_DIR = platform.get_package_dir("framework-arduinoespressif32")
+safemode_dir = join(env["PROJECT_DIR"], "safemode")
+variants_dir = join(FRAMEWORK_DIR, "variants", "tasmota")
+
+
+def esp32_fetch_safemode_bin(chip):
+    safemode_fw_url = "https://github.com/Jason2866/Tasmota-specials/raw/firmware/firmware/tasmota32/other/tasmota" + chip[3:] + "-safemode.bin"
+    safemode_fw_name = safemode_dir + "/tasmota" + chip[3:] + "-safemode.bin"
+    if(exists(safemode_fw_name)):
+        print("Safemode binary already downloaded.")
+        return safemode_fw_name
+    print("Will download safemode binary from URL:")
+    print(safemode_fw_url)
+    response = requests.get(safemode_fw_url)
+    open(safemode_fw_name, "wb").write(response.content)
+    print("Safemode binary written to safemode dir.")
+    return safemode_fw_name
+
 
 def esp32_create_combined_bin(source, target, env):
     #print("Generating combined binary for serial flashing")
 
     # The offset from begin of the file where the app0 partition starts
     # This is defined in the partition .csv file
-    app_offset = 0x10000 # default value
+    factory_offset = -1      # error code value
+    app_offset = 0x10000     # default value for "old" scheme
+    spiffs_offset = -1       # error code value
     with open(env.BoardConfig().get("build.partitions")) as csv_file:
         print("Read partitions from ",env.BoardConfig().get("build.partitions"))
         csv_reader = csv.reader(csv_file, delimiter=',')
@@ -46,12 +72,17 @@ def esp32_create_combined_bin(source, target, env):
                 line_count += 1
                 if(row[0] == 'app0'):
                     app_offset = int(row[3],base=16)
+                elif(row[0] == 'factory'):
+                    factory_offset = int(row[3],base=16)
+                elif(row[0] == 'spiffs'):
+                    spiffs_offset = int(row[3],base=16)
 #                    print("Got app_offset from .csv:", row[3])
 
     new_file_name = env.subst("$BUILD_DIR/${PROGNAME}.factory.bin")
     sections = env.subst(env.get("FLASH_EXTRA_IMAGES"))
     firmware_name = env.subst("$BUILD_DIR/${PROGNAME}.bin")
     chip = env.get("BOARD_MCU")
+    esp32_fetch_safemode_bin(chip)
     flash_size = env.BoardConfig().get("upload.flash_size")
     cmd = [
         "--chip",
@@ -69,6 +100,14 @@ def esp32_create_combined_bin(source, target, env):
         print(f" -  {sect_adr} | {sect_file}")
         cmd += [sect_adr, sect_file]
 
+    if os.path.exists(safemode_dir):
+        #print("safemode.bin dir exists")
+        if os.path.exists(variants_dir):
+            #print("variants/tasmota exists")
+            shutil.rmtree(variants_dir)
+        shutil.copytree(safemode_dir, variants_dir)
+
+    # "main" firmware to app0 - mandatory
     print(f" - {hex(app_offset)} | {firmware_name}")
     cmd += [hex(app_offset), firmware_name]
 
