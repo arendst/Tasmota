34084c6e7521f2f9acc67809770b990007fc70c7
Christian Baars <christianbaars@MacBook-Pro-von-Christian.local>
2022-09-03 15:32:24 +0200

    generate berry structures before every build process

---
+++ pio-tools/gen-berry-structures.py
@@ -0,0 +1,12 @@
+Import("env")
+import os
+import subprocess
+from os.path import join
+
+# generate all precompiled Berry structures from multiple modules
+CURRENT_DIR = os.getcwd()
+BERRY_GEN_DIR = join(CURRENT_DIR, "lib", "libesp32","berry")
+os.chdir(BERRY_GEN_DIR)
+cmd = ("python3",join("tools","coc","coc"),"-o","generate","src","default",join("..","berry_tasmota","src"),join("..","berry_mapping","src"),join("..","berry_int64","src"),join("..","..","libesp32_lvgl","lv_binding_berry","src"),join("..","..","libesp32_lvgl","lv_binding_berry","generate"),"-c",join("default","berry_conf.h"))
+returncode = subprocess.call(cmd, shell=False)
+os.chdir(CURRENT_DIR)
