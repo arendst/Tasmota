cd68adbc40528ed88d513086a15d0c69a9197441
Theo Arends <11044339+arendst@users.noreply.github.com>
2022-09-06 12:24:39 +0200

    Fix python location
    Fix python location (#16425)

--- pio-tools/gen-berry-structures.py
+++ pio-tools/gen-berry-structures.py
@@ -9,6 +9,6 @@ from os.path import join
 CURRENT_DIR = os.getcwd()
 BERRY_GEN_DIR = join(env.subst("$PROJECT_DIR"), "lib", "libesp32","berry")
 os.chdir(BERRY_GEN_DIR)
-cmd = ("python3",join("tools","coc","coc"),"-o","generate","src","default",join("..","berry_tasmota","src"),join("..","berry_mapping","src"),join("..","berry_int64","src"),join("..","..","libesp32_lvgl","lv_binding_berry","src"),join("..","..","libesp32_lvgl","lv_binding_berry","generate"),"-c",join("default","berry_conf.h"))
+cmd = (env["PYTHONEXE"],join("tools","coc","coc"),"-o","generate","src","default",join("..","berry_tasmota","src"),join("..","berry_mapping","src"),join("..","berry_int64","src"),join("..","..","libesp32_lvgl","lv_binding_berry","src"),join("..","..","libesp32_lvgl","lv_binding_berry","generate"),"-c",join("default","berry_conf.h"))
 returncode = subprocess.call(cmd, shell=False)
 os.chdir(CURRENT_DIR)
