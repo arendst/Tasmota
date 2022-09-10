8ff3438979f0144d252a8bf92a8b3f2c4668f0f0
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2022-09-10 12:54:28 +0200

    add more cams via `.h` file (#310)

--- tasmota/tasmota_xdrv_driver/xdrv_81_esp32_webcam.ino
+++ tasmota/tasmota_xdrv_driver/xdrv_81_esp32_webcam.ino
@@ -92,7 +92,7 @@
  *
  * Only boards with PSRAM should be used.
  * To speed up cam processing cpu frequency should be better set to 240Mhz
- * 
+ *
  * remarks for AI-THINKER
  * GPIO0 zero must be disconnected from any wire after programming because this pin drives the cam clock and does
  * not tolerate any capictive load
@@ -109,32 +109,12 @@
 #include "esp_camera.h"
 #include "sensor.h"
 #include "fb_gfx.h"
+#include "camera_pins.h"
 
 bool HttpCheckPriviledgedAccess(bool);
 extern ESP8266WebServer *Webserver;
 
 #define BOUNDARY "e8b8c539-047d-4777-a985-fbba6edff11e"
-
-
-
-// CAMERA_MODEL_AI_THINKER default template pins
-#define PWDN_GPIO_NUM     32
-#define RESET_GPIO_NUM    -1
-#define XCLK_GPIO_NUM      0
-#define SIOD_GPIO_NUM     26
-#define SIOC_GPIO_NUM     27
-
-#define Y9_GPIO_NUM       35
-#define Y8_GPIO_NUM       34
-#define Y7_GPIO_NUM       39
-#define Y6_GPIO_NUM       36
-#define Y5_GPIO_NUM       21
-#define Y4_GPIO_NUM       19
-#define Y3_GPIO_NUM       18
-#define Y2_GPIO_NUM        5
-#define VSYNC_GPIO_NUM    25
-#define HREF_GPIO_NUM     23
-#define PCLK_GPIO_NUM     22
 
 #ifndef MAX_PICSTORE
 #define MAX_PICSTORE 4
