81bf1ef8b6536279eeb7a70c7060f099d4b76cc3
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2022-09-10 13:20:24 +0200

    no config if no model set

--- lib/libesp32/esp32-camera/driver/include/camera_pins.h
+++ lib/libesp32/esp32-camera/driver/include/camera_pins.h
@@ -333,5 +333,21 @@
 #define PCIE_RX_PIN    46
 #define PCIE_LED_PIN   21
 #else
-#error "Camera model not selected"
+#define PWDN_GPIO_NUM  -1
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM  -1
+#define SIOD_GPIO_NUM  -1
+#define SIOC_GPIO_NUM  -1
+
+#define Y9_GPIO_NUM    -1
+#define Y8_GPIO_NUM    -1
+#define Y7_GPIO_NUM    -1
+#define Y6_GPIO_NUM    -1
+#define Y5_GPIO_NUM    -1
+#define Y4_GPIO_NUM    -1
+#define Y3_GPIO_NUM    -1
+#define Y2_GPIO_NUM    -1
+#define VSYNC_GPIO_NUM -1
+#define HREF_GPIO_NUM  -1
+#define PCLK_GPIO_NUM  -1
 #endif
