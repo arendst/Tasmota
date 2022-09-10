9ec5009c6d19551530121c2d5f5a3ab0bc352ba3
Christian Baars <christianbaars@MacBook-Pro-von-Christian.local>
2022-09-10 21:10:39 +0200

    refactor MI32 functions to CTYPE_DECLARATION

--- lib/libesp32/berry_tasmota/src/be_MI32_lib.c
+++ lib/libesp32/berry_tasmota/src/be_MI32_lib.c
@@ -7,23 +7,34 @@
 #include "be_mapping.h"
 
 #ifdef USE_MI_ESP32
-extern int be_MI32_devices(bvm *vm);
-extern int be_MI32_set_bat(bvm *vm);
-extern int be_MI32_get_name(bvm *vm);
-extern int be_MI32_get_MAC(bvm *vm);
-extern int be_MI32_set_hum(bvm *vm);
-extern int be_MI32_set_temp(bvm *vm);
+extern int be_MI32_devices(void);
+BE_FUNC_CTYPE_DECLARE(be_MI32_devices, "i", "");
+
+extern void be_MI32_set_bat(int slot, int bat_val);
+BE_FUNC_CTYPE_DECLARE(be_MI32_set_bat, "", "ii");
+
+extern const char * be_MI32_get_name(int slot);
+BE_FUNC_CTYPE_DECLARE(be_MI32_get_name, "s", "i");
+
+extern uint8_t *be_MI32_get_MAC(int32_t slot, size_t *size);
+BE_FUNC_CTYPE_DECLARE(be_MI32_get_MAC, "&", "i");
+
+extern void be_MI32_set_hum(int slot, int hum_val);
+BE_FUNC_CTYPE_DECLARE(be_MI32_set_hum, "", "ii");
+
+extern void be_MI32_set_temp(int slot, int temp_val);
+BE_FUNC_CTYPE_DECLARE(be_MI32_set_temp, "", "ii");
 
 #include "be_fixed_MI32.h"
 
 /* @const_object_info_begin
 module MI32 (scope: global) {
-  devices,    func(be_MI32_devices)
-  get_name,   func(be_MI32_get_name)
-  get_MAC,    func(be_MI32_get_MAC)
-  set_bat,    func(be_MI32_set_bat)
-  set_hum,    func(be_MI32_set_hum)
-  set_temp,   func(be_MI32_set_temp)
+  devices,    ctype_func(be_MI32_devices)
+  get_name,   ctype_func(be_MI32_get_name)
+  get_MAC,    ctype_func(be_MI32_get_MAC)
+  set_bat,    ctype_func(be_MI32_set_bat)
+  set_hum,    ctype_func(be_MI32_set_hum)
+  set_temp,   ctype_func(be_MI32_set_temp)
 }
 @const_object_info_end */
 
