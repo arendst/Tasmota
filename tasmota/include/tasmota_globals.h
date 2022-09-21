a6d4fe1e46eb6a8253e9f150fe0aac30d25b6313
ortegafernando <ortega.fernando@gmail.com>
2022-09-21 09:27:19 +0200

    Add SetOption157
    Add SetOption147  (MQTT) Enable publish SSerialReceived MQTT messages. If disabled, use event rules instead.
    If it is disabled, tasmota will not publish an MQTT message every time a SSerialReceived event fires. Use rules to control how and when publish these messages.

--- tasmota/include/tasmota_globals.h
+++ tasmota/include/tasmota_globals.h
@@ -283,6 +283,9 @@ String EthernetMacAddress(void);
 #ifndef MQTT_CLEAN_SESSION
 #define MQTT_CLEAN_SESSION          1          // 0 = No clean session, 1 = Clean session (default)
 #endif
+#ifndef MQTT_SSERIALRECEIVED
+#define MQTT_SSERIALRECEIVED        1          // 0 = Disable sserialreceived mqtt messages, 1 = Enable sserialreceived mqtt messages (default)
+#endif
 #ifndef MQTT_LWT_OFFLINE
 #define MQTT_LWT_OFFLINE            "Offline"  // MQTT LWT offline topic message
 #endif
