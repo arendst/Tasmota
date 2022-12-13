3552e0a00293d38fa0e9b48913924cfe1332d244
Pascal Gollor <pascal@pgollor.de>
2022-12-13 13:22:03 +0100

    work with newer espupload script and remove old one

--- pio-tools/http-uploader.py
+++ pio-tools/http-uploader.py
@@ -1,13 +1,23 @@
+# createt by Theo Arends at 2017-09-30
+# modified by mtfurlan at 2010-01-03
+# modified by Pascal Gollor at 2022-12-13
+
 Import("env")
 import os
+import tasmotapiolib
 
-# pio < 4.0.0
-# from base64 import b64decode
-# env.Replace(UPLOADER="pio-tools\espupload.py")
-# env.Replace(UPLOADERFLAGS="")
-# env.Replace(UPLOADCMD="$UPLOADER -u " + b64decode(ARGUMENTS.get("UPLOAD_PORT")) + " -f $SOURCES")
 
-# pio >= 4.0.0
-env.Replace(UPLOADER=os.path.join("pio-tools", "espupload.py"))
+# clear upload flags
 env.Replace(UPLOADERFLAGS="")
-env.Replace(UPLOADCMD="$UPLOADER -u $UPLOAD_PORT -f $SOURCES")
+
+# get espupload script
+uploader=os.path.join("tools", "espupload.py")
+
+# get firmware file
+upload_file = tasmotapiolib.get_final_bin_path(env)
+
+# create upload command like:
+# esptool.py -i <server ip> -p <server port> -u <uploader url script> -f <source file>
+# so you need to specify 'upload_port' in platform_override.ini at '[env]' section
+env.Replace(UPLOADCMD="{} $UPLOAD_PORT -f {}".format(uploader, upload_file))
+
