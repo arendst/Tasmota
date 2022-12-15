84ebad290a216beb0af45cceb2daa781bcb37119
sedgi <james@jsedgwick.com>
2022-12-15 00:47:51 +0000

    ArtNet DMX - Added RGBWW support for single lights

--- tasmota/tasmota_xdrv_driver/xdrv_04_light_artnet.ino
+++ tasmota/tasmota_xdrv_driver/xdrv_04_light_artnet.ino
@@ -197,11 +197,20 @@ void ArtNetProcessPacket(uint8_t * buf, size_t len) {
     uint8_t r8 = buf[idx+1];
     uint8_t g8 = buf[idx];
     uint8_t b8 = buf[idx+2];
-    uint16_t dimmer10 = changeUIntScale(artnet_conf.dimm, 0, 100, 0, 1023);
+    uint8_t w8 = buf[idx+3];
+    uint8_t ww8 = buf[idx+4];
+    // scale dimmer values to RGBWWTable calibration
+    uint16_t r_dimmer = changeUIntScale(artnet_conf.dimm, 0, 100, 0, Settings->rgbwwTable[0]) * 4;
+    uint16_t g_dimmer = changeUIntScale(artnet_conf.dimm, 0, 100, 0, Settings->rgbwwTable[1]) * 4;
+    uint16_t b_dimmer = changeUIntScale(artnet_conf.dimm, 0, 100, 0, Settings->rgbwwTable[2]) * 4;
+    uint16_t w_dimmer = changeUIntScale(artnet_conf.dimm, 0, 100, 0, Settings->rgbwwTable[3]) * 4;
+    uint16_t ww_dimmer = changeUIntScale(artnet_conf.dimm, 0, 100, 0, Settings->rgbwwTable[4]) * 4;
     uint16_t color[LST_MAX] = {0};
-    color[0] = changeUIntScale(r8, 0, 255, 0, dimmer10);
-    color[1] = changeUIntScale(g8, 0, 255, 0, dimmer10);
-    color[2] = changeUIntScale(b8, 0, 255, 0, dimmer10);
+    color[0] = changeUIntScale(r8, 0, 255, 0, r_dimmer);
+    color[1] = changeUIntScale(g8, 0, 255, 0, g_dimmer);
+    color[2] = changeUIntScale(b8, 0, 255, 0, b_dimmer);
+    color[3] = changeUIntScale(w8, 0, 255, 0, w_dimmer);
+    color[4] = changeUIntScale(ww8, 0, 255, 0, ww_dimmer);
     // AddLog(LOG_LEVEL_DEBUG_MORE, PSTR("DMX: %02X-%02X-%02X univ=%i rows=%i max_univ=%i"), buf[idx+1], buf[idx], buf[idx+2], universe, row, artnet_conf.univ + artnet_conf.rows);
     LightSetOutputs(color);
   }
