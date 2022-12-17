73883978e2ab0ad8289c1a9552acebaaeba293cb
Stephan Hadinger <stephan.hadinger@gmail.com>
2022-12-17 23:22:31 +0100

    Berry support for `crypto.SHA256`

--- lib/libesp32/berry_tasmota/src/be_crypto_lib.c
+++ lib/libesp32/berry_tasmota/src/be_crypto_lib.c
@@ -15,8 +15,13 @@ extern int m_aes_gcm_tag(bvm *vm);
 extern int m_ec_c25519_pubkey(bvm *vm);
 extern int m_ec_c25519_sharedkey(bvm *vm);
 
+extern int m_hash_sha256_init(bvm *vm);
+extern int m_hash_sha256_update(bvm *vm);
+extern int m_hash_sha256_out(bvm *vm);
+
 #include "be_fixed_be_class_aes_gcm.h"
 #include "be_fixed_be_class_ec_c25519.h"
+#include "be_fixed_be_class_sha256.h"
 #include "be_fixed_crypto.h"
 
 /* @const_object_info_begin
@@ -36,9 +41,18 @@ class be_class_ec_c25519 (scope: global, name: EC_C25519) {
     shared_key, func(m_ec_c25519_sharedkey)
 }
 
+class be_class_sha256 (scope: global, name: SHA256) {
+    .p, var
+
+    init, func(m_hash_sha256_init)
+    update, func(m_hash_sha256_update)
+    out, func(m_hash_sha256_out)
+}
+
 module crypto (scope: global) {
   AES_GCM, class(be_class_aes_gcm)
   EC_C25519, class(be_class_ec_c25519)
+  SHA256, class(be_class_sha256)
 }
 
 @const_object_info_end */
