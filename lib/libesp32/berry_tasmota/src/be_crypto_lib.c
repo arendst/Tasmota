d670f476ba977990d1f09ec0ebd989ea7a3845cd
s-hadinger <49731213+s-hadinger@users.noreply.github.com>
2022-12-19 22:30:54 +0100

    Berry add crypto AES_CTR, HDMAC_SHA256, MD5 (#17451)

--- lib/libesp32/berry_tasmota/src/be_crypto_lib.c
+++ lib/libesp32/berry_tasmota/src/be_crypto_lib.c
@@ -12,6 +12,10 @@ extern int m_aes_gcm_encryt(bvm *vm);
 extern int m_aes_gcm_decryt(bvm *vm);
 extern int m_aes_gcm_tag(bvm *vm);
 
+extern int m_aes_ctr_init(bvm *vm);
+extern int m_aes_ctr_run(bvm *vm);
+extern int m_aes_ctr_tag(bvm *vm);
+
 extern int m_ec_c25519_pubkey(bvm *vm);
 extern int m_ec_c25519_sharedkey(bvm *vm);
 
@@ -19,9 +23,17 @@ extern int m_hash_sha256_init(bvm *vm);
 extern int m_hash_sha256_update(bvm *vm);
 extern int m_hash_sha256_out(bvm *vm);
 
+extern int m_hmac_sha256_init(bvm *vm);
+extern int m_hmac_sha256_update(bvm *vm);
+extern int m_hmac_sha256_out(bvm *vm);
+
+extern const bclass be_class_md5;
+
 #include "be_fixed_be_class_aes_gcm.h"
+#include "be_fixed_be_class_aes_ctr.h"
 #include "be_fixed_be_class_ec_c25519.h"
 #include "be_fixed_be_class_sha256.h"
+#include "be_fixed_be_class_hmac_sha256.h"
 #include "be_fixed_crypto.h"
 
 /* @const_object_info_begin
@@ -34,6 +46,14 @@ class be_class_aes_gcm (scope: global, name: AES_GCM) {
     encrypt, func(m_aes_gcm_encryt)
     decrypt, func(m_aes_gcm_decryt)
     tag, func(m_aes_gcm_tag)
+}
+
+class be_class_aes_ctr (scope: global, name: AES_CTR) {
+    .p1, var
+
+    init, func(m_aes_ctr_init)
+    encrypt, func(m_aes_ctr_run)
+    decrypt, func(m_aes_ctr_run)
 }
 
 class be_class_ec_c25519 (scope: global, name: EC_C25519) {
@@ -49,10 +69,21 @@ class be_class_sha256 (scope: global, name: SHA256) {
     out, func(m_hash_sha256_out)
 }
 
+class be_class_hmac_sha256 (scope: global, name: HMAC_SHA256) {
+    .p, var
+
+    init, func(m_hmac_sha256_init)
+    update, func(m_hmac_sha256_update)
+    out, func(m_hmac_sha256_out)
+}
+
 module crypto (scope: global) {
   AES_GCM, class(be_class_aes_gcm)
+  AES_CTR, class(be_class_aes_ctr)
   EC_C25519, class(be_class_ec_c25519)
   SHA256, class(be_class_sha256)
+  HMAC_SHA256, class(be_class_hmac_sha256)
+  MD5, class(be_class_md5)
 }
 
 @const_object_info_end */
