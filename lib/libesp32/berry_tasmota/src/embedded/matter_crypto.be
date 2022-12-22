5513bcba9ed82906571d00b84065e0432fe3d705
Stephan Hadinger <stephan.hadinger@gmail.com>
2022-12-22 11:47:06 +0100

    working

--- lib/libesp32/berry_tasmota/src/embedded/matter_crypto.be
+++ lib/libesp32/berry_tasmota/src/embedded/matter_crypto.be
@@ -14,18 +14,19 @@ class Matter_Crypto
   var w1
   var L
   # value for prover
-  var pA
+  var pA, pB, Z, V
+  var x, y
   # values from the Matter specification
   static var CRYPTO_GROUP_SIZE_BYTES = 32
   static var CRYPTO_W_SIZE_BYTES = 40         # CRYPTO_GROUP_SIZE_BYTES + 8
   static var spake_M_hex = "04886e2f97ace46e55ba9dd7242579f2993b64e16ef3dcab95afd497333d8fa12f5ff355163e43ce224e0b0e65ff02ac8e5c7be09419c785e0ca547d55a12e2d20"
   static var spake_N_hex = "04d8bbd6c639c62937b04d997f38c3770719c629d7014d49a24b4f98baa1292b4907d60aa6bfade45008a636337f5168c64d9bd36034808cd564490b1e656edbe7"
-  var spake_M, spake_N    # due to the nature of solification, we can't solidify the bytes versions nor store as static variable
+  var M, N    # due to the nature of solification, we can't solidify the bytes versions nor store as static variable
 
   # init does not do much
   def init()
-    self.spake_M = bytes().fromhex(self.spake_M_hex)
-    self.spake_N = bytes().fromhex(self.spake_N_hex)
+    self.M = bytes().fromhex(self.spake_M_hex)
+    self.N = bytes().fromhex(self.spake_N_hex)
   end
 
   # set device information
@@ -54,14 +55,54 @@ class Matter_Crypto
     self.L = ec.public_key(self.w1)
   end
 
-  # compute the shared values
+  # compute the shared values pA
   # w0 must be known
   # x is a random 32 bytes vector. It does not need to be lower than the order (we take the modulus anyways)
   def compute_pA(x)
     import crypto
     var ec = crypto.EC_P256()
-    x = ec.mod(x)
-    self.pA = ec.muladd(self.w0, self.spake_M, x, bytes() #- empty means generator -#)    # compute x*G+w0*M
+    self.x = ec.mod(x)
+    self.pA = ec.muladd(self.w0, self.M, self.x, bytes() #- empty means generator -#)    # compute x*P+w0*M
+  end
+
+  # compute the shared values pB
+  # w0 must be known
+  # x is a random 32 bytes vector. It does not need to be lower than the order (we take the modulus anyways)
+  def compute_pB(y)
+    import crypto
+    var ec = crypto.EC_P256()
+    self.y = ec.mod(y)
+    self.pB = ec.muladd(self.w0, self.N, self.y, bytes() #- empty means generator -#)    # compute y*P+w0*M
+  end
+
+  # compute Z and V for the Prover, receiving pB from the verifier
+  # pB must be bytes(65)
+  # x must be known
+  def compute_ZV_prover(pB)
+    import crypto
+    var ec = crypto.EC_P256()
+    self.pB = pB
+    # we are supposed to check that the point is on the curve -- TODO
+    # we need a substraction so we negate w0
+    var w0_neg = ec.neg(self.w0)
+    var t_inner = ec.muladd(bytes("01"), self.pB, w0_neg, self.N)    # compute 1*pB-w0*N
+    self.Z = ec.mul(self.x, t_inner)
+    self.V = ec.mul(self.w1, t_inner)
+  end
+
+  # compute Z and V for the Verifier, receiving pA from the prover
+  # pA must be bytes(65)
+  # y must be known
+  def compute_ZV_verifier(pA)
+    import crypto
+    var ec = crypto.EC_P256()
+    self.pA = pA
+    # we are supposed to check that the point is on the curve -- TODO
+    # we need a substraction so we negate w0
+    var w0_neg = ec.neg(self.w0)
+    var t_inner = ec.muladd(bytes("01"), self.pA, w0_neg, self.M)    # compute 1*pA-w0*M
+    self.Z = ec.mul(self.y, t_inner)
+    self.V = ec.mul(self.y, self.L)
   end
 end
 
@@ -96,6 +137,19 @@ mc.L  = bytes("04eb7c9db3d9a9eb1f8adab81b5794c1f13ae3e225efbe91ea487425854c7fc00
 # x is supposed to be random 32 bytes, test vector sets it
 var x = bytes("d1232c8e8693d02368976c174e2088851b8365d0d79a9eee709c6a05a2fad539")
 mc.compute_pA(x)
-
 assert(mc.pA == bytes('04EF3BD051BF78A2234EC0DF197F7828060FE9856503579BB1733009042C15C0C1DE127727F418B5966AFADFDD95A6E4591D171056B333DAB97A79C7193E341727'))
+
+# y is supposed to be random 32 bytes, test vector sets it
+var y = bytes("717a72348a182085109c8d3917d6c43d59b224dc6a7fc4f0483232fa6516d8b3")
+mc.compute_pB(y)
+assert(mc.pB == bytes('04C0F65DA0D11927BDF5D560C69E1D7D939A05B0E88291887D679FCADEA75810FB5CC1CA7494DB39E82FF2F50665255D76173E09986AB46742C798A9A68437B048'))
+
+mc.compute_ZV_prover(mc.pB)
+assert(mc.Z == bytes('04BBFCE7DD7F277819C8DA21544AFB7964705569BDF12FB92AA388059408D50091A0C5F1D3127F56813B5337F9E4E67E2CA633117A4FBD559946AB474356C41839'))
+assert(mc.V == bytes('0458BF27C6BCA011C9CE1930E8984A797A3419797B936629A5A937CF2F11C8B9514B82B993DA8A46E664F23DB7C01EDC87FAA530DB01C2EE405230B18997F16B68'))
+
+mc.compute_ZV_verifier(mc.pA)
+assert(mc.Z == bytes('04BBFCE7DD7F277819C8DA21544AFB7964705569BDF12FB92AA388059408D50091A0C5F1D3127F56813B5337F9E4E67E2CA633117A4FBD559946AB474356C41839'))
+assert(mc.V == bytes('0458BF27C6BCA011C9CE1930E8984A797A3419797B936629A5A937CF2F11C8B9514B82B993DA8A46E664F23DB7C01EDC87FAA530DB01C2EE405230B18997F16B68'))
+
 -#
--- lib/libesp32/berry_tasmota/src/solidify/solidified_matter_crypto.h
+++ lib/libesp32/berry_tasmota/src/solidify/solidified_matter_crypto.h
@@ -5,9 +5,9 @@
 #include "be_constobj.h"
 
 /********************************************************************
-** Solidified function: compute_pA
+** Solidified function: compute_pB
 ********************************************************************/
-be_local_closure(Matter_Crypto_compute_pA,   /* name */
+be_local_closure(Matter_Crypto_compute_pB,   /* name */
   be_nested_proto(
     10,                          /* nstack */
     2,                          /* argc */
@@ -17,76 +17,35 @@ be_local_closure(Matter_Crypto_compute_pA,   /* name */
     0,                          /* has sup protos */
     NULL,                       /* no sub protos */
     1,                          /* has constants */
-    ( &(const bvalue[ 7]) {     /* constants */
+    ( &(const bvalue[ 8]) {     /* constants */
     /* K0   */  be_nested_str_weak(crypto),
     /* K1   */  be_nested_str_weak(EC_P256),
-    /* K2   */  be_nested_str_weak(mod),
-    /* K3   */  be_nested_str_weak(pA),
-    /* K4   */  be_nested_str_weak(muladd),
-    /* K5   */  be_nested_str_weak(w0),
-    /* K6   */  be_nested_str_weak(spake_M),
+    /* K2   */  be_nested_str_weak(y),
+    /* K3   */  be_nested_str_weak(mod),
+    /* K4   */  be_nested_str_weak(pB),
+    /* K5   */  be_nested_str_weak(muladd),
+    /* K6   */  be_nested_str_weak(w0),
+    /* K7   */  be_nested_str_weak(N),
     }),
-    be_str_weak(compute_pA),
+    be_str_weak(compute_pB),
     &be_const_str_solidified,
     ( &(const binstruction[16]) {  /* code */
       0xA40A0000,  //  0000  IMPORT	R2	K0
       0x8C0C0501,  //  0001  GETMET	R3	R2	K1
       0x7C0C0200,  //  0002  CALL	R3	1
-      0x8C100702,  //  0003  GETMET	R4	R3	K2
+      0x8C100703,  //  0003  GETMET	R4	R3	K3
       0x5C180200,  //  0004  MOVE	R6	R1
       0x7C100400,  //  0005  CALL	R4	2
-      0x5C040800,  //  0006  MOVE	R1	R4
-      0x8C100704,  //  0007  GETMET	R4	R3	K4
-      0x88180105,  //  0008  GETMBR	R6	R0	K5
-      0x881C0106,  //  0009  GETMBR	R7	R0	K6
-      0x5C200200,  //  000A  MOVE	R8	R1
+      0x90020404,  //  0006  SETMBR	R0	K2	R4
+      0x8C100705,  //  0007  GETMET	R4	R3	K5
+      0x88180106,  //  0008  GETMBR	R6	R0	K6
+      0x881C0107,  //  0009  GETMBR	R7	R0	K7
+      0x88200102,  //  000A  GETMBR	R8	R0	K2
       0x60240015,  //  000B  GETGBL	R9	G21
       0x7C240000,  //  000C  CALL	R9	0
       0x7C100A00,  //  000D  CALL	R4	5
-      0x90020604,  //  000E  SETMBR	R0	K3	R4
+      0x90020804,  //  000E  SETMBR	R0	K4	R4
       0x80000000,  //  000F  RET	0
-    })
-  )
-);
-/*******************************************************************/
-
-
-/********************************************************************
-** Solidified function: init
-********************************************************************/
-be_local_closure(Matter_Crypto_init,   /* name */
-  be_nested_proto(
-    4,                          /* nstack */
-    1,                          /* argc */
-    2,                          /* varg */
-    0,                          /* has upvals */
-    NULL,                       /* no upvals */
-    0,                          /* has sup protos */
-    NULL,                       /* no sub protos */
-    1,                          /* has constants */
-    ( &(const bvalue[ 5]) {     /* constants */
-    /* K0   */  be_nested_str_weak(spake_M),
-    /* K1   */  be_nested_str_weak(fromhex),
-    /* K2   */  be_nested_str_weak(spake_M_hex),
-    /* K3   */  be_nested_str_weak(spake_N),
-    /* K4   */  be_nested_str_weak(spake_N_hex),
-    }),
-    be_str_weak(init),
-    &be_const_str_solidified,
-    ( &(const binstruction[13]) {  /* code */
-      0x60040015,  //  0000  GETGBL	R1	G21
-      0x7C040000,  //  0001  CALL	R1	0
-      0x8C040301,  //  0002  GETMET	R1	R1	K1
-      0x880C0102,  //  0003  GETMBR	R3	R0	K2
-      0x7C040400,  //  0004  CALL	R1	2
-      0x90020001,  //  0005  SETMBR	R0	K0	R1
-      0x60040015,  //  0006  GETGBL	R1	G21
-      0x7C040000,  //  0007  CALL	R1	0
-      0x8C040301,  //  0008  GETMET	R1	R1	K1
-      0x880C0104,  //  0009  GETMBR	R3	R0	K4
-      0x7C040400,  //  000A  CALL	R1	2
-      0x90020601,  //  000B  SETMBR	R0	K3	R1
-      0x80000000,  //  000C  RET	0
     })
   )
 );
@@ -139,6 +98,222 @@ be_local_closure(Matter_Crypto_set_device_info,   /* name */
       0x90020602,  //  0014  SETMBR	R0	K3	R2
       0x90020803,  //  0015  SETMBR	R0	K4	R3
       0x80000000,  //  0016  RET	0
+    })
+  )
+);
+/*******************************************************************/
+
+
+/********************************************************************
+** Solidified function: compute_ZV_verifier
+********************************************************************/
+be_local_closure(Matter_Crypto_compute_ZV_verifier,   /* name */
+  be_nested_proto(
+    11,                          /* nstack */
+    2,                          /* argc */
+    2,                          /* varg */
+    0,                          /* has upvals */
+    NULL,                       /* no upvals */
+    0,                          /* has sup protos */
+    NULL,                       /* no sub protos */
+    1,                          /* has constants */
+    ( &(const bvalue[13]) {     /* constants */
+    /* K0   */  be_nested_str_weak(crypto),
+    /* K1   */  be_nested_str_weak(EC_P256),
+    /* K2   */  be_nested_str_weak(pA),
+    /* K3   */  be_nested_str_weak(neg),
+    /* K4   */  be_nested_str_weak(w0),
+    /* K5   */  be_nested_str_weak(muladd),
+    /* K6   */  be_nested_str_weak(01),
+    /* K7   */  be_nested_str_weak(M),
+    /* K8   */  be_nested_str_weak(Z),
+    /* K9   */  be_nested_str_weak(mul),
+    /* K10  */  be_nested_str_weak(y),
+    /* K11  */  be_nested_str_weak(V),
+    /* K12  */  be_nested_str_weak(L),
+    }),
+    be_str_weak(compute_ZV_verifier),
+    &be_const_str_solidified,
+    ( &(const binstruction[26]) {  /* code */
+      0xA40A0000,  //  0000  IMPORT	R2	K0
+      0x8C0C0501,  //  0001  GETMET	R3	R2	K1
+      0x7C0C0200,  //  0002  CALL	R3	1
+      0x90020401,  //  0003  SETMBR	R0	K2	R1
+      0x8C100703,  //  0004  GETMET	R4	R3	K3
+      0x88180104,  //  0005  GETMBR	R6	R0	K4
+      0x7C100400,  //  0006  CALL	R4	2
+      0x8C140705,  //  0007  GETMET	R5	R3	K5
+      0x601C0015,  //  0008  GETGBL	R7	G21
+      0x58200006,  //  0009  LDCONST	R8	K6
+      0x7C1C0200,  //  000A  CALL	R7	1
+      0x88200102,  //  000B  GETMBR	R8	R0	K2
+      0x5C240800,  //  000C  MOVE	R9	R4
+      0x88280107,  //  000D  GETMBR	R10	R0	K7
+      0x7C140A00,  //  000E  CALL	R5	5
+      0x8C180709,  //  000F  GETMET	R6	R3	K9
+      0x8820010A,  //  0010  GETMBR	R8	R0	K10
+      0x5C240A00,  //  0011  MOVE	R9	R5
+      0x7C180600,  //  0012  CALL	R6	3
+      0x90021006,  //  0013  SETMBR	R0	K8	R6
+      0x8C180709,  //  0014  GETMET	R6	R3	K9
+      0x8820010A,  //  0015  GETMBR	R8	R0	K10
+      0x8824010C,  //  0016  GETMBR	R9	R0	K12
+      0x7C180600,  //  0017  CALL	R6	3
+      0x90021606,  //  0018  SETMBR	R0	K11	R6
+      0x80000000,  //  0019  RET	0
+    })
+  )
+);
+/*******************************************************************/
+
+
+/********************************************************************
+** Solidified function: init
+********************************************************************/
+be_local_closure(Matter_Crypto_init,   /* name */
+  be_nested_proto(
+    4,                          /* nstack */
+    1,                          /* argc */
+    2,                          /* varg */
+    0,                          /* has upvals */
+    NULL,                       /* no upvals */
+    0,                          /* has sup protos */
+    NULL,                       /* no sub protos */
+    1,                          /* has constants */
+    ( &(const bvalue[ 5]) {     /* constants */
+    /* K0   */  be_nested_str_weak(M),
+    /* K1   */  be_nested_str_weak(fromhex),
+    /* K2   */  be_nested_str_weak(spake_M_hex),
+    /* K3   */  be_nested_str_weak(N),
+    /* K4   */  be_nested_str_weak(spake_N_hex),
+    }),
+    be_str_weak(init),
+    &be_const_str_solidified,
+    ( &(const binstruction[13]) {  /* code */
+      0x60040015,  //  0000  GETGBL	R1	G21
+      0x7C040000,  //  0001  CALL	R1	0
+      0x8C040301,  //  0002  GETMET	R1	R1	K1
+      0x880C0102,  //  0003  GETMBR	R3	R0	K2
+      0x7C040400,  //  0004  CALL	R1	2
+      0x90020001,  //  0005  SETMBR	R0	K0	R1
+      0x60040015,  //  0006  GETGBL	R1	G21
+      0x7C040000,  //  0007  CALL	R1	0
+      0x8C040301,  //  0008  GETMET	R1	R1	K1
+      0x880C0104,  //  0009  GETMBR	R3	R0	K4
+      0x7C040400,  //  000A  CALL	R1	2
+      0x90020601,  //  000B  SETMBR	R0	K3	R1
+      0x80000000,  //  000C  RET	0
+    })
+  )
+);
+/*******************************************************************/
+
+
+/********************************************************************
+** Solidified function: compute_ZV_prover
+********************************************************************/
+be_local_closure(Matter_Crypto_compute_ZV_prover,   /* name */
+  be_nested_proto(
+    11,                          /* nstack */
+    2,                          /* argc */
+    2,                          /* varg */
+    0,                          /* has upvals */
+    NULL,                       /* no upvals */
+    0,                          /* has sup protos */
+    NULL,                       /* no sub protos */
+    1,                          /* has constants */
+    ( &(const bvalue[13]) {     /* constants */
+    /* K0   */  be_nested_str_weak(crypto),
+    /* K1   */  be_nested_str_weak(EC_P256),
+    /* K2   */  be_nested_str_weak(pB),
+    /* K3   */  be_nested_str_weak(neg),
+    /* K4   */  be_nested_str_weak(w0),
+    /* K5   */  be_nested_str_weak(muladd),
+    /* K6   */  be_nested_str_weak(01),
+    /* K7   */  be_nested_str_weak(N),
+    /* K8   */  be_nested_str_weak(Z),
+    /* K9   */  be_nested_str_weak(mul),
+    /* K10  */  be_nested_str_weak(x),
+    /* K11  */  be_nested_str_weak(V),
+    /* K12  */  be_nested_str_weak(w1),
+    }),
+    be_str_weak(compute_ZV_prover),
+    &be_const_str_solidified,
+    ( &(const binstruction[26]) {  /* code */
+      0xA40A0000,  //  0000  IMPORT	R2	K0
+      0x8C0C0501,  //  0001  GETMET	R3	R2	K1
+      0x7C0C0200,  //  0002  CALL	R3	1
+      0x90020401,  //  0003  SETMBR	R0	K2	R1
+      0x8C100703,  //  0004  GETMET	R4	R3	K3
+      0x88180104,  //  0005  GETMBR	R6	R0	K4
+      0x7C100400,  //  0006  CALL	R4	2
+      0x8C140705,  //  0007  GETMET	R5	R3	K5
+      0x601C0015,  //  0008  GETGBL	R7	G21
+      0x58200006,  //  0009  LDCONST	R8	K6
+      0x7C1C0200,  //  000A  CALL	R7	1
+      0x88200102,  //  000B  GETMBR	R8	R0	K2
+      0x5C240800,  //  000C  MOVE	R9	R4
+      0x88280107,  //  000D  GETMBR	R10	R0	K7
+      0x7C140A00,  //  000E  CALL	R5	5
+      0x8C180709,  //  000F  GETMET	R6	R3	K9
+      0x8820010A,  //  0010  GETMBR	R8	R0	K10
+      0x5C240A00,  //  0011  MOVE	R9	R5
+      0x7C180600,  //  0012  CALL	R6	3
+      0x90021006,  //  0013  SETMBR	R0	K8	R6
+      0x8C180709,  //  0014  GETMET	R6	R3	K9
+      0x8820010C,  //  0015  GETMBR	R8	R0	K12
+      0x5C240A00,  //  0016  MOVE	R9	R5
+      0x7C180600,  //  0017  CALL	R6	3
+      0x90021606,  //  0018  SETMBR	R0	K11	R6
+      0x80000000,  //  0019  RET	0
+    })
+  )
+);
+/*******************************************************************/
+
+
+/********************************************************************
+** Solidified function: compute_pA
+********************************************************************/
+be_local_closure(Matter_Crypto_compute_pA,   /* name */
+  be_nested_proto(
+    10,                          /* nstack */
+    2,                          /* argc */
+    2,                          /* varg */
+    0,                          /* has upvals */
+    NULL,                       /* no upvals */
+    0,                          /* has sup protos */
+    NULL,                       /* no sub protos */
+    1,                          /* has constants */
+    ( &(const bvalue[ 8]) {     /* constants */
+    /* K0   */  be_nested_str_weak(crypto),
+    /* K1   */  be_nested_str_weak(EC_P256),
+    /* K2   */  be_nested_str_weak(x),
+    /* K3   */  be_nested_str_weak(mod),
+    /* K4   */  be_nested_str_weak(pA),
+    /* K5   */  be_nested_str_weak(muladd),
+    /* K6   */  be_nested_str_weak(w0),
+    /* K7   */  be_nested_str_weak(M),
+    }),
+    be_str_weak(compute_pA),
+    &be_const_str_solidified,
+    ( &(const binstruction[16]) {  /* code */
+      0xA40A0000,  //  0000  IMPORT	R2	K0
+      0x8C0C0501,  //  0001  GETMET	R3	R2	K1
+      0x7C0C0200,  //  0002  CALL	R3	1
+      0x8C100703,  //  0003  GETMET	R4	R3	K3
+      0x5C180200,  //  0004  MOVE	R6	R1
+      0x7C100400,  //  0005  CALL	R4	2
+      0x90020404,  //  0006  SETMBR	R0	K2	R4
+      0x8C100705,  //  0007  GETMET	R4	R3	K5
+      0x88180106,  //  0008  GETMBR	R6	R0	K6
+      0x881C0107,  //  0009  GETMBR	R7	R0	K7
+      0x88200102,  //  000A  GETMBR	R8	R0	K2
+      0x60240015,  //  000B  GETGBL	R9	G21
+      0x7C240000,  //  000C  CALL	R9	0
+      0x7C100A00,  //  000D  CALL	R4	5
+      0x90020804,  //  000E  SETMBR	R0	K4	R4
+      0x80000000,  //  000F  RET	0
     })
   )
 );
@@ -231,27 +406,35 @@ be_local_closure(Matter_Crypto_compute_w0_w1,   /* name */
 ** Solidified class: Matter_Crypto
 ********************************************************************/
 be_local_class(Matter_Crypto,
-    9,
+    14,
     NULL,
-    be_nested_map(17,
+    be_nested_map(25,
     ( (struct bmapnode*) &(const bmapnode[]) {
-        { be_const_key_weak(iterations, -1), be_const_var(2) },
-        { be_const_key_weak(compute_pA, 7), be_const_closure(Matter_Crypto_compute_pA_closure) },
+        { be_const_key_weak(Z, 24), be_const_var(8) },
+        { be_const_key_weak(salt, 23), be_const_var(1) },
         { be_const_key_weak(spake_M_hex, -1), be_nested_str_weak(04886e2f97ace46e55ba9dd7242579f2993b64e16ef3dcab95afd497333d8fa12f5ff355163e43ce224e0b0e65ff02ac8e5c7be09419c785e0ca547d55a12e2d20) },
-        { be_const_key_weak(salt, -1), be_const_var(1) },
-        { be_const_key_weak(spake_N_hex, -1), be_nested_str_weak(04d8bbd6c639c62937b04d997f38c3770719c629d7014d49a24b4f98baa1292b4907d60aa6bfade45008a636337f5168c64d9bd36034808cd564490b1e656edbe7) },
-        { be_const_key_weak(compute_w0_w1, 9), be_const_closure(Matter_Crypto_compute_w0_w1_closure) },
-        { be_const_key_weak(spake_N, 5), be_const_var(8) },
-        { be_const_key_weak(pA, -1), be_const_var(6) },
-        { be_const_key_weak(CRYPTO_GROUP_SIZE_BYTES, 10), be_const_int(32) },
-        { be_const_key_weak(set_device_info, -1), be_const_closure(Matter_Crypto_set_device_info_closure) },
+        { be_const_key_weak(compute_pB, 4), be_const_closure(Matter_Crypto_compute_pB_closure) },
+        { be_const_key_weak(w1, 6), be_const_var(4) },
+        { be_const_key_weak(set_device_info, 8), be_const_closure(Matter_Crypto_set_device_info_closure) },
+        { be_const_key_weak(compute_w0_w1, -1), be_const_closure(Matter_Crypto_compute_w0_w1_closure) },
         { be_const_key_weak(L, -1), be_const_var(5) },
-        { be_const_key_weak(spake_M, -1), be_const_var(7) },
-        { be_const_key_weak(w1, -1), be_const_var(4) },
-        { be_const_key_weak(CRYPTO_W_SIZE_BYTES, 4), be_const_int(40) },
-        { be_const_key_weak(passcode, 3), be_const_var(0) },
-        { be_const_key_weak(init, 2), be_const_closure(Matter_Crypto_init_closure) },
-        { be_const_key_weak(w0, 0), be_const_var(3) },
+        { be_const_key_weak(init, -1), be_const_closure(Matter_Crypto_init_closure) },
+        { be_const_key_weak(w0, -1), be_const_var(3) },
+        { be_const_key_weak(pA, -1), be_const_var(6) },
+        { be_const_key_weak(spake_N_hex, -1), be_nested_str_weak(04d8bbd6c639c62937b04d997f38c3770719c629d7014d49a24b4f98baa1292b4907d60aa6bfade45008a636337f5168c64d9bd36034808cd564490b1e656edbe7) },
+        { be_const_key_weak(compute_ZV_verifier, 10), be_const_closure(Matter_Crypto_compute_ZV_verifier_closure) },
+        { be_const_key_weak(M, -1), be_const_var(12) },
+        { be_const_key_weak(x, -1), be_const_var(10) },
+        { be_const_key_weak(passcode, -1), be_const_var(0) },
+        { be_const_key_weak(compute_ZV_prover, -1), be_const_closure(Matter_Crypto_compute_ZV_prover_closure) },
+        { be_const_key_weak(CRYPTO_W_SIZE_BYTES, -1), be_const_int(40) },
+        { be_const_key_weak(pB, 17), be_const_var(7) },
+        { be_const_key_weak(CRYPTO_GROUP_SIZE_BYTES, 16), be_const_int(32) },
+        { be_const_key_weak(N, 14), be_const_var(13) },
+        { be_const_key_weak(compute_pA, -1), be_const_closure(Matter_Crypto_compute_pA_closure) },
+        { be_const_key_weak(V, -1), be_const_var(9) },
+        { be_const_key_weak(y, -1), be_const_var(11) },
+        { be_const_key_weak(iterations, -1), be_const_var(2) },
     })),
     be_str_weak(Matter_Crypto)
 );
