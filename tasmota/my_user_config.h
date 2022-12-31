a4e91b684c097d8b21e9c9b59b2c79ce3cf0ef06
Pascal Heinrich <kaedwen@heinrich.blue>
2022-12-31 12:38:38 +0100

    remove debugging

--- tasmota/my_user_config.h
+++ tasmota/my_user_config.h
@@ -634,10 +634,10 @@
 //    #define USE_PCA9685_FREQ 50                  // Define default PWM frequency in Hz to be used (must be within 24 to 1526) - If other value is used, it will rever to 50Hz
 //  #define USE_PCA9632                            // [I2cDriver91] Enable PCA9632 I2C HW PWM Driver
 //    #define USE_PCA9632_ADDR 0x60                // Define PCA9685 I2C Address to use (Must be within range 0x60 through 0x63 - set according to your wired setup)
-//    #define USE_PCA9632_CM_0 0
-//    #define USE_PCA9632_CM_1 1
-//    #define USE_PCA9632_CM_2 2
-//    #define USE_PCA9632_CM_3 3
+//    #define USE_PCA9632_CM_0 0                   // Mapping for channel 0
+//    #define USE_PCA9632_CM_1 1                   // Mapping for channel 1
+//    #define USE_PCA9632_CM_2 2                   // Mapping for channel 2
+//    #define USE_PCA9632_CM_3 3                   // Mapping for channel 3
 //  #define USE_MPR121                             // [I2cDriver23] Enable MPR121 controller (I2C addresses 0x5A, 0x5B, 0x5C and 0x5D) in input mode for touch buttons (+1k3 code)
 //  #define USE_CCS811                             // [I2cDriver24] Enable CCS811 sensor (I2C address 0x5A) (+2k2 code)
 //  #define USE_CCS811_V2                          // [I2cDriver24] Enable CCS811 sensor (I2C addresses 0x5A and 0x5B) (+2k8 code)
@@ -1177,11 +1177,10 @@
  * Debug features
 \*********************************************************************************************/
 
-#define DEBUG_TASMOTA_TRACE
-#define DEBUG_TASMOTA_CORE                       // Enable core debug messages
-#define DEBUG_TASMOTA_DRIVER                     // Enable driver debug messages
-#define DEBUG_TASMOTA_SENSOR                     // Enable sensor debug messages
-#define USE_DEBUG_DRIVER                         // Use xdrv_99_debug.ino providing commands CpuChk, CfgXor, CfgDump, CfgPeek and CfgPoke
+//#define DEBUG_TASMOTA_CORE                       // Enable core debug messages
+//#define DEBUG_TASMOTA_DRIVER                     // Enable driver debug messages
+//#define DEBUG_TASMOTA_SENSOR                     // Enable sensor debug messages
+//#define USE_DEBUG_DRIVER                         // Use xdrv_99_debug.ino providing commands CpuChk, CfgXor, CfgDump, CfgPeek and CfgPoke
 
 /*********************************************************************************************\
  * Profiling features
