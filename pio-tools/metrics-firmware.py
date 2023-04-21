42e990f7c028d23aa4d559bb62a5395dcd60d80d
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-04-21 15:26:42 +0200

    rm debugging print

--- pio-tools/metrics-firmware.py
+++ pio-tools/metrics-firmware.py
@@ -6,7 +6,6 @@ if env["PIOPLATFORM"] == "espressif32":
     def firm_metrics(source, target, env):
         # Print Metrics for firmware using "map" file from ".pio/build/..."
         import tasmota_metrics
-        print("$PYTHONEXE -m tasmota_metrics " + str(tasmotapiolib.get_source_map_path(env).resolve()))
         env.Execute("$PYTHONEXE -m tasmota_metrics " + str(tasmotapiolib.get_source_map_path(env).resolve()))
 
 env.AddPostAction("$BUILD_DIR/${PROGNAME}.bin",firm_metrics)
