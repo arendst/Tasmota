45eb82bc3fe01d4ab32d1a105f5e20c61c82db3f
Jeroen <jeroenst@users.noreply.github.com>
2023-05-08 09:59:28 +0200

    Fix for not sending changes to mqtt (#18600)
    Co-authored-by: JeroenSt <nospam@nospam.org>

--- tasmota/tasmota_xsns_sensor/xsns_34_hx711.ino
+++ tasmota/tasmota_xsns_sensor/xsns_34_hx711.ino
@@ -430,7 +430,7 @@ void HxEvery100mSecond(void) {
           Hx.weight_diff = Hx.weight;
           Hx.weight_changed = true;
         }
-        else if (Hx.weight_changed && (Hx.weight == Hx.weight_diff)) {
+        else if (Hx.weight_changed && (abs(Hx.weight - Hx.weight_diff) < Hx.weight_delta)) {
           ResponseClear();
           ResponseAppendTime();
           HxShow(true);
