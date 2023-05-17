691be8560c6e663f5c75f7067eb694aac82b5312
stefanbode <stefan_bode@web.de>
2023-05-17 23:38:27 +0200

    Update xdrv_27_esp32_shutter.ino

--- tasmota/tasmota_xdrv_driver/xdrv_27_esp32_shutter.ino
+++ tasmota/tasmota_xdrv_driver/xdrv_27_esp32_shutter.ino
@@ -389,6 +389,10 @@ uint8_t ShutterGetStartRelay(uint8_t index) {
   return ShutterSettings.shutter_startrelay[index];
 }
 
+uint8_t ShutterGetOptions(uint8_t index) {
+   return ShutterSettings.shutter_options[index];
+}
+
 int8_t ShutterGetTiltConfig(uint8_t config_idx,uint8_t index) {
   return Shutter[index].tilt_config[config_idx];
 }
