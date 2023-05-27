8a6e9ca1795f1bab41d3bcd74e871b8754908c22
jonschz <jonschz@users.noreply.github.com>
2023-05-27 10:01:15 +0200

    Relocate RGBWWTable implementation This fixes a bug related to RGBWWTable and SO92. Also, the code is tidier this way

--- tasmota/tasmota_xdrv_driver/xdrv_04_light.ino
+++ tasmota/tasmota_xdrv_driver/xdrv_04_light.ino
@@ -1911,19 +1911,12 @@ void LightAnimate(void)
         cur_col_10[i] = change8to10(Light.new_color[i]);
       }
 
-      bool rgbwwtable_applied_white = false;      // did we already applied RGBWWTable to white channels (ex: in white_blend_mode or virtual_ct)
       if (Light.pwm_multi_channels) {
         calcGammaMultiChannels(cur_col_10);
       } else {
         // AddLog(LOG_LEVEL_INFO, PSTR(">>> calcGammaBulbs In  %03X,%03X,%03X,%03X,%03X"), cur_col_10[0], cur_col_10[1], cur_col_10[2], cur_col_10[3], cur_col_10[4]);
-        rgbwwtable_applied_white = calcGammaBulbs(cur_col_10);     // true means that one PWM channel is used for CT
+        calcGammaBulbs(cur_col_10);
         // AddLog(LOG_LEVEL_INFO, PSTR(">>> calcGammaBulbs Out %03X,%03X,%03X,%03X,%03X"), cur_col_10[0], cur_col_10[1], cur_col_10[2], cur_col_10[3], cur_col_10[4]);
-      }
-
-      // Apply RGBWWTable only if not Settings->flag4.white_blend_mode
-      for (uint32_t i = 0; i < (rgbwwtable_applied_white ? 3 : Light.subtype); i++) {
-        uint32_t adjust = change8to10(Settings->rgbwwTable[i]);
-        cur_col_10[i] = changeUIntScale(cur_col_10[i], 0, 1023, 0, adjust);
       }
 
       // final adjusments for PMW ranges, post-gamma correction
@@ -2320,7 +2313,7 @@ void calcGammaBulb5Channels_8(uint8_t in8[LST_MAX], uint16_t col10[LST_MAX]) {
   calcGammaBulb5Channels(col10, nullptr, nullptr);
 }
 
-bool calcGammaBulbs(uint16_t cur_col_10[5]) {
+void calcGammaBulbs(uint16_t cur_col_10[5]) {
   bool rgbwwtable_applied_white = false;
   bool white_free_cw = false;         // true if White channels are uncorrelated. Happens when CW+WW>255, i.e. manually setting white channels to exceed to total power of a single channel (may harm the power supply)
   // Various values needed for accurate White calculation
@@ -2339,7 +2332,7 @@ bool calcGammaBulbs(uint16_t cur_col_10[5]) {
     calcGammaBulbCW(cur_col_10, &white_bri10, &white_free_cw);
   }
 
-  // Now see if we need to mix RGB and  White
+  // Now see if we need to mix RGB and White
   // Valid only for LST_RGBW, LST_RGBCW, SetOption105 1, and white is zero (see doc)
   if ((LST_RGBW <= Light.subtype) && (Settings->flag4.white_blend_mode) && (0 == cur_col_10[3]+cur_col_10[4])) {
     uint32_t min_rgb_10 = min3(cur_col_10[0], cur_col_10[1], cur_col_10[2]);
@@ -2404,6 +2397,12 @@ bool calcGammaBulbs(uint16_t cur_col_10[5]) {
     }
   }
 
+  // Apply RGBWWTable (RGB: always, CW: only if white_blend_mode is not engaged)
+  for (uint32_t i = 0; i < (rgbwwtable_applied_white ? 3 : Light.subtype); i++) {
+    uint32_t adjust = change8to10(Settings->rgbwwTable[i]);
+    cur_col_10[i] = changeUIntScale(cur_col_10[i], 0, 1023, 0, adjust);
+  }
+
   if ((LST_COLDWARM == Light.subtype) || (LST_RGBCW == Light.subtype)) {
 #ifdef ESP8266
     if ((PHILIPS == TasmotaGlobal.module_type) || (Settings->flag4.pwm_ct_mode)) {   // channel 1 is the color tone, mapped to cold channel (0..255)
@@ -2412,24 +2411,22 @@ bool calcGammaBulbs(uint16_t cur_col_10[5]) {
 #endif  // ESP8266
       // Xiaomi Philips bulbs follow a different scheme:
       // channel 0=intensity, channel1=temperature
+      // Need to compute white_bri10 and ct_10 from cur_col_10[] for compatibility with VirtualCT
       white_bri10 = cur_col_10[cw0] + cur_col_10[cw0+1];
       ct_10 = changeUIntScale(cur_col_10[cw0+1], 0, white_bri10, 0, 1023);
       if (white_free_cw || white_bri10 > 1023) {
-        // cannot represent free_ct_mode here.
-        // There is one edge case: In white_free_cw, the gamma correction
-        // is done for cw and ww separately, so their gamma-corrected sum might
-        // turn out to be smaller than 1023. For consistent behaviour we still set
-        // the brightness to 1023 in this case; otherwise, Color1 0000007979 is much brighter
-        // than Color1 0000008181
+        // Cannot represent white_free_cw in pwm_ct_mode because we can't set white_bri10 > 1023,
+        // so we set the maximum brightness instead. Note that in white_free_cw, the gamma correction
+        // is done for cw and ww separately, so their gamma-corrected sum may be smaller than 1023.
+        // For consistent behaviour we still set the white_bri10 to 1023 in this case; otherwise, 
+        // Color1 0000007979 is much brighter than Color1 0000008181.
         white_bri10 = 1023;
       }
+
       cur_col_10[cw0] = white_bri10;
       cur_col_10[cw0+1] = ct_10;
-      return false;     // avoid any interference
     }
   }
-
-  return rgbwwtable_applied_white;
 }
 
 #ifdef USE_DEVICE_GROUPS
