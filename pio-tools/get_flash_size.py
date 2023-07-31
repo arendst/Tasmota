222b47627f9cce81733ed10c79d8beb4516266ef
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-07-31 17:43:47 +0200

    fix 4MB PIO script upload regression

--- pio-tools/get_flash_size.py
+++ pio-tools/get_flash_size.py
@@ -29,15 +29,17 @@ if "upload" in COMMAND_LINE_TARGETS:
     old_maximum_size = env.BoardConfig().get("upload.maximum_size")
     new_maximum_size = int(size.split("MB")[0]) * 0x100000
 
-    extra_flags = env.BoardConfig().get("build.extra_flags").split(" ")
-    new_flags = ""
-    for flag in extra_flags:
-        if flag.startswith("-DESP32"):
-            flag = f"-DESP32_{size}"
-        new_flags += flag + " "
-    env.BoardConfig().update("build.extra_flags",new_flags)
-    env.BoardConfig().update("upload.flash_size",size)
+    if new_maximum_size > old_maximum_size:
 
-    env.BoardConfig().update("upload.maximum_size", new_maximum_size)
+        extra_flags = env.BoardConfig().get("build.extra_flags").split(" ")
+        new_flags = ""
+        for flag in extra_flags:
+            if flag.startswith("-DESP32"):
+                flag = f"-DESP32_{size}"
+            new_flags += flag + " "
+        env.BoardConfig().update("build.extra_flags",new_flags)
+        env.BoardConfig().update("upload.flash_size",size)
 
-    env.Replace(TASMOTA_flash_size=size)
+        env.BoardConfig().update("upload.maximum_size", new_maximum_size)
+
+        env.Replace(TASMOTA_flash_size=size)
