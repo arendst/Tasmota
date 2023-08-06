fd8596f072aaa211ae5e4f416d984b3982f4706a
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-08-06 13:27:34 +0200

    integrate Arduino 3.0 in devel Tasmota

--- pio-tools/add_c_flags.py
+++ pio-tools/add_c_flags.py
@@ -2,3 +2,6 @@ Import("env")
 
 # General options that are passed to the C compiler (C only; not C++).
 env.Append(CFLAGS=["-Wno-discarded-qualifiers", "-Wno-implicit-function-declaration"])
+
+# General options that are passed to the C++ compiler
+env.Append(CXXFLAGS=["-Wno-volatile"])
