7cafe4ecbc7c1108812f90cfd25f8438022ea57e
Staars <baars@klinikum-brandenburg.de>
2023-08-11 23:53:28 +0200

    fix 8266 builds

--- pio-tools/add_c_flags.py
+++ pio-tools/add_c_flags.py
@@ -5,7 +5,3 @@ env.Append(CFLAGS=["-Wno-discarded-qualifiers", "-Wno-implicit-function-declarat
 
 # General options that are passed to the C++ compiler
 env.Append(CXXFLAGS=["-Wno-volatile"])
-
-# Pass flashmode at build time to macro
-tasmota_flash_mode = "-DCONFIG_TASMOTA_FLASHMODE_" + (env.BoardConfig().get("build.flash_mode", "dio")).upper()
-env.Append(CXXFLAGS=[tasmota_flash_mode])
