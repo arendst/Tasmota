270e1a58d842f69a0cfe740088359e1077de4557
Christian Baars <Baars@gmx.de>
2023-08-12 21:11:40 +0200

    finale fix for all platforms: OneWire.h

--- lib/lib_basic/OneWire-Stickbreaker/OneWire.h
+++ lib/lib_basic/OneWire-Stickbreaker/OneWire.h
@@ -149,7 +149,7 @@
 #include <driver/rtc_io.h>
 #if ESP_IDF_VERSION_MAJOR >= 5
 #include "soc/gpio_periph.h"
-#endif //ESP_IDF_VERSION_MAJOR
+#endif // ESP_IDF_VERSION_MAJOR >= 5
 #define PIN_TO_BASEREG(pin)             (0)
 #define PIN_TO_BITMASK(pin)             (pin)
 #define IO_REG_TYPE uint32_t
@@ -163,9 +163,15 @@ IO_REG_TYPE directRead(IO_REG_TYPE pin)
     return (GPIO.in.val >> pin) & 0x1;
 #else // plain ESP32
     if ( pin < 32 )
+#if ESP_IDF_VERSION_MAJOR >= 5
         return (GPIO.in.val >> pin) & 0x1;
+#else
+        return (GPIO.in >> pin) & 0x1;
+#endif //ESP_IDF_VERSION_MAJOR
+#if (!defined(CONFIG_IDF_TARGET_ESP32C2) && !defined(CONFIG_IDF_TARGET_ESP32C3)) // we do not have that many pins
     else if ( pin < 46 )
-        return ((uint32_t)GPIO.in1.val >> (pin - 32)) & 0x1;
+        return (GPIO.in1.val >> (pin - 32)) & 0x1;
+#endif //(!defined(CONFIG_IDF_TARGET_ESP32C2) && !defined(CONFIG_IDF_TARGET_ESP32C3))
 #endif
 
     return 0;
@@ -178,9 +184,15 @@ void directWriteLow(IO_REG_TYPE pin)
     GPIO.out_w1tc.val = ((uint32_t)1 << pin);
 #else // plain ESP32
     if ( pin < 32 )
+#if ESP_IDF_VERSION_MAJOR >= 5
         GPIO.out_w1tc.val = ((uint32_t)1 << pin);
+#else
+        GPIO.out_w1tc = ((uint32_t)1 << pin);
+#if (!defined(CONFIG_IDF_TARGET_ESP32C2) && !defined(CONFIG_IDF_TARGET_ESP32C3)) // we do not have that many pins
     else if ( pin < 46 )
         GPIO.out1_w1tc.val = ((uint32_t)1 << (pin - 32));
+#endif // (!defined(CONFIG_IDF_TARGET_ESP32C2) && !defined(CONFIG_IDF_TARGET_ESP32C3))
+#endif // ESP_IDF_VERSION_MAJOR >= 5
 #endif
 }
 
@@ -191,9 +203,15 @@ void directWriteHigh(IO_REG_TYPE pin)
     GPIO.out_w1ts.val = ((uint32_t)1 << pin);
 #else // plain ESP32
     if ( pin < 32 )
+#if ESP_IDF_VERSION_MAJOR >= 5
         GPIO.out_w1ts.val = ((uint32_t)1 << pin);
+#else
+        GPIO.out_w1ts = ((uint32_t)1 << pin);
+#endif // ESP_IDF_VERSION_MAJOR >= 5
+#if (!defined(CONFIG_IDF_TARGET_ESP32C2) && !defined(CONFIG_IDF_TARGET_ESP32C3)) // we do not have that many pins
     else if ( pin < 46 )
         GPIO.out1_w1ts.val = ((uint32_t)1 << (pin - 32));
+#endif //(!defined(CONFIG_IDF_TARGET_ESP32C2) && !defined(CONFIG_IDF_TARGET_ESP32C3))
 #endif
 }
 
@@ -216,9 +234,15 @@ void directModeInput(IO_REG_TYPE pin)
 #endif
         // Input
         if ( pin < 32 )
+#if ESP_IDF_VERSION_MAJOR >= 5
             GPIO.enable_w1tc.val = ((uint32_t)1 << pin);
+#else
+            GPIO.enable_w1tc = ((uint32_t)1 << pin);
+#endif
+#if !defined(CONFIG_IDF_TARGET_ESP32C2)
         else
             GPIO.enable1_w1tc.val = ((uint32_t)1 << (pin - 32));
+#endif // !defined(CONFIG_IDF_TARGET_ESP32C2)
     }
 #endif
 }
@@ -226,7 +250,7 @@ void directModeInput(IO_REG_TYPE pin)
 static inline __attribute__((always_inline))
 void directModeOutput(IO_REG_TYPE pin)
 {
-#if CONFIG_IDF_TARGET_ESP32C3
+#if defined(CONFIG_IDF_TARGET_ESP32C3) || defined(CONFIG_IDF_TARGET_ESP32C2)
     GPIO.enable_w1ts.val = ((uint32_t)1 << (pin));
 #else
     if ( digitalPinIsValid(pin) && pin <= 33 ) // pins above 33 can be only inputs
@@ -242,7 +266,11 @@ void directModeOutput(IO_REG_TYPE pin)
 #endif
         // Output
         if ( pin < 32 )
+#if ESP_IDF_VERSION_MAJOR >= 5
             GPIO.enable_w1ts.val = ((uint32_t)1 << pin);
+#else
+            GPIO.enable_w1ts = ((uint32_t)1 << pin);
+#endif
         else // already validated to pins <= 33
             GPIO.enable1_w1ts.val = ((uint32_t)1 << (pin - 32));
     }
