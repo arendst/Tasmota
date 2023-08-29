240d43d32dab9dbe7d0fd7b569e61b7248f427ec
Stefan Weber <stefan.weber93@googlemail.com>
2023-08-29 15:35:38 +0200

    Implement ChallengeResponse
    Signed-off-by: Stefan Weber <stefan.weber93@googlemail.com>

--- tasmota/include/i18n.h
+++ tasmota/include/i18n.h
@@ -260,6 +260,7 @@
 #define D_CMND_PUBLICKEYS "PublicKeys"
 #define D_CMND_PLANETMINTAPI "PlanetmintAPI"
 #define D_CMND_ACCOUNTID "AccountID"
+#define D_CMND_CHALLENGERESPONSE "ChallengeResponse"
 #define D_CMND_STATUS "Status"
   #define D_STATUS1_PARAMETER "PRM"
   #define D_STATUS2_FIRMWARE "FWR"
