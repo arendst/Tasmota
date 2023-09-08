b4ba0b847b6fb5c30a06ee4cabdc42fb68bbcaf3
Christoph Friese <49806974+chrfriese123@users.noreply.github.com>
2023-09-08 11:50:06 +0200

    Update support_features.ino
    Add USE_ENS16x and USE_ENS210

--- tasmota/tasmota_support/support_features.ino
+++ tasmota/tasmota_support/support_features.ino
@@ -903,8 +903,12 @@ void ResponseAppendFeatures(void)
 #if defined(USE_I2C) && defined(USE_MAX17043)
     feature9 |= 0x02000000;
 #endif
-//    feature9 |= 0x04000000;
-//    feature9 |= 0x08000000;
+#if defined(USE_I2C) && defined(USE_ENS16x)
+    feature9 |= 0x04000000;   //xsns_111_ens16x.ino
+#endif
+#if defined(USE_I2C) && defined(USE_ENS210)
+    feature9 |= 0x08000000;    //xsns_112_ens210.ino
+#endif
 
 //    feature9 |= 0x10000000;
 //    feature9 |= 0x20000000;
