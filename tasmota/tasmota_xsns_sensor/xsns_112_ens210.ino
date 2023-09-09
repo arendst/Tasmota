8fa628b549b7e03eda495032462ec3f7693ff1e8
Barbudor <barbudor@barbudor.net>
2023-09-09 17:06:41 +0200

    optimize tasmota side

--- tasmota/tasmota_xsns_sensor/xsns_112_ens210.ino
+++ tasmota/tasmota_xsns_sensor/xsns_112_ens210.ino
@@ -31,138 +31,112 @@
 #define XSNS_112             112
 #define XI2C_85             85  // See I2CDEVICES.md
 
-#define ENS210_I2CADDR_0      0x43
-#define ENS210_I2CADDR_1      0x44
-#define EVERYNSECONDS 5
+#define ENS210_EVERYNSECONDS 	5
+#define ENS210_DEVICE_NAME		"EN210"
+#define ENS210_LOG				    "E21"
+#define ENS210_MAX_COUNT		  2
+
 
 #include "ScioSense_ENS210.h"
 
-ScioSense_ENS210     ens210a(ENS210_I2CADDR_0);
-struct  {
+typedef struct ENS210DATA_s {
+  ScioSense_ENS210     *ens210;
+
   float  temperature;
   float  humidity;
 
-  uint8_t ready = 0;
-  uint8_t type = 0;;
-  uint8_t tcnt = 0;
-  uint8_t ecnt = 0;
-} ENS210dataA;
+  uint8_t ready;
+  uint8_t type;
+  uint8_t tcnt;
+  uint8_t ecnt;
+} ENS210DATA_t;
 
-ScioSense_ENS210      ens210b(ENS210_I2CADDR_1);
-struct  {
-  float  temperature;
-  float  humidity;
-
-  uint8_t ready = 0;
-  uint8_t type = 0;;
-  uint8_t tcnt = 0;
-  uint8_t ecnt = 0;
-} ENS210dataB;
-
+ENS210DATA_t *ENS210data[ENS210_MAX_COUNT] = {nullptr, };
 uint8_t ENS210Count = 0;
 
 /********************************************************************************************/
 
 void ens210Detect(void)
 {
-	ENS210Count = 0;
-	if (!I2cActive(ENS210_I2CADDR_0)) {
-		if (ens210a.begin()) {
-			if (ens210a.available()) {
-				ens210a.setSingleMode(false);
-				ENS210dataA.type = 1;
-				I2cSetActiveFound(ENS210_I2CADDR_0, "ENS210a");
-				ENS210Count++;
-			}
-		}
-	}
-	
-	if (!I2cActive(ENS210_I2CADDR_1)) {
-		if (ens210b.begin()) {
-			if (ens210b.available()) {
-				ens210b.setSingleMode(false);
-				ENS210dataB.type = 1;
-				I2cSetActiveFound(ENS210_I2CADDR_1, "ENS210b");
-				ENS210Count++;
-			}
-		}
-	}
+  ENS210Count = 0;
+  ENS210DATA_t *pENS210;
+  uint8_t i2caddr = ENS210_I2CADDR;
+  for (uint8_t i = 0; i < ENS210_MAX_COUNT; i++, i2caddr++)
+  {
+    if (!I2cActive(i2caddr))
+    {
+      pENS210 = (ENS210DATA_t *)calloc(1, sizeof(ENS210DATA_t));
+      if (!pENS210)
+      {
+        AddLog(LOG_LEVEL_ERROR, PSTR(ENS210_LOG ":@0x02%X Memory error!"), i2caddr);
+        return;
+      }
+      pENS210->ens210 = new ScioSense_ENS210(i2caddr);
+      if (!pENS210->ens210)
+      {
+        AddLog(LOG_LEVEL_ERROR, PSTR(ENS210_LOG ":@0x02%X Memory error 2!"), i2caddr);
+        free(pENS210);
+        return;
+      }
+      ENS210data[i] = pENS210;
+      if (pENS210->ens210->begin())
+      {
+        if (pENS210->ens210->available())
+        {
+          pENS210->ens210->setSingleMode(false);
+          pENS210->type = 1;
+          I2cSetActiveFound(i2caddr, ENS210_DEVICE_NAME);
+          ENS210Count++;
+        }
+      }
+    }
+  }
 }
 
 void ens210Update(void)  // Perform every n second
 {
-	if (ENS210dataA.type) {
-		ENS210dataA.tcnt++;
-		if (ENS210dataA.tcnt >= EVERYNSECONDS) {
-			ENS210dataA.tcnt = 0;
-			ENS210dataA.ready = 0;
-			if (ens210a.available()) {
-				ens210a.measure();
-            
-				ENS210dataA.temperature = ens210a.getTempCelsius();
-				ENS210dataA.humidity = ens210a.getHumidityPercent();
-				ENS210dataA.ready = 1;
-				ENS210dataA.ecnt = 0;
-			} else {
-				// failed, count up
-				ENS210dataA.ecnt++;
-				if (ENS210dataA.ecnt > 6) {
-					// after 30 seconds, restart
-					ens210a.begin();
-					if (ens210a.available()) {
-						ens210a.setSingleMode(false);
-					}
-				}
-			}  
-		}
-	}
-	if (ENS210dataB.type) {
-		ENS210dataB.tcnt++;
-		if (ENS210dataB.tcnt >= EVERYNSECONDS) {
-			ENS210dataB.tcnt = 0;
-			ENS210dataB.ready = 0;
-			if (ens210b.available()) {
-				ens210b.measure();
-            
-				ENS210dataB.temperature = ens210b.getTempCelsius();
-				ENS210dataB.humidity = ens210b.getHumidityPercent();
-				ENS210dataB.ready = 1;
-				ENS210dataB.ecnt = 0;
-			} else {
-				// failed, count up
-				ENS210dataB.ecnt++;
-				if (ENS210dataB.ecnt > 6) {
-					// after 30 seconds, restart
-					ens210b.begin();
-					if (ens210b.available()) {
-						ens210b.setSingleMode(false);
-					}
-				}
-			}  
-		}
-	}
+  for (uint8_t i = 0 ; i < ENS210Count; i++) {
+    ENS210DATA_t *pENS210 = ENS210data[i];
+    if (pENS210->type) {
+      pENS210->tcnt++;
+      if (pENS210->tcnt >= ENS210_EVERYNSECONDS) {
+        pENS210->tcnt = 0;
+        pENS210->ready = 0;
+        if (pENS210->ens210->available()) {
+          pENS210->ens210->measure();
+              
+          pENS210->temperature = pENS210->ens210->getTempCelsius();
+          pENS210->humidity = pENS210->ens210->getHumidityPercent();
+          pENS210->ready = 1;
+          pENS210->ecnt = 0;
+        } else {
+          // failed, count up
+          pENS210->ecnt++;
+          if (pENS210->ecnt > 6) {
+            // after 30 seconds, restart
+            pENS210->ens210->begin();
+            if (pENS210->ens210->available()) {
+              pENS210->ens210->setSingleMode(false);
+            }
+          }
+        }  
+      }
+    }
+  }
 }
 
 void ens210Show(bool json)
 {
-	if (ENS210dataA.type) {
-		if (ENS210dataA.ready) {
-			if (ENS210Count == 1) {
-				TempHumDewShow(json, (0 == TasmotaGlobal.tele_period), "ENS210", ENS210dataA.temperature, ENS210dataA.humidity);
-			} else {
-				TempHumDewShow(json, (0 == TasmotaGlobal.tele_period), "ENS210a", ENS210dataA.temperature, ENS210dataA.humidity);
-			}
-		}
-	}
-	if (ENS210dataB.type) {
-		if (ENS210dataB.ready) {
-			if (ENS210Count == 1) {
-				TempHumDewShow(json, (0 == TasmotaGlobal.tele_period), "ENS210", ENS210dataB.temperature, ENS210dataB.humidity);
-			} else {
-				TempHumDewShow(json, (0 == TasmotaGlobal.tele_period), "ENS210b", ENS210dataB.temperature, ENS210dataB.humidity);
-			}
-		}
-	}
+  char name[20];
+  for (uint8_t i = 0 ; i < ENS210Count; i++) {
+    ENS210DATA_t *pENS210 = ENS210data[i];
+    if (pENS210->type) {
+      if (pENS210->ready) {
+        snprintf_P(name, sizeof(name), (ENS210Count > 1) ? PSTR("%s%c%d") : PSTR("%s"), ENS210_DEVICE_NAME, IndexSeparator(), i +1);
+        TempHumDewShow(json, (0 == TasmotaGlobal.tele_period), name, pENS210->temperature, pENS210->humidity);
+      }
+    }
+  }
 }
 
 /*********************************************************************************************\
