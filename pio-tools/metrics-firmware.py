8246410c8a3fc66efd7ff8548b7e671bbb509d56
blakadder <blakadder@users.noreply.github.com>
2023-09-13 15:06:01 +0200

    fix compile errors on exotic windows codepages (#19508)

--- pio-tools/metrics-firmware.py
+++ pio-tools/metrics-firmware.py
@@ -10,7 +10,7 @@ def firm_metrics(source, target, env):
         env.Execute("$PYTHONEXE -m tasmota_metrics \"" + str(tasmotapiolib.get_source_map_path(env).resolve()) + "\"")
     elif env["PIOPLATFORM"] == "espressif8266":
         map_file = join(env.subst("$BUILD_DIR")) + os.sep + "firmware.map"
-        with open(map_file,'r') as f:
+        with open(map_file,'r', encoding='utf-8') as f:
             phrase = "_text_end = ABSOLUTE (.)"
             for line in f:
                 if  phrase in line:
