ae054008b46d55d894ad3743d840d3d35c76cb3d
Stephan Hadinger <stephan.hadinger@gmail.com>
2023-10-16 12:06:02 +0200

    Re-enable features for Arduino 3

--- tasmota/include/tasmota_configurations_ESP32.h
+++ tasmota/include/tasmota_configurations_ESP32.h
@@ -211,34 +211,10 @@
 #undef FIRMWARE_LVGL
 #undef FIRMWARE_TASMOTA32
 
-#undef USE_EMULATION                             // Disable Wemo or Hue emulation
-#undef USE_EMULATION_HUE                         // Disable Hue Bridge emulation for Alexa (+14k code, +2k mem common)
-#undef USE_EMULATION_WEMO                        // Disable Belkin WeMo emulation for Alexa (+6k code, +2k mem common)
-
 
 // -- Optional modules ----------------------------
-#undef ROTARY_V1                                 // Disable support for MI Desk Lamp
-#undef USE_SONOFF_RF                             // Disable support for Sonoff Rf Bridge (+3k2 code)
-  #undef USE_RF_FLASH                            // Disable support for flashing the EFM8BB1 chip on the Sonoff RF Bridge. C2CK must be connected to GPIO4, C2D to GPIO5 on the PCB
-#undef USE_SONOFF_SC                             // Disable support for Sonoff Sc (+1k1 code)
-#undef USE_TUYA_MCU                              // Disable support for Tuya Serial MCU
-#undef USE_ARMTRONIX_DIMMERS                     // Disable support for Armtronix Dimmers (+1k4 code)
-#undef USE_PS_16_DZ                              // Disable support for PS-16-DZ Dimmer and Sonoff L1 (+2k code)
-#undef USE_SONOFF_IFAN                           // Disable support for Sonoff iFan02 and iFan03 (+2k code)
-#undef USE_BUZZER                                // Disable support for a buzzer (+0k6 code)
-#undef USE_ARILUX_RF                             // Disable support for Arilux RF remote controller
 #undef USE_SHUTTER                               // Disable Shutter support for up to 4 shutter with different motortypes (+6k code)
-#undef USE_DEEPSLEEP                             // Disable support for deepsleep (+1k code)
-#undef USE_EXS_DIMMER                            // Disable support for EX-Store WiFi Dimmer
-#undef USE_HOTPLUG                               // Disable support for HotPlug
-#undef USE_DEVICE_GROUPS                         // Disable support for device groups (+3k5 code)
-#undef USE_PWM_DIMMER                            // Disable support for MJ-SD01/acenx/NTONPOWER PWM dimmers (+4k5 code)
-#undef USE_PWM_DIMMER_REMOTE                     // Disbale support for remote switches to PWM Dimmer
-#undef USE_KEELOQ                                // Disable support for Jarolift rollers by Keeloq algorithm (+4k5 code)
-#undef USE_SONOFF_D1                             // Disable support for Sonoff D1 Dimmer (+0k7 code)
-#undef USE_SHELLY_DIMMER                         // Disable support for Shelly Dimmer (+3k code)
 
-#undef USE_ENERGY_SENSOR                         // Disable energy sensors
 #undef USE_IR_REMOTE                             // Disable IR driver
 #undef USE_AC_ZERO_CROSS_DIMMER                  // Disable support for AC_ZERO_CROSS_DIMMER
 
