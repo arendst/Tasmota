79d02e772c200d7c7ad5a05218f2969ddc322bed
s-hadinger <49731213+s-hadinger@users.noreply.github.com>
2024-03-10 23:03:40 +0100

    HASPmota cleanup (#20920)

--- tasmota/berry/haspmota_src/gen_haspmota.sh
+++
@@ -1,5 +0,0 @@
-# generate all haspmota tapp files
-rm haspmota.tapp; zip -j -0 haspmota.tapp haspmota_core/*
-rm haspmota_demo.tapp ; zip -j -0 haspmota_demo.tapp haspmota_demo/*
-cp haspmota.tapp ../haspmota
-cp haspmota_demo.tapp ../haspmota
