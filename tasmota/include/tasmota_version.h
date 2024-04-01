a8af97133c198e6ae321681c2f3b91f8b257384b
stefanbode <stefan_bode@web.de>
2024-04-01 21:46:39 +0200

    Bump to version 13.4.0.4

--- tasmota/include/tasmota_version.h
+++ tasmota/include/tasmota_version.h
@@ -22,6 +22,6 @@
 
 #define TASMOTA_SHA_SHORT                      // Filled by Github sed
 
-const uint32_t TASMOTA_VERSION = 0x0D040003;   // 13.4.0.3
+const uint32_t TASMOTA_VERSION = 0x0D040004;   // 13.4.0.4
 
 #endif  // _TASMOTA_VERSION_H_
