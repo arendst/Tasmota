8a06984947e7a8f2c4f5082032cc0cfaa0188993
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-04-06 18:27:57 +0200

    Shrink safeboot size for non ESP32 MCU (#21115)
    * Ethernet in safeboot only for ESP32

--- tasmota/include/tasmota_configurations_ESP32.h
+++ tasmota/include/tasmota_configurations_ESP32.h
@@ -21,6 +21,7 @@
 #define _TASMOTA_CONFIGURATIONS_ESP32_H_
 
 #ifdef ESP32
+#include "sdkconfig.h"
 
 /*********************************************************************************************\
  * [tasmota32x-safeboot.bin]
@@ -185,8 +186,10 @@
 #define USE_WEBSERVER
 #define USE_WEBCLIENT
 #define USE_WEBCLIENT_HTTPS
-#define USE_SERIAL_BRIDGE                        // Add support for software Serial Bridge console Tee (+2k code)
-#define USE_ETHERNET
+#if CONFIG_IDF_TARGET_ESP32
+  #define USE_SERIAL_BRIDGE                        // Add support for software Serial Bridge console Tee (+2k code)
+  #define USE_ETHERNET
+#endif  // CONFIG_IDF_TARGET_ESP32
 
 #endif  // FIRMWARE_SAFEBOOT
 
