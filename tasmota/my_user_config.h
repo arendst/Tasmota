7443236fd35b88b99ce1b6e7c3d3844c748fd084
Barbudor <barbudor@barbudor.net>
2024-05-05 20:13:46 +0200

    add code usage

--- tasmota/my_user_config.h
+++ tasmota/my_user_config.h
@@ -491,7 +491,7 @@ https://rya.nc/tasmota-fingerprint.html"
   #define USE_ENHANCED_GUI_WIFI_SCAN             // Enable Wi-Fi scan output with BSSID (+0k5 code)
 //  #define USE_WEBSEND_RESPONSE                   // Enable command WebSend response message (+1k code)
 //  #define USE_WEBGETCONFIG                       // Enable restoring config from external webserver (+0k6)
-//  #define USE_WEBRUN                             // Enable executing a tasmota command file from external web server (+)
+//  #define USE_WEBRUN                             // Enable executing a tasmota command file from external web server (+0.4 code)
 //  #define USE_GPIO_VIEWER                        // Enable GPIO Viewer to see realtime GPIO states (+6k code)
 //    #define GV_SAMPLING_INTERVAL  100            // [GvSampling] milliseconds - Use Tasmota Scheduler (100) or Ticker (20..99,101..1000)
   #define USE_EMULATION_HUE                      // Enable Hue Bridge emulation for Alexa (+14k code, +2k mem common)
