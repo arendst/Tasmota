3436ef59610d98f0170590a5ebee1e5b953e2bb6
macheloortiz <macheloortiz@gmail.com>
2024-05-10 18:14:45 -0500

    fix: pub & sub thinkspeak working

--- tasmota/my_user_config.h
+++ tasmota/my_user_config.h
@@ -71,10 +71,10 @@
 #define WIFI_DNS               "192.168.1.1"     // [IpAddress4] If not using DHCP set DNS1 IP address (might be equal to WIFI_GATEWAY)
 #define WIFI_DNS2              "0.0.0.0"         // [IpAddress5] If not using DHCP set DNS2 IP address (might be equal to WIFI_GATEWAY)
 
-#define STA_SSID1              "docentes"                // [Ssid1] Wi-Fi SSID
-#define STA_PASS1              "do2022asd*"                // [Password1] Wi-Fi password
-#define STA_SSID2              ""                // [Ssid2] Optional alternate AP Wi-Fi SSID
-#define STA_PASS2              ""                // [Password2] Optional alternate AP Wi-Fi password
+#define STA_SSID1              ""                // [Ssid1] Wi-Fi SSID
+#define STA_PASS1              ""                // [Password1] Wi-Fi password
+#define STA_SSID2              "mantenimiento"                // [Ssid2] Optional alternate AP Wi-Fi SSID
+#define STA_PASS2              "123456789"                // [Password2] Optional alternate AP Wi-Fi password
 #define WIFI_AP_PASSPHRASE     ""                // AccessPoint passphrase. For WPA2 min 8 char, for open use "" (max 63 char).
 #define WIFI_CONFIG_TOOL       WIFI_RETRY        // [WifiConfig] Default tool if Wi-Fi fails to connect (default option: 4 - WIFI_RETRY)
                                                  // (WIFI_RESTART, WIFI_MANAGER, WIFI_RETRY, WIFI_WAIT, WIFI_SERIAL, WIFI_MANAGER_RESET_ONLY)
