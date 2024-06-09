8cfa6f28ea0c9ee2b9a07b152d98a5dcbb046385
Theo Arends <11044339+arendst@users.noreply.github.com>
2024-06-09 10:40:01 +0200

    Fix restart on wifimanager timeout

--- tasmota/tasmota_support/support_wifi.ino
+++ tasmota/tasmota_support/support_wifi.ino
@@ -923,6 +923,10 @@ void WifiCheck(uint8_t param)
     if (Wifi.config_counter) {
       Wifi.config_counter--;
       Wifi.counter = Wifi.config_counter +5;
+      if (!Wifi.config_counter) {
+//        SettingsSdkErase();  //  Disabled v6.1.0b due to possible bad wifi connects
+        TasmotaGlobal.restart_flag = 2;
+      }
     } else {
       if (Wifi.counter <= 0) {
         WifiCheckIp();
