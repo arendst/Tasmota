fe280fe6aa88ee2571beacbd5f78b75328838838
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-06-20 16:22:49 +0200

    remove compile flag for risc-v MCUs which is only valid for xtensa MCUs (#21665)

--- pio-tools/add_c_flags.py
+++ pio-tools/add_c_flags.py
@@ -5,3 +5,12 @@ env.Append(CXXFLAGS=["-Wno-volatile"])
 
 # General options that are passed to the C compiler (C only; not C++).
 env.Append(CFLAGS=["-Wno-discarded-qualifiers", "-Wno-implicit-function-declaration", "-Wno-incompatible-pointer-types"])
+
+
+# Remove build flags which are not valid for risc-v
+build_flags = env['BUILD_FLAGS']
+chip = env.get("BOARD_MCU").lower()
+
+if "c" in chip:
+  build_flags.pop(build_flags.index("-mno-target-align"))
+  build_flags.pop(build_flags.index("-mtarget-align"))
