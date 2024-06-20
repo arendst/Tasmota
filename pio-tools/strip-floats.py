bdd24c610d7820e3edacb5cf3b94df23fc811327
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-06-20 22:57:37 +0200

    Update strip-floats.py

--- pio-tools/strip-floats.py
+++ pio-tools/strip-floats.py
@@ -3,13 +3,6 @@ Import('env')
 link_flags = " ".join(env['LINKFLAGS'])
 build_flags = " ".join(env['BUILD_FLAGS'])
 
-#
-# Dump build environment (for debug)
-#print(env.Dump())
-#
-
-link_flags = link_flags.replace("-u _printf_float", "")
-link_flags = link_flags.replace("-u _scanf_float", "")
 if "FIRMWARE_SAFEBOOT" in build_flags:
   # Crash Recorder is not included in safeboot firmware -> remove Linker wrap
   link_flags = link_flags.replace("-Wl,--wrap=panicHandler", "")
