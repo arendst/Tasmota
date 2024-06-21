6021b72b4546e2a7e228951b11d63c0cba20d46f
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-06-21 12:45:52 +0200

    refactor strip-flags.py

--- pio-tools/strip-flags.py
+++ pio-tools/strip-flags.py
@@ -1,15 +1,15 @@
 Import('env')
 
-link_flags = " ".join(env['LINKFLAGS'])
+link_flags = env['LINKFLAGS']
 build_flags = " ".join(env['BUILD_FLAGS'])
 
 if "FIRMWARE_SAFEBOOT" in build_flags:
   # Crash Recorder is not included in safeboot firmware -> remove Linker wrap
-  link_flags = link_flags.replace("-Wl,--wrap=panicHandler", "")
-  link_flags = link_flags.replace("-Wl,--wrap=xt_unhandled_exception", "")
-
-new_link_flags = link_flags.split()
-
-env.Replace(
-  LINKFLAGS=new_link_flags
-)
+  try:
+    link_flags.pop(link_flags.index("-Wl,--wrap=panicHandler"))
+  except:
+    do_nothing=""
+  try:
+    link_flags.pop(link_flags.index("-Wl,--wrap=xt_unhandled_exception"))
+  except:
+    do_nothing=""
