9c7a506f7d8c2bb9ec564e91b578d563e61f1712
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-06-21 14:21:02 +0200

    move var to top

--- pio-tools/add_c_flags.py
+++ pio-tools/add_c_flags.py
@@ -1,4 +1,6 @@
 Import("env")
+build_flags = env['BUILD_FLAGS']
+chip = env.get("BOARD_MCU").lower()
 
 # General options that are passed to the C++ compiler
 env.Append(CXXFLAGS=["-Wno-volatile"])
@@ -6,11 +8,7 @@ env.Append(CXXFLAGS=["-Wno-volatile"])
 # General options that are passed to the C compiler (C only; not C++).
 env.Append(CFLAGS=["-Wno-discarded-qualifiers", "-Wno-implicit-function-declaration", "-Wno-incompatible-pointer-types"])
 
-
 # Remove build flags which are not valid for risc-v
-build_flags = env['BUILD_FLAGS']
-chip = env.get("BOARD_MCU").lower()
-
 if chip in ("esp32c2", "esp32c3", "esp32c6", "esp32h2"):
   try:
     build_flags.pop(build_flags.index("-mno-target-align"))
