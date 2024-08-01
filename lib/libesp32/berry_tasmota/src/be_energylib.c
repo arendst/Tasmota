65717a72d24981a0f31dc3c41e14a9d104792b18
Stephan Hadinger <stephan.hadinger@gmail.com>
2024-08-01 22:08:28 +0200

    Berry `energy` module support for 8 phases and move to pseudo-arrays

--- lib/libesp32/berry_tasmota/src/be_energylib.c
+++ lib/libesp32/berry_tasmota/src/be_energylib.c
@@ -9,6 +9,9 @@
 
 #ifdef USE_ENERGY_SENSOR
 
+// unless we get it from an include
+#define ENERGY_MAX_PHASES         8        // Support max eight phases/channels
+
 extern struct ENERGY Energy;
 extern int module_energy_update_total(bvm *vm);
 
@@ -21,10 +24,17 @@ module energy (scope: global) {
   init, closure(module_energy_init_closure)
   _ptr, comptr(&Energy)
   _deref, closure(module_energy__deref_closure)
+  _phases, int(ENERGY_MAX_PHASES)
+
+  _phases_float, class(be_class_energy_phases_float)
+  _phases_int32, class(be_class_energy_phases_int32)
+  _phases_uint8, class(be_class_energy_phases_uint8)
+  _phases_uint16, class(be_class_energy_phases_uint16)
 
   read, closure(module_energy_read_closure)
   member, closure(module_energy_member_closure)
   setmember, closure(module_energy_setmember_closure)
+  tomap, closure(module_energy_tomap_closure)
 
   update_total, func(module_energy_update_total)
 }
