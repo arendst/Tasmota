1e58c9845d07a2c6b701a10330be9d94990ae285
stefanbode <stefan_bode@web.de>
2024-08-15 09:48:04 +0200

    fix potential overflow

--- tasmota/tasmota_xdrv_driver/xdrv_27_esp32_shutter.ino
+++ tasmota/tasmota_xdrv_driver/xdrv_27_esp32_shutter.ino
@@ -213,12 +213,11 @@ void ShutterAllowPreStartProcedure(uint8_t i) {
   // Prestart allow e.g. to release a LOCK or something else before the movement start
   // Anyway, as long var1 != 99 this is skipped (luckily)
 #ifdef USE_RULES
-  uint32_t uptime_Local = 0;
   AddLog(LOG_LEVEL_DEBUG_MORE, PSTR("SHT: Delay Start? var%d <99>=<%s>, max10s?"),i + 1, rules_vars[i]);
   // wait for response from rules
   uint32_t start_time = millis();
   while (TimePassedSince(start_time) < 10000 && (String)rules_vars[i] == "99") {
-    delay(1);
+      delay(1);
   }
 #endif  // USE_RULES
 }
