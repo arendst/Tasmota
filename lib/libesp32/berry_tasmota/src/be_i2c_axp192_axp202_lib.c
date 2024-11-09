a5c33eba5ef731b9e3af28364d2b49cf89bdcabd
s-hadinger <49731213+s-hadinger@users.noreply.github.com>
2024-11-09 19:29:29 +0100

    Berry drivers for PCA9535 (generic and in SenseCAP D1) (#22451)

--- lib/libesp32/berry_tasmota/src/be_i2c_axp192_axp202_lib.c
+++ lib/libesp32/berry_tasmota/src/be_i2c_axp192_axp202_lib.c
@@ -1,5 +1,5 @@
 /********************************************************************
- * Tasmota LVGL lv_signal_bars widget
+ * Drivers for AXP192 and AXP202 I2C Solidified
  *******************************************************************/
 #include "solidify/solidified_i2c_axp192.h"
 #include "solidify/solidified_i2c_axp202.h"
