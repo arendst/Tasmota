cf3185448b97184fa989e6d48ab581c94b81205a
md5sum-as <md5sum.as@gmail.com>
2024-12-19 10:25:31 +0300

    Update tasmota/my_user_config.h
    Co-authored-by: Erik Kunze <eku@users.noreply.github.com>

--- tasmota/my_user_config.h
+++ tasmota/my_user_config.h
@@ -895,7 +895,7 @@
 //  #define VINDRIKTNING_SHOW_PM1                  // Display undocumented/supposed PM1.0 values
 //  #define VINDRIKTNING_SHOW_PM10                 // Display undocumented/supposed PM10 values
 //#define USE_LD2410                               // Add support for HLK-LD2410 24GHz smart wave motion sensor (+3k7 code, 88 RAM)
-//#define USE_LD2410S                               // Add support for HLK-LD2410S Ultra Low-power 24GHz smart wave motion sensor (+4k7 code, 144 RAM)
+//#define USE_LD2410S                              // Add support for HLK-LD2410S Ultra Low-power 24GHz smart wave motion sensor (+4k7 code, 144 RAM)
 //#define USE_LOX_O2                               // Add support for LuminOx LOX O2 Sensor (+0k8 code)
 //#define USE_GM861                                // Add support for GM861 1D and 2D Bar Code Reader (+1k3 code)
 //  #define GM861_DECODE_AIM                       // Decode AIM-id (+0k3 code)
