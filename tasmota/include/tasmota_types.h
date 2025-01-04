e0d58e949dd1b94f8614b9dcc5f3b34d6c625509
s-hadinger <49731213+s-hadinger@users.noreply.github.com>
2025-01-04 12:12:41 +0100

    fix compilation

--- tasmota/include/tasmota_types.h
+++ tasmota/include/tasmota_types.h
@@ -789,7 +789,7 @@ typedef struct {
 
   uint8_t       free_eb0[20];              // EB0  20 bytes
 
-  uint16_t      light_pixels_height : 15;  // EC4  Pixels height minus 1, default 0 (0 means 1 line)
+  uint16_t      light_pixels_height_1 : 15;// EC4  Pixels height minus 1, default 0 (0 means 1 line)
   uint16_t      light_pixels_alternate : 1;// EC4  Indicates alternate lines in Pixels Matrix
   uint8_t       shift595_device_count;     // EC6
   uint8_t       sta_config;                // EC7
