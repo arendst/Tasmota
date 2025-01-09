759e0d0957af5aab86e07d242a9425ec9f125208
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-01-09 18:46:32 +0100

    fix IDF version compare

--- lib/libesp32/ESP32-to-ESP8266-compat/src/esp8266toEsp32.cpp
+++ lib/libesp32/ESP32-to-ESP8266-compat/src/esp8266toEsp32.cpp
@@ -315,7 +315,7 @@ int32_t analogAttach(uint32_t pin, bool output_invert) {    // returns ledc chan
       (ledc_timer_t)timer,        // timer_sel
       0,            // duty
       0,            // hpoint
-#if ESP_IDF_VERSION > ESP_IDF_VERSION_VAL(5, 3, 0)
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 4, 0)
       (ledc_sleep_mode_t) 2,
 #endif
       { output_invert ? 1u : 0u },// output_invert
