5ac1fdb8fe626ca01f15d84efb4edf3c4d50dd5c
anishsane <anishsane@gmail.com>
2025-01-16 21:10:37 +0530

    Added "cam":1 in the discovery message, so that we can create a home assistant camera (#22818)
    * Added "cam":1 in the discovery message, so that we can create a home assistant camera.
    
    * Update tasmota/tasmota_xdrv_driver/xdrv_12_discovery.ino
    
    Co-authored-by: Erik Kunze <eku@users.noreply.github.com>
    
    * Rearrange the newly added field by size, for better packing.
    
    ---------
    
    Co-authored-by: Erik Kunze <eku@users.noreply.github.com>

--- tasmota/tasmota.ino
+++ tasmota/tasmota.ino
@@ -314,8 +314,9 @@ struct TasmotaGlobal_t {
   bool pwm_present;                         // Any PWM channel configured with SetOption15 0
   bool i2c_enabled[2];                      // I2C configured for all possible buses (1 or 2)
 #ifdef ESP32
+  bool camera_initialized;                  // For esp32-webcam, to be used in discovery
   bool ota_factory;                         // Select safeboot binary
-#endif
+#endif  // ESP32
   bool ntp_force_sync;                      // Force NTP sync
   bool skip_light_fade;                     // Temporarily skip light fading
   bool restart_halt;                        // Do not restart but stay in wait loop
