dea536634ead73aef630246840523107645f904c
zzdovydas <zzdovydas@gmail.com>
2025-02-09 15:45:35 +0200

    add_lang_lt_to_config_files

--- tasmota/my_user_config.h
+++ tasmota/my_user_config.h
@@ -389,6 +389,7 @@
 //#define MY_LANGUAGE            hu_HU           // Hungarian in Hungary
 //#define MY_LANGUAGE            it_IT           // Italian in Italy
 //#define MY_LANGUAGE            ko_KO           // Korean in Korea
+//#define MY_LANGUAGE            lt_LT           // Lithuanian in Lithuania
 //#define MY_LANGUAGE            nl_NL           // Dutch in the Nederland
 //#define MY_LANGUAGE            pl_PL           // Polish in Poland
 //#define MY_LANGUAGE            pt_BR           // Portuguese in Brazil
