42c1096537d0821ff685e60d3e938c817cbffbe8
s-hadinger <49731213+s-hadinger@users.noreply.github.com>
2025-03-27 22:44:57 +0100

    Berry bytes add with 3 bytes

--- lib/libesp32/berry/src/be_byteslib.c
+++ lib/libesp32/berry/src/be_byteslib.c
@@ -258,6 +258,26 @@ static size_t buf_add2_be(buf_impl* attr, const uint16_t data) // append 16 bits
     return attr->len;
 }
 
+static size_t buf_add3_le(buf_impl* attr, const uint32_t data) // append 32 bits value
+{
+    if (attr->len < attr->size - 2) {     // do we have room for 4 bytes
+        attr->bufptr[attr->len++] = data;
+        attr->bufptr[attr->len++] = data >> 8;
+        attr->bufptr[attr->len++] = data >> 16;
+    }
+    return attr->len;
+}
+
+size_t buf_add3_be(buf_impl* attr, const uint32_t data) // append 32 bits value
+{
+    if (attr->len < attr->size - 2) {     // do we have room for 4 bytes
+        attr->bufptr[attr->len++] = data >> 16;
+        attr->bufptr[attr->len++] = data >> 8;
+        attr->bufptr[attr->len++] = data;
+    }
+    return attr->len;
+}
+
 static size_t buf_add4_le(buf_impl* attr, const uint32_t data) // append 32 bits value
 {
     if (attr->len < attr->size - 3) {     // do we have room for 4 bytes
@@ -838,10 +858,12 @@ static int m_add(bvm *vm)
             case -1:    /* fallback below */
             case 1:     buf_add1(&attr, v);       break;
             case 2:     buf_add2_le(&attr, v);    break;
+            case 3:     buf_add3_le(&attr, v);    break;
             case 4:     buf_add4_le(&attr, v);    break;
             case -2:    buf_add2_be(&attr, v);    break;
+            case -3:    buf_add3_be(&attr, v);    break;
             case -4:    buf_add4_be(&attr, v);    break;
-            default:    be_raise(vm, "type_error", "size must be -4, -2, -1, 0, 1, 2 or 4.");
+            default:    be_raise(vm, "type_error", "size must be between -4 and 4.");
         }
         be_pop(vm, argc - 1);
         m_write_attributes(vm, 1, &attr);  /* update attributes */
