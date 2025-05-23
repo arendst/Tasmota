fcbb98845a60f0ee3cf6bf595ab02f6f5feb4d1c
Fabrizio Amodio <ziofabry@hotmail.com>
2025-05-23 20:13:03 +0200

    Berry: 'webserver.content_status_sticker' attribute parameter added

--- tasmota/tasmota_xdrv_driver/xdrv_52_3_berry_webserver.ino
+++ tasmota/tasmota_xdrv_driver/xdrv_52_3_berry_webserver.ino
@@ -403,14 +403,18 @@ extern "C" {
     be_raise(vm, kTypeError, nullptr);
   }
 
-  // Berry: `webserver.content_status_sticker(msg:string) -> nil`
+  // Berry: `webserver.content_status_sticker(msg:string,attr:string) -> nil`
   //
   int32_t w_webserver_content_status_sticker(struct bvm *vm) {
   #ifdef USE_WEB_STATUS_LINE
     int32_t argc = be_top(vm); // Get the number of arguments
     if (argc >= 1 && be_isstring(vm, 1)) {
       const char * msg = be_tostring(vm, 1);
-      WSContentStatusSticker(msg);
+      const char * attr = nullptr;
+      if (argc >= 2 && be_isstring(vm, 2)) {
+        attr = be_tostring(vm, 2);
+      }
+      WSContentStatusSticker(msg, attr);
       be_return_nil(vm);
     }
     be_raise(vm, kTypeError, nullptr);
