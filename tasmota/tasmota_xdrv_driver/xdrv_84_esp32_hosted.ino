cfb984fcb784bed3689dfdfae9c4e057b0c79f62
Jason2866 <obermeier.johann@googlemail.com>
2025-09-18 18:43:37 +0200

    fix compile / weird linker error

--- tasmota/tasmota_xdrv_driver/xdrv_84_esp32_hosted.ino
+++ tasmota/tasmota_xdrv_driver/xdrv_84_esp32_hosted.ino
@@ -20,6 +20,7 @@
 #endif  //#include "port_esp_hosted_host_config.h"
 #include "esp_hosted_api_types.h"
 #include "esp_hosted_ota.h"
+#include "esp_hosted_transport_config.h"
 
 struct Hosted_t {
   char *hosted_ota_url;                     // Hosted MCU OTA URL
