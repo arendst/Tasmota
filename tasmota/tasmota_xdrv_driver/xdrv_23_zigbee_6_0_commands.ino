bf16a469e581703149abca9008edd1bcfaa22c01
SteWers <github@wershoven.com>
2026-01-03 23:07:36 +0100

    get32IBigEndian

--- tasmota/tasmota_xdrv_driver/xdrv_23_zigbee_6_0_commands.ino
+++ tasmota/tasmota_xdrv_driver/xdrv_23_zigbee_6_0_commands.ino
@@ -549,7 +549,7 @@ void parseSingleTuyaAttribute(Z_attribute & attr, const SBuffer &buf,
       attr.setUInt(buf.get8(i));
       break;
     case 0x02:      // 4 bytes value (signed? Yes, signed!)
-      attr.setInt(buf.get32BigEndian(i));
+      attr.setInt(buf.get32IBigEndian(i));
       break;
     case 0x03:      // String (we expect it is not ended with \00)
      {
