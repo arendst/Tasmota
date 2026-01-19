1840283df6f6d0d0c9bc8b82bd4cae973391e480
Gerhard Mutz <gerhardmutz1@imac.local>
2026-01-19 09:14:08 +0100

    new_jpeg

--- tasmota/tasmota_xdrv_driver/xdrv_13_display.ino
+++ tasmota/tasmota_xdrv_driver/xdrv_13_display.ino
@@ -2412,10 +2412,14 @@ char ppath[16];
 #define USE_NEW_JPG
 #include "img_converters.h"
 #include "jpeg_decoder.h"
-#include "esp_jpg_decode.h"
 
+#ifndef USE_NEW_JPG
+#include "esp_jpg_decode.h"
 bool jpg2rgb888(const uint8_t *src, size_t src_len, uint8_t * out, jpg_scale_t scale);
 bool jpg2rgb565(const uint8_t *src, size_t src_len, uint8_t * out, jpg_scale_t scale);
+#endif
+
+
 char get_jpeg_size(unsigned char* data, unsigned int data_size, unsigned short *width, unsigned short *height);
 #endif // JPEG_PICTS
 #endif // ESP32
