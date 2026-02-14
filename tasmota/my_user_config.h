a95cfe9acbd36e6275f21f9865bf0f710d75d711
TheHexaMaster <129121144+TheHexaMaster@users.noreply.github.com>
2026-02-14 02:38:14 +0100

    Tasmota - Berry ASYNC Webclient integration
    Add library.json for Async HttpClient Light
    
    Update readme.txt for AsyncHttpClientLight
    
    Clarified the integration of AsyncHttpClientLight with Berry webclient and improved formatting for example URLs.
    
    AsyncHttpClientLight.cpp
    
    Add AsyncHttpClientLight header file
    
    Integrate ASYNC Webclient functionality
    
    Add USE_BERRY_WEBCLIENT_ASYNC feature
    
    USE_BERRY_WEBCLIENT_ASYNC definition for async webclient mode.
    
    Refactor webclient to use AsyncHttpClientLight

--- tasmota/my_user_config.h
+++ tasmota/my_user_config.h
@@ -1235,6 +1235,7 @@
 #define USE_AUTOCONF                             // Enable Esp32(x) autoconf feature, requires USE_BERRY and USE_WEBCLIENT_HTTPS (12KB Flash)
 #define USE_EXTENSION_MANAGER                    // Enable Esp32(x) extensions manager, requires USE_BERRY and USE_WEBCLIENT_HTTPS (11KB Flash)
 #define USE_BERRY                                // Enable Berry scripting language
+//  #define USE_BERRY_WEBCLIENT_ASYNC              // Enable ASYNC webclient mode as an additional mode to standary berry webclient.   
   #define USE_BERRY_PYTHON_COMPAT                // Enable by default `import python_compat`
   #define USE_BERRY_TIMEOUT             4000     // Timeout in ms, will raise an exception if running time exceeds this timeout
   #define USE_BERRY_PSRAM                        // Allocate Berry memory in PSRAM if PSRAM is connected - this might be slightly slower but leaves main memory intact
