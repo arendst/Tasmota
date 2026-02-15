eebaa9a66d0fa349d8037979b90db6080d34874e
TheHexaMaster <129121144+TheHexaMaster@users.noreply.github.com>
2026-02-15 23:04:16 +0100

    Fix formatting in license comment section

--- tasmota/tasmota_xdrv_driver/xdrv_93_fm24cxx.ino
+++ tasmota/tasmota_xdrv_driver/xdrv_93_fm24cxx.ino
@@ -6,7 +6,7 @@
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
+  (at your option) any later version. 
 
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
