a7ce4f527dccbd6f0652515aeec9a5b7142bb7b8
bovirus <1262554+bovirus@users.noreply.github.com>
2026-02-18 11:06:35 +0100

    Italian language

--- tasmota/language/it_IT.h
+++ tasmota/language/it_IT.h
@@ -28,7 +28,7 @@
  * Use online command StateText to translate ON, OFF, HOLD and TOGGLE.
  * Use online command Prefix to translate cmnd, stat and tele.
  *
- * Updated until v9.4.0.1 - Last update 28.11.2025
+ * Updated until v9.4.0.1 - Last update 18.02.2026
 \*********************************************************************/
 
 #define LANGUAGE_MODULE_NAME         // Enable to display "Module Generic" (ie Spanish), Disable to display "Generic Module" (ie English)
@@ -118,7 +118,7 @@
 #define D_GATEWAY              "Gateway"
 #define D_GROUP                "Gruppo"
 #define D_HALL_EFFECT          "Effetto hall"
-#define D_HCHO "Formaldehyde"
+#define D_HCHO                 "Formaldeide"
 #define D_HEATINDEX            "Indice calore"
 #define D_HOST                 "Host"
 #define D_HOSTNAME             "Nome host"
