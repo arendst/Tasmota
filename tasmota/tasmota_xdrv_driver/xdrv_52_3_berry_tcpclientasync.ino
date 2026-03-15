d0e7cc34f4f7fb5356b904b8e7c0c070db9e8bba
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2026-03-15 18:17:53 +0100

    Picolib

--- tasmota/tasmota_xdrv_driver/xdrv_52_3_berry_tcpclientasync.ino
+++ tasmota/tasmota_xdrv_driver/xdrv_52_3_berry_tcpclientasync.ino
@@ -26,10 +26,17 @@
 #include <lwip/sockets.h>
 #include <lwip/netdb.h>
 
+#ifdef CONFIG_LIBC_PICOLIBC
+#define T_IN6_IS_ADDR_V4MAPPED(a) \
+        ((((const uint32_t *) (a))[0] == 0) \
+         && (((const uint32_t *) (a))[1] == 0) \
+         && (((const uint32_t *) (a))[2] == htonl (0xffff)))
+#else
 #define T_IN6_IS_ADDR_V4MAPPED(a) \
         ((((__const uint32_t *) (a))[0] == 0) \
          && (((__const uint32_t *) (a))[1] == 0) \
          && (((__const uint32_t *) (a))[2] == htonl (0xffff)))
+#endif
 
 enum class AsyncTCPState {
   INPROGRESS,
