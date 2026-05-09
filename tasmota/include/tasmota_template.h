4b7523b05d70cbfc1d3ad301488306e8fb287526
Jacek Ziółkowski <jacek@ziolkowscy.com>
2026-05-09 12:12:31 +0200

    fix typo

--- tasmota/include/tasmota_template.h
+++ tasmota/include/tasmota_template.h
@@ -237,7 +237,7 @@ enum UserSelectablePins {
 #endif
   GPIO_VID6608_F, GPIO_VID6608_CW,      // VID6608
   GPIO_MKSKYBLU_TX, GPIO_MKSKYBLU_RX,   // MakeSkyBlue solar charge controller
-  GPIO_MBR_RX_ENA,                      // Modbus Bridge Serial Receive Enable
+  GPIO_MBS_RX_ENA,                      // Modbus Bridge Serial Receive Enable
   GPIO_SENSOR_END };
 
 // Error as warning to rethink GPIO usage with max 2045
@@ -1039,7 +1039,7 @@ const uint16_t kGpioNiceList[] PROGMEM = {
 #ifdef USE_MODBUS_BRIDGE
   AGPIO(GPIO_MBR_TX_ENA),                        // Modbus Bridge Serial interface
   AGPIO(GPIO_MBR_TX),                            // Modbus Bridge Serial interface
-  AGPIO(GPIO_MBR_RX_ENA),                        // Modbus Bridge Serial interface
+  AGPIO(GPIO_MBS_RX_ENA),                        // Modbus Bridge Serial interface
   AGPIO(GPIO_MBR_RX),                            // Modbus Bridge Serial interface
 #endif
 #ifdef USE_TCP_BRIDGE
