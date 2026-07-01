5363042f457fa44c7942e39d2d86536e5b634e13
Jens.Heilig <jens.heilig@esystems-mtg.de>
2026-07-02 00:14:43 +0200

    Fix *float* output in EQ3 mqtt messages
    Recent core refactoring removed standard support for float variables in printf family functions, resulting in eQ-3 telemetry messages showing '*float' instead of actual float values. Updated the formatting specifier to %1_f and passed the reference to the float parameter to ResponseAppend_P function

--- tasmota/tasmota_xdrv_driver/xdrv_85_esp32_ble_eq3_trv.ino
+++ tasmota/tasmota_xdrv_driver/xdrv_85_esp32_ble_eq3_trv.ino
@@ -461,7 +461,7 @@ int EQ3ParseOp(BLE_ESP32::generic_sensor_t *op, bool success, int retries){
   if ((statlen >= 6) && (status[0] == 2) && (status[1] == 1)) {
     ResponseAppend_P(PSTR(",\"stattime\":%u"), stattime);
     eq3->TargetTemp = (float)status[5] / 2;
-    ResponseAppend_P(PSTR(",\"temp\":%2.1f"), eq3->TargetTemp);
+    ResponseAppend_P(PSTR(",\"temp\":%1_f"), &(eq3->TargetTemp));
     eq3->DutyCycle = status[3];
     ResponseAppend_P(PSTR(",\"posn\":%d"), eq3->DutyCycle);
     int stat = status[2];
@@ -518,11 +518,16 @@ int EQ3ParseOp(BLE_ESP32::generic_sensor_t *op, bool success, int retries){
       );
 
     if (statlen >= 15) {
-      ResponseAppend_P(PSTR(",\"windowtemp\":%2.1f"), ((float)status[10]) /  2);
+      float f_temp;
+      f_temp = ((float)status[10]) /  2;
+      ResponseAppend_P(PSTR(",\"windowtemp\":%1_f"), &f_temp);
       ResponseAppend_P(PSTR(",\"windowdur\":%d"), ((int)status[11]) * 5);
-      ResponseAppend_P(PSTR(",\"day\":%2.1f"), ((float)status[12]) / 2);
-      ResponseAppend_P(PSTR(",\"night\":%2.1f"), ((float)status[13]) / 2);
-      ResponseAppend_P(PSTR(",\"offset\":%2.1f"), ((float)status[14] - 7) / 2);
+      f_temp = ((float)status[12]) / 2;
+      ResponseAppend_P(PSTR(",\"day\":%1_f"), &f_temp);
+      f_temp = ((float)status[13]) / 2;
+      ResponseAppend_P(PSTR(",\"night\":%1_f"), &f_temp);
+      f_temp = ((float)status[14] - 7) / 2;
+      ResponseAppend_P(PSTR(",\"offset\":%1_f"), &f_temp);
     }
 
   }
@@ -554,7 +559,7 @@ int EQ3ParseOp(BLE_ESP32::generic_sensor_t *op, bool success, int retries){
         mm *= 10;
         int hh = mm / 60;
         mm = mm % 60;
-        ResponseAppend_P(PSTR("%2.1f-%02d:%02d"), t, hh, mm);
+        ResponseAppend_P(PSTR("%1_f-%02d:%02d"), &t, hh, mm);
         // stop if the last one is 24.
         if (hh == 24){
           break;
