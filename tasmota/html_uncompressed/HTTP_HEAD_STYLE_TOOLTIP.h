946ba2710844ef22beb1c3fb6f82adf62c3f40b9
SteWers <42718143+SteWers@users.noreply.github.com>
2026-09-12 12:22:59 +0200

    Fix MI32 battery polling and period handling (#25024)
    * Fix MI32 battery polling and period handling
    
    - Add a dedicated hourly battery polling cycle for BLE devices, independent of the normal `MI32Period` / `TelePeriod` interval. This extends the battery life of the BLE devices, especially the LYWSD02MMC.
    - Avoid redundant active reads for LYWSD02/03 sensors.
    - `lastTime` is updated whenever temperature, humidity, or battery data is received.
    - `MI32Period 1` correctly resets internal counters and trigger an immediate active polling cycle.
    - `MI32Period 0` now correctly disables the active polling.
    - Add device type to the title.
    
    * Fix pollSeconds
    
    * Multiline tooltip formatting
    
    Split tooltip in two lines.
    Adjust tooltip CSS to allow manual line breaks on mobile devices.
    
    * Optimize tooltip position
    
    * Update HTTP_HEAD_STYLE_TOOLTIP.h

--- tasmota/html_uncompressed/HTTP_HEAD_STYLE_TOOLTIP.h
+++ tasmota/html_uncompressed/HTTP_HEAD_STYLE_TOOLTIP.h
@@ -1,14 +1,19 @@
 const char HTTP_HEAD_STYLE_TOOLTIP[] PROGMEM =
   // Enable tooltips on touch media
   "@media(pointer:coarse),(hover:none){"
-    "[title]{position:relative;display:inline-flex;justify-content:center;}"
+    "[title]{position:relative;display:inline-block;}"
     "[title]:hover::after{"
-      "content:attr(title);position:absolute;top:90%;"
-      "color:var(--c_btntxt);background-color:var(--c_btn);border:1px solid;"
-      "width:fit-content;"
+      "content:attr(title);"
+      "position:absolute;"
+      "top:100%;"
+      "left:0;"
+      "margin-left:-50%;"
+      "color:var(--c_btntxt);"
+      "background-color:var(--c_btn);"
+      "border:1px solid;"
       "padding:2px 5px;"
       "font-style:normal;"    // Disable italic and/or bold
       "font-size:12px;"
-      "white-space:nowrap;"   // Stay on one line
+      "white-space:pre;"      // Stay on one line except sending \n
     "}"
   "}";
