32204a02a2cd937069fa46dc8c2f0b0265965cc7
s-hadinger <49731213+s-hadinger@users.noreply.github.com>
2026-09-22 10:19:27 +0200

    Fix WT32_ETH01 initialization

--- tasmota/tasmota_xdrv_driver/xdrv_82_esp32_ethernet.ino
+++ tasmota/tasmota_xdrv_driver/xdrv_82_esp32_ethernet.ino
@@ -226,16 +226,17 @@ void EthernetSetIp(void) {
 void EthernetInit(void) {
   if (!Settings->flag4.network_ethernet) { return; }
 
-  int32_t eth_type = (Settings->eth_type < sizeof(eth_type_xtable)) ? eth_type_xtable[Settings->eth_type] : 0;    // make sure we don't overflow
-  bool eth_uses_spi = (eth_type & ETH_USES_SPI);
-  eth_type = eth_type & 0x7F;     // remove SPI flag
-#if CONFIG_ETH_USE_ESP32_EMAC
+#if CONFIG_ETH_USE_ESP32_EMAC     // deprecated, please use standard module and template + configuration instead
   if (WT32_ETH01 == TasmotaGlobal.module_type) {
     Settings->eth_address = 1;                    // EthAddress
     Settings->eth_type = ETH_PHY_LAN8720;         // EthType 0 = LAN8720
     Settings->eth_clk_mode = 0;                   // EthClockMode 0 = ETH_CLOCK_GPIO0_IN
   }
 #endif  // CONFIG_ETH_USE_ESP32_EMAC
+
+  int32_t eth_type = (Settings->eth_type < sizeof(eth_type_xtable)) ? eth_type_xtable[Settings->eth_type] : 0;    // make sure we don't overflow
+  bool eth_uses_spi = (eth_type & ETH_USES_SPI);
+  eth_type = eth_type & 0x7F;     // remove SPI flag
 
   if (eth_uses_spi) {
     // Uses SPI Ethernet and needs at least SPI CS being ETH MDC
